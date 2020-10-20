b2e8d34d06faf588281ec3f2321176e24c939664
Matt Farina <matt@mattfarina.com>
2020-10-20 14:53:59 -0400

    minikube dl scripts makes executable, too
    Signed-off-by: Matt Farina <matt@mattfarina.com>

--- scripts/setupmac.js
+++ scripts/setupmac.js
@@ -1,10 +1,13 @@
 const { https } = require('follow-redirects');
 const fs = require('fs');
+const { spawn } = require('child_process');
 
 const file = fs.createWriteStream("./resources/darwin/minikube");
 const request = https.get("https://github.com/kubernetes/minikube/releases/download/v1.14.0/minikube-darwin-amd64", function(response) {
   response.pipe(file);
 });
 
-// TODO: chmod +x file
 // TODO: handle failed download
+
+// The download needs to be executable to run
+const bat = spawn('chmod', ['+x', './resources/darwin/minikube']);
