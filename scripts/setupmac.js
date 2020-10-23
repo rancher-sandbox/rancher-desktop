acac748f8968b031eeaf6b6e819ba32dcf8ecce8
Chris Olstrom <chris@olstrom.com>
2020-10-23 14:53:34 -0700

    Creates directory before writing to it
    Fixes ENOENT when directory does not exist.

--- scripts/setupmac.js
+++ scripts/setupmac.js
@@ -2,6 +2,8 @@ const { https } = require('follow-redirects');
 const fs = require('fs');
 const { spawn } = require('child_process');
 
+fs.mkdirSync("./resources/darwin", { recursive: true });
+
 const file = fs.createWriteStream("./resources/darwin/minikube");
 const request = https.get("https://github.com/kubernetes/minikube/releases/download/v1.14.0/minikube-darwin-amd64", function(response) {
   response.pipe(file);
