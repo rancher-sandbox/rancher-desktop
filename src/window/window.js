a65562c380f71e91cf855ee8e53630566a791646
Mark Yen <mark.yen@suse.com>
2020-12-17 12:40:19 -0800

    src/window: use app:// protocol when not in DEV
    Because file:// doesn't really work due to security restrictions.

--- src/window/window.js
+++ src/window/window.js
@@ -7,7 +7,7 @@ let url
 if (process.env.NODE_ENV === 'DEV') {
   url = 'http://localhost:8080/';
 } else {
-  url = `file://${process.cwd()}/dist/index.html`;
+  url = 'app:///renderer/index.html';
 }
 
 let window;
