02808e2a647eff681cb052f26a4cbecd679c836d
Eric Promislow <epromislow@suse.com>
2021-01-13 15:03:23 -0800

    Add fields to control kubernetes settings
    - Amount of memory to allocate (in GB)
    - # of CPUs to allocate
    
    Also introduce unit-testing, do TDD against
    the settings component.

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   plugins: ["@babel/plugin-proposal-private-methods",
+    ["babel-plugin-root-import", { "rootPathSuffix": "./", "rootPathPrefix": "@/"}],
     ['module-resolver',
       {
         root: ['.'],
