8276985dd1f085b8aa5b034ad7d4b6f5ce5ad8d9
Eric Promislow <epromislow@suse.com>
2021-01-20 16:15:03 -0800

    Rebase on main: use vue2
    -- Need to add a module-resolver to get the '@' paths to work.

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,15 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   plugins: ["@babel/plugin-proposal-private-methods",
-    ["babel-plugin-root-import", { "rootPathSuffix": "./", "rootPathPrefix": "@/"}]
+    ["babel-plugin-root-import", { "rootPathSuffix": "./", "rootPathPrefix": "@/"}],
+    ['module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@': '.',
+          '~': '.',
+        },
+      }
+    ]
   ]
 }
