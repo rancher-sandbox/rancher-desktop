3f74a60fc24d6ea86c92a19f7145142a09f8b178
Mark Yen <mark.yen@suse.com>
2021-01-25 12:17:40 -0800

    lint: Fix semicolon in helper scripts
    These files get linted on CI but not via `npm run lint`.

--- scripts/generateReleaseList.js
+++ scripts/generateReleaseList.js
@@ -1,12 +1,12 @@
-const { Octokit } = require("@octokit/rest")
-const semverRsort = require('semver/functions/rsort')
-const semver = require('semver')
-const fs = require('fs')
+const { Octokit } = require("@octokit/rest");
+const semverRsort = require('semver/functions/rsort');
+const semver = require('semver');
+const fs = require('fs');
 
 const octokit = new Octokit({
     userAgent: 'mattfarina/rd',
     baseUrl: 'https://api.github.com',
-})
+});
 
 // octokit.request('GET /rate_limit').then(foo => {
 // console.log(foo)
@@ -15,23 +15,23 @@ octokit.paginate(octokit.repos.listReleases, {
     owner: 'kubernetes',
     repo: 'kubernetes',
 }).then(data => {
-    let vers = []
+    let vers = [];
     data.forEach((val) => {
         // Remove prereleases by looking for a -
         if (!val.tag_name.includes('-') && val.tag_name != undefined) {
             if (semver.valid(semver.coerce(val.tag_name))) {
                 if (semver.gte(semver.coerce(val.tag_name), 'v1.13.0')) {
-                    vers.push(val.tag_name)
+                    vers.push(val.tag_name);
                 }
             }
         }
-    })
+    });
 
-    semverRsort(vers, true)
+    semverRsort(vers, true);
 
     try {
-        fs.writeFileSync("./src/generated/versions.json", JSON.stringify(vers))
+        fs.writeFileSync("./src/generated/versions.json", JSON.stringify(vers));
       } catch (err) {
-        console.error(err)
+        console.error(err);
       }
 });
