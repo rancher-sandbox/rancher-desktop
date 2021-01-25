1bf059e6772991e8ae5c0e30c8b25bc435d2ff4d
Eric Promislow <epromislow@suse.com>
2021-01-25 10:11:32 -0800

    don't need both ways to resolve path shortcuts

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,6 @@ module.exports = {
     '@vue/cli-plugin-babel/preset',
   ],
   plugins: ["@babel/plugin-proposal-private-methods",
-    ["babel-plugin-root-import", { "rootPathSuffix": "./", "rootPathPrefix": "@/"}],
     ['module-resolver',
       {
         root: ['.'],
