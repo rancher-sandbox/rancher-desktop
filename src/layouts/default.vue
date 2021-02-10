f5be06891a50ebf821b4ba4daa38d2378c92a77b
Eric Promislow <epromislow@suse.com>
2021-02-10 15:42:59 -0800

    When the system is set to dark mode, use the dark theme
    Only tested on macos
    
    Dashboard currently has three modes: always light, always dark,
    and follow system mode.  This branch implements only the last choice.
    To test:
    
    System Preferences > General > Appearance

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -17,6 +17,17 @@ export default {
     Header,
   },
 
+  head() {
+    // If dark-mode is set to auto (follow system-prefs) this is all we need
+    // In a possible future with a three-way pref
+    // (Always off // Always on // Follow system pref)
+    // the "dark" part will be a dynamic pref.
+    // See https://github.com/rancher/dashboard/blob/3454590ff6a825f7e739356069576fbae4afaebc/layouts/default.vue#L227 for an example
+    return {
+      bodyAttrs: { class: 'theme-dark' },
+    };
+  },
+
   data() {
     return {
       routes: ['/Welcome', '/K8s', '/Troubleshooting'],
@@ -31,6 +42,7 @@ export default {
 .wrapper {
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  background-color: var(--body-bg);
 
   .header {
     grid-column: 1 / 5;
