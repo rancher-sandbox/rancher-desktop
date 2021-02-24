58fce2f5645e54dd427b292a602eb614c69a779f
Mark Yen <mark.yen@suse.com>
2021-02-23 17:24:46 -0800

    build: Fix loading translations
    We neeed to use a loader that parses YAML files, not save it as a blob.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -33,7 +33,8 @@ export default {
       // Add necessary loaders
       webpackConfig.module.rules.push({
         test:   /\.ya?ml(?:\?[a-z0-9=&.]+)?$/,
-        loader: 'file-loader',
+        loader: 'js-yaml-loader',
+        options: { name: '[path][name].[ext]' },
       });
     },
   },
