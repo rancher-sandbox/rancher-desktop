3836c1d43436f9a24ba25d6790d48a4d47b17948
Mark Yen <mark.yen@suse.com>
2021-04-02 16:46:41 -0700

    Create setup script for Windows
    This converts setupmac.js into a generic download-resources.mjs and make it
    work on Windows.  This also drop the follow-redirects dependency in favour
    of using node-fetch (which we already need in the app).  Additionally,
    reduce the callback nesting we have in the same script.
    
    There doesn't seem to be significant reason to convert the script to async
    (considering that this is run as a one-off).
    
    Additionally, make all this get called as a postinstall hook, so that it
    happens automatically upon `npm install`, and the user no longer needs to
    worry about it.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

---
+++ scripts/download-resources.mjs
@@ -0,0 +1,99 @@
+import childProcess from 'child_process';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import util from 'util';
+import fetch from 'node-fetch';
+
+const chmod = util.promisify(fs.chmod);
+
+function exeName(name) {
+  return `${ name }${ os.platform() === 'win32' ? '.exe' : '' }`;
+}
+
+/**
+ * Execute a process and wait for it to finish.
+ * @param command {readonly string} The executable to run.
+ * @param args {readonly string[]} Arguments to the executable.
+ */
+function spawnSync(command, ...args) {
+  /** @type {childProcess.SpawnOptions} */
+  const options = { stdio: 'inherit', windowsHide: true };
+  const { status, signal, error } = childProcess.spawnSync(command, args, options);
+
+  if (error) {
+    throw error;
+  }
+  if (signal !== null && signal !== 'SIGTERM') {
+    throw new Error(`${ command } exited with signal ${ signal }`);
+  }
+  if (status !== null && status !== 0) {
+    throw new Error(`${ command } exited with status ${ status }`);
+  }
+}
+
+/**
+ * Download the given URL, making the result executable
+ * @param url {string} The URL to download
+ * @param path {string} The path to download to
+ */
+async function download(url, path) {
+  console.log(`Downloading ${ url } to ${ path }...`);
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Error downloading ${ url }: ${ response.statusText }`);
+  }
+  const file = fs.createWriteStream(path);
+  const promise = new Promise(resolve => file.on('finish', resolve));
+
+  response.body.pipe(file);
+  await promise;
+  await chmod(path, 0o755);
+}
+
+async function main() {
+  const resourcesDir = path.join(process.cwd(), 'resources', os.platform());
+  const binDir = path.join(resourcesDir, 'bin');
+  /** The platform string, as used by golang / Kubernetes. */
+  const kubePlatform = {
+    darwin: 'darwin',
+    win32:  'windows',
+  }[os.platform()];
+
+  fs.mkdirSync(binDir, { recursive: true });
+
+  // Download minikube, but only for darwin.
+  if (os.platform === 'darwin') {
+    const minikubeURL = `https://github.com/jandubois/minikube/releases/download/k3s1/${ exeName(`minikube-${ kubePlatform }-amd64`) }`;
+    const minikubePath = path.join(resourcesDir, exeName('minikube'));
+
+    await download(minikubeURL, minikubePath);
+  }
+
+  // Download Kubectl
+  const kubeVersion = '1.20.2';
+  const kubectlURL = `https://storage.googleapis.com/kubernetes-release/release/v${ kubeVersion }/bin/${ kubePlatform }/amd64/${ exeName('kubectl') }`;
+  const kubectlPath = path.join(binDir, exeName('kubectl'));
+
+  await download(kubectlURL, kubectlPath);
+
+  // Download Helm. It is a tar.gz file that needs to be expanded and file moved.
+  const helmVersion = '3.5.2';
+  const helmURL = `https://get.helm.sh/helm-v${ helmVersion }-${ kubePlatform }-amd64.tar.gz`;
+  const helmDir = fs.mkdtempSync(path.join(os.tmpdir(), 'rd-helm-'));
+
+  try {
+    const helmPath = path.join(helmDir, 'helm.tar.gz');
+    const helmFinalPath = path.join(binDir, exeName('helm'));
+
+    await download(helmURL, helmPath);
+    spawnSync('tar', '-zxvf', helmPath, '--directory', helmDir);
+    fs.copyFileSync(path.join(helmDir, `${ kubePlatform }-amd64`, exeName('helm')), helmFinalPath);
+    fs.chmodSync(helmFinalPath, 0o755);
+  } finally {
+    fs.rmSync(helmDir, { recursive: true, maxRetries: 10 });
+  }
+}
+
+main();
