58ea07ad8b642012f7b9f4044406d02759c20884
Matt Farina <matt.farina@suse.com>
2021-04-29 17:21:08 -0400

    Setup to sign and notarize
    This requires a valid signing certificate and credentials in
    environment variables to upload to apple for notorization
    
    Closes #165
    Closes #167
    
    Signed-off-by: Matt Farina <matt.farina@suse.com>

---
+++ scripts/notarize.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+    const { electronPlatformName, appOutDir } = context;  
+    if (electronPlatformName !== 'darwin') {
+        return;
+    }
+
+    const appName = context.packager.appInfo.productFilename;
+    const appleId = process.env.APPLEID;
+    
+    if (!appleId) {
+        return;
+    }
+
+    return await notarize({
+        appBundleId: 'io.rancherdesktop.app',
+        appPath: `${appOutDir}/${appName}.app`,
+        appleId,
+        appleIdPassword: process.env.AC_PASSWORD
+    });
+};
