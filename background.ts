67c1ac12ef4b5131e9bd6aef602dd5356762b125
Mark Yen <mark.yen@suse.com>
2021-05-11 12:35:23 -0700

    background.ts: Add support for system certificate authorities
    This makes it possible to handle MITM-style corporate proxies, where custom
    certificate authorities are added to the system certificate store.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- background.ts
+++ background.ts
@@ -4,8 +4,12 @@ import path from 'path';
 import os from 'os';
 import { URL } from 'url';
 import util from 'util';
+
 import Electron from 'electron';
 import _ from 'lodash';
+import MacCA from 'mac-ca';
+import WinCA from 'win-ca';
+
 import * as settings from './src/config/settings';
 import { Tray } from './src/menu/tray.js';
 import window from './src/window/window.js';
@@ -36,6 +40,10 @@ Electron.protocol.registerSchemesAsPrivileged([
 ]);
 
 Electron.app.whenReady().then(async() => {
+  if (os.platform().startsWith('win')) {
+    // Inject the Windows certs.
+    WinCA({ inject: '+' });
+  }
   try {
     tray = new Tray();
   } catch (e) {
@@ -175,6 +183,52 @@ Electron.ipcMain.handle('settings-write', (event, arg: Partial<settings.Settings
   tray?.emit('settings-update', cfg);
 
   Electron.ipcMain.emit('k8s-restart-required');
+});
+
+// Set up certificate handling for system certificates on Windows and macOS
+Electron.app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+  if (error === 'net::ERR_CERT_INVALID') {
+    // If we're getting *this* particular error, it means it's an untrusted cert.
+    // Ask the system store.
+    console.log(`Attempting to check system certificates for ${ url } (${ certificate.subjectName }/${ certificate.fingerprint })`);
+    if (os.platform().startsWith('win')) {
+      const certs: string[] = [];
+
+      WinCA({
+        format: WinCA.der2.pem, ondata: certs, fallback: false
+      });
+      for (const cert of certs) {
+        // For now, just check that the PEM data matches exactly; this is
+        // probably a little more strict than necessary, but avoids issues like
+        // an attacker generating a cert with the same serial.
+        if (cert === certificate.data) {
+          console.log(`Accepting system certificate for ${ certificate.subjectName } (${ certificate.fingerprint })`);
+          // eslint-disable-next-line node/no-callback-literal
+          callback(true);
+
+          return;
+        }
+      }
+    } else if (os.platform() === 'darwin') {
+      for (const cert of MacCA.all(MacCA.der2.pem)) {
+        // For now, just check that the PEM data matches exactly; this is
+        // probably a little more strict than necessary, but avoids issues like
+        // an attacker generating a cert with the same serial.
+        if (cert === certificate.data) {
+          console.log(`Accepting system certificate for ${ certificate.subjectName } (${ certificate.fingerprint })`);
+          // eslint-disable-next-line node/no-callback-literal
+          callback(true);
+
+          return;
+        }
+      }
+    }
+  }
+
+  console.log(`Not handling certificate error ${ error } for ${ url }`);
+
+  // eslint-disable-next-line node/no-callback-literal
+  callback(false);
 });
 
 Electron.ipcMain.on('confirm-do-image-deletion', async(event, imageName, imageID) => {
