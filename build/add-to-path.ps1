bf9814c28a80333f8087cdd992ad531d070eb4db
Mark Yen <mark.yen@suse.com>
2021-05-13 17:08:28 -0700

    NSIS: Add extra nested `resources` to PATH
    The extra layer is because that's where Electron puts things.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- build/add-to-path.ps1
+++ build/add-to-path.ps1
@@ -4,7 +4,7 @@ param($InstallDir)
 
 $TargetUser = [System.EnvironmentVariableTarget]::User
 $path = [System.Environment]::GetEnvironmentVariable('PATH', $TargetUser) -split ';'
-$desiredPath = Join-Path $InstallDir 'resources\win32\bin'
+$desiredPath = Join-Path $InstallDir 'resources\resources\win32\bin'
 if ($path -notcontains $desiredPath) {
   $path += $desiredPath
   [System.Environment]::SetEnvironmentVariable('PATH', ($path -join ';'), $TargetUser)
