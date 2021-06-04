6f2f616fa79cf5caeee2ea30a68b94a1deb9dc7a
Mark Yen <mark.yen@suse.com>
2021-06-04 14:31:45 -0700

    components/BackendProgress: add duration label
    This adds a label showing the current duration, or remaining download size
    if available.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/components/BackendProgress.vue
+++ src/components/BackendProgress.vue
@@ -7,7 +7,8 @@
     <!-- Wrap a label in a container, and make that flex horizontally to let
       it grow as needed without contributing to the width of the nav column. -->
     <div class="label-container">
-      <label :title="progressDetails">{{ progressDetails }}</label>
+      <label class="details" :title="progressDetails">{{ progressDetails }}</label>
+      <label class="duration" :title="progressDuration">{{ progressDuration }}</label>
     </div>
     <Progress
       :indeterminate="progressIndeterminate"
@@ -36,11 +37,15 @@ class BackendProgress extends Vue {
     description?: string;
     /** Time since the description became valid. */
     transitionTime?: Date;
-  } = { current: 0, max: 0 };
+  } = { current: 1, max: 1 };
+
+  progressInterval: ReturnType<typeof setInterval> | undefined;
 
   get progressDetails(): string {
     return this.progress.description || '';
   }
+
+  progressDuration = '';
 
   get progressIndeterminate(): boolean {
     return this.progress.max <= 0;
@@ -50,9 +55,57 @@ class BackendProgress extends Vue {
     return this.progressIndeterminate || this.progress.current < this.progress.max;
   }
 
+  /** Return a string describing the elapsed time or progress. */
+  describeElapsed(since: number): string {
+    if (this.progress.max > 0) {
+      // If we have numbers, give a description about that.
+      const units = ['', 'K', 'M', 'G', 'T'];
+      let remaining = this.progress.max - this.progress.current;
+
+      while (remaining > 512 && units.length > 0) {
+        remaining /= 1024;
+        units.shift();
+      }
+      if (remaining > 0) {
+        remaining = Math.round(remaining);
+      } else {
+        remaining = Math.round(remaining * 10) / 10;
+      }
+
+      return `${ remaining }${ units[0] } left`;
+    }
+    if (!since) {
+      return '';
+    }
+    let remaining = Math.floor((Date.now() - since) / 1000);
+    const parts: [number, string][] = [];
+
+    parts.unshift([remaining % 60, 's']);
+    remaining = Math.floor(remaining / 60);
+    parts.unshift([remaining % 60, 'm']);
+    remaining = Math.floor(remaining / 60);
+    parts.unshift([remaining % 24, 'h']);
+    parts.unshift([Math.floor(remaining / 24), 'd']);
+
+    return parts.filter(([n, s]) => n > 0).map(([n, s]) => `${ n }${ s }`).join('');
+  }
+
   mounted() {
     ipcRenderer.on('k8s-progress', (event, progress) => {
       this.progress = progress;
+      if (this.progress.transitionTime) {
+        if (!this.progressInterval) {
+          const start = this.progress.transitionTime.valueOf();
+
+          this.progressInterval = setInterval(() => {
+            this.progressDuration = this.describeElapsed(start);
+          }, 500);
+        }
+      } else if (this.progressInterval) {
+        clearInterval(this.progressInterval);
+        this.progressInterval = undefined;
+        this.progressDuration = '';
+      }
     });
   }
 }
@@ -70,6 +123,9 @@ export default BackendProgress;
       label {
         overflow: hidden;
         white-space: nowrap;
+      }
+
+      .details {
         text-overflow: ellipsis;
         flex: 1 0px;
         width: 0px;
