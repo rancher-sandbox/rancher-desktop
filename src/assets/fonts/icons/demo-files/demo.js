86ce9f5f633c01dd108d98bc21a78c7c6acc8379
Mark Yen <mark.yen@suse.com>
2021-06-08 17:45:20 -0700

    ESLint: Automated fixes
    This is needed because we pulled in a new version of dashboard deps.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/assets/fonts/icons/demo-files/demo.js
+++ src/assets/fonts/icons/demo-files/demo.js
@@ -1,30 +1,33 @@
 if (!('boxShadow' in document.body.style)) {
-    document.body.setAttribute('class', 'noBoxShadow');
+  document.body.setAttribute('class', 'noBoxShadow');
 }
 
-document.body.addEventListener("click", function(e) {
-    var target = e.target;
-    if (target.tagName === "INPUT" &&
-        target.getAttribute('class').indexOf('liga') === -1) {
-        target.select();
-    }
+document.body.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.tagName === 'INPUT' &&
+        !target.getAttribute('class').includes('liga')) {
+    target.select();
+  }
 });
 
 (function() {
-    var fontSize = document.getElementById('fontSize'),
-        testDrive = document.getElementById('testDrive'),
-        testText = document.getElementById('testText');
-    function updateTest() {
-        testDrive.innerHTML = testText.value || String.fromCharCode(160);
-        if (window.icomoonLiga) {
-            window.icomoonLiga(testDrive);
-        }
+  const fontSize = document.getElementById('fontSize');
+  const testDrive = document.getElementById('testDrive');
+  const testText = document.getElementById('testText');
+
+  function updateTest() {
+    testDrive.innerHTML = testText.value || String.fromCharCode(160);
+    if (window.icomoonLiga) {
+      window.icomoonLiga(testDrive);
     }
-    function updateSize() {
-        testDrive.style.fontSize = fontSize.value + 'px';
-    }
-    fontSize.addEventListener('change', updateSize, false);
-    testText.addEventListener('input', updateTest, false);
-    testText.addEventListener('change', updateTest, false);
-    updateSize();
+  }
+
+  function updateSize() {
+    testDrive.style.fontSize = `${ fontSize.value }px`;
+  }
+  fontSize.addEventListener('change', updateSize, false);
+  testText.addEventListener('input', updateTest, false);
+  testText.addEventListener('change', updateTest, false);
+  updateSize();
 }());
