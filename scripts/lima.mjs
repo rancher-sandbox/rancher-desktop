d340ee8788744fddc92626edc64763c55b8d52f4
Mark Yen <mark.yen@suse.com>
2021-07-02 09:56:27 -0700

    Scripts/Lima: Verify checksum for alpine-lima image.
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- scripts/lima.mjs
+++ scripts/lima.mjs
@@ -43,8 +43,11 @@ async function getLima() {
 async function getAlpineLima() {
   const url = `${ alpineLimaRepo }/releases/download/${ alpineLimaTag }/alpine-lima-${ alpineLimaEdition }-${ alpineLimaVersion }-x86_64.iso`;
   const destPath = path.join(process.cwd(), 'resources', os.platform(), `alpline-lima-${ alpineLimaTag }-${ alpineLimaEdition }-${ alpineLimaVersion }.iso`);
+  const expectedChecksum = (await getResource(`${ url }.sha512sum`)).split(/\s+/)[0];
 
-  await download(url, destPath, { access: fs.constants.W_OK });
+  await download(url, destPath, {
+    expectedChecksum, checksumAlgorithm: 'sha512', access: fs.constants.W_OK
+  });
 }
 
 export default function run() {
