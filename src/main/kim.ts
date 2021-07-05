4871316429752406674f08a55e2873d697820160
Mark Yen <mark.yen@suse.com>
2021-07-05 14:03:48 -0700

    src/main/kim: Move most of the code into setupKim()
    This ensures that we only trigger them once we've progressed to the point
    where they're necessary.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/main/kim.ts
+++ src/main/kim.ts
@@ -13,152 +13,154 @@ import Logging from '@/utils/logging';
 import window from '@/window/window.js';
 
 const console = new Console(Logging.kim.stream);
-const imageManager = new Kim();
+
+let imageManager: Kim;
 let lastBuildDirectory = '';
 let mountCount = 0;
 
-interface KimImage {
+export function setupKim() {
+  imageManager = imageManager ?? new Kim();
+
+  interface KimImage {
     imageName: string,
     tag: string,
     imageID: string,
     size: string
-}
-imageManager.on('readiness-changed', (state: boolean) => {
-  window.send('images-check-state', state);
-});
-imageManager.on('kim-process-output', (data: string, isStderr: boolean) => {
-  window.send('kim-process-output', data, isStderr);
-});
-
-function onImagesChanged(images: KimImage[]) {
-  window.send('images-changed', images);
-}
-Electron.ipcMain.handle('images-mounted', (_, mounted: boolean) => {
-  mountCount += mounted ? 1 : -1;
-  if (mountCount < 1) {
-    imageManager.removeListener('images-changed', onImagesChanged);
-  } else if (mountCount === 1) {
-    imageManager.on('images-changed', onImagesChanged);
   }
-
-  return imageManager.listImages();
-});
-
-Electron.ipcMain.on('confirm-do-image-deletion', async(event, imageName, imageID) => {
-  const choice = Electron.dialog.showMessageBoxSync({
-    message:   `Delete image ${ imageName }?`,
-    type:      'warning',
-    buttons:   ['Yes', 'No'],
-    defaultId: 1,
-    title:     `Delete image ${ imageName }`,
-    cancelId:  1
+  imageManager.on('readiness-changed', (state: boolean) => {
+    window.send('images-check-state', state);
+  });
+  imageManager.on('kim-process-output', (data: string, isStderr: boolean) => {
+    window.send('kim-process-output', data, isStderr);
   });
 
-  if (choice === 0) {
-    try {
-      const maxNumAttempts = 2;
-      // On macOS a second attempt is needed to actually delete the image.
-      // Probably due to a timing issue on the server part of kim, but not determined why.
-      // Leave this in for windows in case it can happen there too.
-      let i = 0;
+  function onImagesChanged(images: KimImage[]) {
+    window.send('images-changed', images);
+  }
+  Electron.ipcMain.handle('images-mounted', (_, mounted: boolean) => {
+    mountCount += mounted ? 1 : -1;
+    if (mountCount < 1) {
+      imageManager.removeListener('images-changed', onImagesChanged);
+    } else if (mountCount === 1) {
+      imageManager.on('images-changed', onImagesChanged);
+    }
 
-      for (i = 0; i < maxNumAttempts; i++) {
-        await imageManager.deleteImage(imageID);
-        await imageManager.refreshImages();
-        if (!imageManager.listImages().some(image => image.imageID === imageID)) {
-          break;
+    return imageManager.listImages();
+  });
+
+  Electron.ipcMain.on('confirm-do-image-deletion', async(event, imageName, imageID) => {
+    const choice = Electron.dialog.showMessageBoxSync({
+      message:   `Delete image ${ imageName }?`,
+      type:      'warning',
+      buttons:   ['Yes', 'No'],
+      defaultId: 1,
+      title:     `Delete image ${ imageName }`,
+      cancelId:  1
+    });
+
+    if (choice === 0) {
+      try {
+        const maxNumAttempts = 2;
+        // On macOS a second attempt is needed to actually delete the image.
+        // Probably due to a timing issue on the server part of kim, but not determined why.
+        // Leave this in for windows in case it can happen there too.
+        let i = 0;
+
+        for (i = 0; i < maxNumAttempts; i++) {
+          await imageManager.deleteImage(imageID);
+          await imageManager.refreshImages();
+          if (!imageManager.listImages().some(image => image.imageID === imageID)) {
+            break;
+          }
+          await util.promisify(setTimeout)(500);
         }
-        await util.promisify(setTimeout)(500);
+        if (i === maxNumAttempts) {
+          console.log(`Failed to delete ${ imageID } in ${ maxNumAttempts } tries`);
+        }
+        event.reply('kim-process-ended', 0);
+      } catch (err) {
+        Electron.dialog.showMessageBox({
+          message: `Error trying to delete image ${ imageName } (${ imageID }):\n\n ${ err.stderr } `,
+          type:    'error'
+        });
       }
-      if (i === maxNumAttempts) {
-        console.log(`Failed to delete ${ imageID } in ${ maxNumAttempts } tries`);
-      }
-      event.reply('kim-process-ended', 0);
+    }
+  });
+
+  Electron.ipcMain.on('do-image-build', async(event, taggedImageName: string) => {
+    const options: any = {
+      title:      'Pick the build directory',
+      properties: ['openFile'],
+      message:    'Please select the Dockerfile to use (could have a different name)'
+    };
+
+    if (lastBuildDirectory) {
+      options.defaultPath = lastBuildDirectory;
+    }
+    const results = Electron.dialog.showOpenDialogSync(options);
+
+    if (results === undefined) {
+      return;
+    }
+    if (results.length !== 1) {
+      console.log(`Expecting exactly one result, got ${ results.join(', ') }`);
+
+      return;
+    }
+    const pathParts = path.parse(results[0]);
+    let code;
+
+    lastBuildDirectory = pathParts.dir;
+    try {
+      code = (await imageManager.buildImage(lastBuildDirectory, pathParts.base, taggedImageName)).code;
+      await imageManager.refreshImages();
     } catch (err) {
+      code = err.code;
       Electron.dialog.showMessageBox({
-        message: `Error trying to delete image ${ imageName } (${ imageID }):\n\n ${ err.stderr } `,
+        message: `Error trying to build ${ taggedImageName }:\n\n ${ err.stderr } `,
         type:    'error'
       });
     }
-  }
-});
+    event.reply('kim-process-ended', code);
+  });
 
-Electron.ipcMain.on('do-image-build', async(event, taggedImageName: string) => {
-  const options: any = {
-    title:      'Pick the build directory',
-    properties: ['openFile'],
-    message:    'Please select the Dockerfile to use (could have a different name)'
-  };
+  Electron.ipcMain.on('do-image-pull', async(event, imageName) => {
+    let taggedImageName = imageName;
+    let code;
 
-  if (lastBuildDirectory) {
-    options.defaultPath = lastBuildDirectory;
-  }
-  const results = Electron.dialog.showOpenDialogSync(options);
+    if (!imageName.includes(':')) {
+      taggedImageName += ':latest';
+    }
+    try {
+      code = (await imageManager.pullImage(taggedImageName)).code;
+      await imageManager.refreshImages();
+    } catch (err) {
+      code = err.code;
+      Electron.dialog.showMessageBox({
+        message: `Error trying to pull ${ taggedImageName }:\n\n ${ err.stderr } `,
+        type:    'error'
+      });
+    }
+    event.reply('kim-process-ended', code);
+  });
 
-  if (results === undefined) {
-    return;
-  }
-  if (results.length !== 1) {
-    console.log(`Expecting exactly one result, got ${ results.join(', ') }`);
+  Electron.ipcMain.on('do-image-push', async(event, imageName, imageID, tag) => {
+    const taggedImageName = `${ imageName }:${ tag }`;
+    let code;
 
-    return;
-  }
-  const pathParts = path.parse(results[0]);
-  let code;
+    try {
+      code = (await imageManager.pushImage(taggedImageName)).code;
+    } catch (err) {
+      code = err.code;
+      Electron.dialog.showMessageBox({
+        message: `Error trying to push ${ taggedImageName }:\n\n ${ err.stderr } `,
+        type:    'error'
+      });
+    }
+    event.reply('kim-process-ended', code);
+  });
 
-  lastBuildDirectory = pathParts.dir;
-  try {
-    code = (await imageManager.buildImage(lastBuildDirectory, pathParts.base, taggedImageName)).code;
-    await imageManager.refreshImages();
-  } catch (err) {
-    code = err.code;
-    Electron.dialog.showMessageBox({
-      message: `Error trying to build ${ taggedImageName }:\n\n ${ err.stderr } `,
-      type:    'error'
-    });
-  }
-  event.reply('kim-process-ended', code);
-});
-
-Electron.ipcMain.on('do-image-pull', async(event, imageName) => {
-  let taggedImageName = imageName;
-  let code;
-
-  if (!imageName.includes(':')) {
-    taggedImageName += ':latest';
-  }
-  try {
-    code = (await imageManager.pullImage(taggedImageName)).code;
-    await imageManager.refreshImages();
-  } catch (err) {
-    code = err.code;
-    Electron.dialog.showMessageBox({
-      message: `Error trying to pull ${ taggedImageName }:\n\n ${ err.stderr } `,
-      type:    'error'
-    });
-  }
-  event.reply('kim-process-ended', code);
-});
-
-Electron.ipcMain.on('do-image-push', async(event, imageName, imageID, tag) => {
-  const taggedImageName = `${ imageName }:${ tag }`;
-  let code;
-
-  try {
-    code = (await imageManager.pushImage(taggedImageName)).code;
-  } catch (err) {
-    code = err.code;
-    Electron.dialog.showMessageBox({
-      message: `Error trying to push ${ taggedImageName }:\n\n ${ err.stderr } `,
-      type:    'error'
-    });
-  }
-  event.reply('kim-process-ended', code);
-});
-
-Electron.ipcMain.handle('images-check-state', () => {
-  return imageManager.isReady;
-});
-
-export function setupKim() {
+  Electron.ipcMain.handle('images-check-state', () => {
+    return imageManager.isReady;
+  });
 }
