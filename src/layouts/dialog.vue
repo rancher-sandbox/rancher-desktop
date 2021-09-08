5c5221bc6a7c17d7457086c8c3a07f3eb8de2a19
Mark Yen <mark.yen@suse.com>
2021-09-08 15:00:13 -0700

    layouts/dialog: dark mode support.
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/layouts/dialog.vue
+++ src/layouts/dialog.vue
@@ -9,6 +9,14 @@
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
+  head() {
+    // If dark-mode is set to auto (follow system-prefs) this is all we need
+    // In a possible future with a three-way pref
+    // (Always off // Always on // Follow system pref)
+    // the "dark" part will be a dynamic pref.
+    // See https://github.com/rancher/dashboard/blob/3454590ff6a825f7e739356069576fbae4afaebc/layouts/default.vue#L227 for an example
+    return { bodyAttrs: { class: 'theme-dark' } };
+  },
   mounted() {
     const wrapper = this.$refs.wrapper as HTMLDialogElement;
 
@@ -25,7 +33,9 @@ export default Vue.extend({
 @import "@/assets/styles/app.scss";
 
 .wrapper {
+  background-color: var(--body-bg);
   border: none;
+  color: var(--body-text);
 }
 
 </style>
