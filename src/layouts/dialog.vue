af07a730b011caa313119fad447ba2b236a0a5be
Mark Yen <mark.yen@suse.com>
2021-09-08 14:06:47 -0700

    Layouts/dialog: Resize to dialog to fit on show.
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/layouts/dialog.vue
+++ src/layouts/dialog.vue
@@ -1,22 +1,30 @@
 <!-- This layout is used dialog boxes that do not want navigation -->
 
 <template>
-  <dialog class="wrapper" open>
+  <dialog ref="wrapper" class="wrapper" open>
     <Nuxt class="body" />
   </dialog>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  mounted() {
+    const wrapper = this.$refs.wrapper as HTMLDialogElement;
+
+    // Dynamically resize the window to fit the contents.
+    // We need a bit extra on the width to ensure we don't get scroll bars.
+    window.resizeBy(
+      wrapper.offsetWidth - document.documentElement.offsetWidth + 14,
+      wrapper.offsetHeight - document.documentElement.offsetHeight);
+  }
+});
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/styles/app.scss";
 
 .wrapper {
-  display: grid;
-  width: 100vw;
-  height: 100vh;
   border: none;
 }
 
