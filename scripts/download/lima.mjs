840ffada1796f4d15fdbb4251845957e4ac7db74
Jan Dubois <jan.dubois@suse.com>
2021-09-17 11:47:41 -0700

    Bump lima-and-qemu to v1.5
    Includes lima 0.6.4 plus the iptables watcher for port forwarding.
    
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- scripts/download/lima.mjs
+++ scripts/download/lima.mjs
@@ -7,7 +7,7 @@ import path from 'path';
 import { download, getResource } from '../lib/download.mjs';
 
 const limaRepo = 'https://github.com/rancher-sandbox/lima-and-qemu';
-const limaTag = 'v1.3';
+const limaTag = 'v1.5';
 
 const alpineLimaRepo = 'https://github.com/lima-vm/alpine-lima';
 const alpineLimaTag = 'v0.1.4';
