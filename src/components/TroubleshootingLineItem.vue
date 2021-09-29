d238aba1ff03510166720aa9a1161a453010d0a4
Phillip Rak <rak.phillip@gmail.com>
2021-09-29 08:14:43 -0700

    Add component for displaying troubleshooting line items
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ src/components/TroubleshootingLineItem.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+
+import Vue from 'vue';
+export default Vue.extend({});
+
+</script>
+
+<template>
+  <div class="troubleshooting-line-item">
+    <div class="description">
+      <h3>
+        <slot name="title">
+        </slot>
+      </h3>
+      <span>
+        <slot name="description">
+        </slot>
+      </span>
+    </div>
+    <slot>
+    </slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .troubleshooting-line-item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-top: 24px;
+    margin-bottom: 24px;
+  }
+
+  .description {
+    margin-right: 24px;
+  }
+</style>
