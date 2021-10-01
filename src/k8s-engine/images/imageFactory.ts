b403e5c6afe8e2851b0e2260ddf1f0439605a6e8
Eric Promislow <epromislow@suse.com>
2021-10-01 11:12:48 -0700

    Remove the Kim ImageProcessor
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/k8s-engine/images/imageFactory.ts
+++ src/k8s-engine/images/imageFactory.ts
@@ -1,12 +1,18 @@
 import { ImageProcessor } from '@/k8s-engine/images/imageProcessor';
-import KimImageProcessor from '@/k8s-engine/images/kimImageProcessor';
 import NerdctlImageProcessor from '@/k8s-engine/images/nerdctlImageProcessor';
 import * as K8s from '~/k8s-engine/k8s';
 
+/**
+ * Currently there's only one image processor.
+ * But at one point, when we transitioned from kim to nerdctl, there were two.
+ * And there might be new ones in the future, so the only changes are adding the new
+ * module and three lines to this file (one for the import, two for the switch stmt).
+ * @param processorName
+ * @param k8sManager
+ */
+
 export default function createImageProcessor(processorName: string, k8sManager: K8s.KubernetesBackend): ImageProcessor {
   switch (processorName) {
-  case 'kim':
-    return new KimImageProcessor(k8sManager);
   case 'nerdctl':
     return new NerdctlImageProcessor(k8sManager);
   default:
