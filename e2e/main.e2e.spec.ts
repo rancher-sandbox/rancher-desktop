e15d1d8168afa269078d0d3c9fe2fb1ebc2007b9
evertonlperes <evertonluperes@gmail.com>
2021-10-07 13:02:08 -0600

    Debug: Added CI check for FirstRun Page
    Signed-off-by: evertonlperes <evertonluperes@gmail.com>

--- e2e/main.e2e.spec.ts
+++ e2e/main.e2e.spec.ts
@@ -4,6 +4,7 @@ import os from 'os';
 import { Application, SpectronClient } from 'spectron';
 import { BrowserWindow } from 'electron';
 import NavBarPage from './pages/navbar';
+import FirstRunPage from './pages/firstrun';
 import GeneralPage from './pages/general';
 import KubernetesPage from './pages/kubernetes';
 import PortForwardingPage from './pages/portforwarding';
@@ -20,6 +21,7 @@ describe('Rancher Desktop', () => {
   let client: SpectronClient;
   let browserWindow: BrowserWindow;
   let navBarPage: NavBarPage;
+  let firstRunPage: FirstRunPage;
 
   beforeAll(async() => {
     app = new Application({
@@ -40,6 +42,7 @@ describe('Rancher Desktop', () => {
     client = app.client;
     browserWindow = app.browserWindow;
     navBarPage = new NavBarPage(app);
+    firstRunPage = new FirstRunPage(app);
   });
 
   afterAll(async() => {
@@ -48,23 +51,37 @@ describe('Rancher Desktop', () => {
     }
   });
 
-  it('should open the main window', async() => {
-    await client.waitUntilWindowLoaded();
-    // typescript doesn't see a value of await in below statement, but
-    // removing await makes the statement not wait till the app window loads
-    // Also, Alternate ways to get the app window title, for example using client
-    // didn't work. So, Leaving 'await' for now. We may need to review this and
-    // fix this in future.
-    await app.client.saveScreenshot('./open_window.png');
-    const title = await browserWindow.getTitle();
+  if (process.env.CI) {
+    it('should open k8s settings page - First Run', async() => {
+      console.log('Into the IF');
+      await client.waitUntilWindowLoaded();
 
-    expect(title).toBe('Rancher Desktop');
-  });
+      const k8sSettings = await firstRunPage.getK8sVersionHeaderText();
+      const acceptBtnSelector = '[data-test="accept-btn"]';
+
+      expect(k8sSettings).toBe('Welcome to Rancher Desktop');
+
+      // It closes k8s settings page
+      (await client.$(acceptBtnSelector)).waitForExist();
+      (await client.$(acceptBtnSelector)).click();
+    });
+  } else {
+    it('should open General the main window', async() => {
+      await client.waitUntilWindowLoaded();
+      const title = await browserWindow.getTitle();
+
+      await client.waitUntilTextExists(title, 'Rancher Desktop', 10000);
+
+      await app.client.saveScreenshot('./open_window.png'); // Debug CI only
+
+      expect(title).toBe('Rancher Desktop');
+    });
+  }
 
   it('should display welcome message in general tab', async() => {
     const generalPage = await navBarPage.getGeneralPage();
 
-    await app.client.saveScreenshot('./general.png');
+    await app.client.saveScreenshot('./general.png'); // Debug CI only
     expect(generalPage).not.toBeNull();
     expect(await generalPage?.getTitle()).toBe('Welcome to Rancher Desktop');
   });
@@ -72,7 +89,7 @@ describe('Rancher Desktop', () => {
   it('should switch to Kubernetes Settings tab', async() => {
     const kubernetesPage = await navBarPage.getKubernetesPage();
 
-    await app.client.saveScreenshot('./kubernetes_settings.png');
+    await app.client.saveScreenshot('./kubernetes_settings.png'); // Debug CI only
     expect(kubernetesPage).not.toBeNull();
     expect(await kubernetesPage?.getResetKubernetesButtonText()).toBe('Reset Kubernetes');
   });
@@ -80,7 +97,7 @@ describe('Rancher Desktop', () => {
   it('should switch to Port Forwarding tab', async() => {
     const portForwardingPage = await navBarPage.getPortForwardingPage();
 
-    await app.client.saveScreenshot('./forwarding.png');
+    await app.client.saveScreenshot('./forwarding.png'); // Debug CI only
     if (os.platform().startsWith('win')) {
       expect(portForwardingPage).not.toBeNull();
     } else {
@@ -91,14 +108,14 @@ describe('Rancher Desktop', () => {
   it('should switch to Images tab', async() => {
     const imagesPage = await navBarPage.getImagesPage();
 
-    await app.client.saveScreenshot('./image_tab.png');
+    await app.client.saveScreenshot('./image_tab.png'); // Debug CI only
     expect(imagesPage).not.toBeNull();
   });
 
   it('should switch to Troubleshooting tab', async() => {
     const troubleShootingPage = await navBarPage.getTroubleshootingPage();
 
-    await app.client.saveScreenshot('./troubleshooting.png');
+    await app.client.saveScreenshot('./troubleshooting.png'); // Debug CI only
     expect(troubleShootingPage).not.toBeNull();
     expect(await troubleShootingPage?.getFactoryResetButtonText()).toBe('Factory Reset');
   });
