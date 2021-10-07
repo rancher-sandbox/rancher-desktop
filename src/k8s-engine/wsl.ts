fd87ad8bdc438646933e18b3419f6050d39daa43
Eric Promislow <epromislow@suse.com>
2021-10-07 10:46:03 -0700

    Tell WSL about the VM_STARTED state, parity with lima
    - In practice, the time delta between this event and
      STARTED so short that it isn't noticeable to the eye
    
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/k8s-engine/wsl.ts
+++ src/k8s-engine/wsl.ts
@@ -773,6 +773,7 @@ export default class WSLBackend extends events.EventEmitter implements K8s.Kuber
           }
         });
 
+        this.setState(K8s.State.VM_STARTED);
         await this.progressTracker.action('Starting guest agent', 100, this.launchAgent());
 
         await this.progressTracker.action(
