58936ffc23cd3c651b8fcf39d7620d5b4c4e4b25
Phillip Rak <rak.phillip@gmail.com>
2021-10-14 12:35:00 -0700

    Create placeholder components for pull & build actions
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ src/components/ImageAddButtonBuild.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="image-input">
+    NOT FAIL
+    <!-- <labeled-input
+      id="imageToPull"
+      v-model="imageToPull"
+      type="text"
+      :disabled="!~imageToPullTextFieldIsDisabled"
+      :placeholder="t('images.manager.input.pull.placeholder')"
+      :label="t('images.manager.input.pull.label')"
+    />
+    <button
+      class="btn role-primary btn-large"
+      :disabled="imageToPullButtonDisabled"
+      @click="doPullAnImage"
+    >
+      {{ t('images.manager.input.pull.button') }}
+    </button> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'image-add-button-build',
+
+  data() {
+    return { imageToPull: '' };
+  }
+};
+</script>
---
+++ src/components/ImageAddButtonPull.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="image-input">
+    <labeled-input
+      id="imageToPull"
+      v-model="imageToPull"
+      type="text"
+      :disabled="!~imageToPullTextFieldIsDisabled"
+      :placeholder="t('images.manager.input.pull.placeholder')"
+      :label="t('images.manager.input.pull.label')"
+    />
+    <button
+      class="btn role-primary btn-large"
+      :disabled="imageToPullButtonDisabled"
+      @click="doPullAnImage"
+    >
+      {{ t('images.manager.input.pull.button') }}
+    </button>
+  </div>
+</template>
+
+<script>
+import LabeledInput from '@/components/form/LabeledInput.vue';
+export default {
+  name: 'image-add-button-pull',
+
+  components: { LabeledInput },
+
+  props: {
+    currentCommand: {
+      type:    String,
+      default: ''
+    },
+    keepImageManagerOutputWindowOpen: {
+      type:    Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return { imageToPull: '' };
+  },
+
+  computed: {
+    imageToPullButtonDisabled() {
+      return this.imageToPullTextFieldIsDisabled || !this.imageToPull;
+    },
+    imageToPullTextFieldIsDisabled() {
+      return this.currentCommand || this.keepImageManagerOutputWindowOpen;
+    },
+  },
+
+  methods: {
+    doPullImage() {
+      this.$emit('click');
+    }
+  }
+};
+</script>
