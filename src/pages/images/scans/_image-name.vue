5aec51d07031b3a5e029ddf2c3d8fb4a15fc8d50
Phillip Rak <rak.phillip@gmail.com>
2021-10-21 14:25:49 -0700

    Update Image Name page to display loading indicator
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/pages/images/scans/_image-name.vue
+++ src/pages/images/scans/_image-name.vue
@@ -1,10 +1,37 @@
 <template>
-  <h1>NOT FAIL</h1>
+  <div>
+    <div v-if="showImageOutput">
+      <banner
+        v-if="!imageManagerProcessIsFinished"
+      >
+        <loading-indicator>
+          Scanning {{ image }}...
+        </loading-indicator>
+      </banner>
+    </div>
+  </div>
 </template>
 
 <script>
+import LoadingIndicator from '@/components/LoadingIndicator.vue';
+import Banner from '@/components/Banner.vue';
+
 export default {
   name: 'images-scan-details',
+
+  components: {
+    LoadingIndicator,
+    Banner
+  },
+
+  data() {
+    return {
+      image:                         this.$route.params.image,
+      showImageOutput:               true,
+      imageManagerProcessIsFinished: false
+    };
+  },
+
   mounted() {
     this.$store.dispatch(
       'page/setHeader',
