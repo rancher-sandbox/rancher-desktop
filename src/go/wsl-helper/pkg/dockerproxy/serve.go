919aea58108798aadd122a026581b1b3a9444579
Mark Yen <mark.yen@suse.com>
2021-11-03 14:05:30 -0700

    WSL-helper: determine the API version to use.
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/go/wsl-helper/pkg/dockerproxy/serve.go
+++ src/go/wsl-helper/pkg/dockerproxy/serve.go
@@ -27,6 +27,8 @@ import (
 	"net/http/httputil"
 	"os"
 	"os/signal"
+	"regexp"
+	"strings"
 
 	"github.com/sirupsen/logrus"
 
@@ -68,21 +70,38 @@ func Serve(endpoint string, dialer func() (net.Conn, error)) error {
 
 	logWriter := logrus.StandardLogger().Writer()
 	defer logWriter.Close()
+	munger := newRequestMunger()
 	proxy := &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
 			logrus.WithField("request", req).
+				WithField("headers", req.Header).
 				WithField("url", req.URL).
 				Debug("got proxy request")
 			// The incoming URL is relative (to the root of the server; we need to
 			// add scheme and host ("http://proxy.invalid/") to it.
 			req.URL.Scheme = "http"
 			req.URL.Host = "proxy.invalid"
+
+			originalReq := *req
+			originalURL := *req.URL
+			originalReq.URL = &originalURL
+			err := munger.Munge(req)
+			if err != nil {
+				logrus.WithError(err).
+					WithField("original request", originalReq).
+					WithField("modified request", req).
+					Error("could not munge request")
+			}
 		},
 		Transport: &http.Transport{
 			Dial: func(string, string) (net.Conn, error) {
 				return dialer()
 			},
 			DisableCompression: true, // for debugging
+		},
+		ModifyResponse: func(resp *http.Response) error {
+			logrus.WithField("response", resp).Debug("got proxy response")
+			return nil
 		},
 		ErrorLog: log.New(logWriter, "", 0),
 	}
@@ -93,4 +112,67 @@ func Serve(endpoint string, dialer func() (net.Conn, error)) error {
 	}
 
 	return nil
+}
+
+// requestMunger is used to modify the incoming http.Request as required.
+type requestMunger struct {
+	// apiDetectPattern is used to detect the API version request path prefix.
+	apiDetectPattern *regexp.Regexp
+}
+
+// newRequestMunger initializes a new requestMunger.
+func newRequestMunger() *requestMunger {
+	apiDetectPattern, err := regexp.Compile(`^v[0-9.]+$`)
+	if err != nil {
+		logrus.WithError(err).Error("could not compile path pattern")
+		panic(err)
+	}
+	return &requestMunger{
+		apiDetectPattern: apiDetectPattern,
+	}
+}
+
+// Munge a given request; it is modified in-place.
+func (m *requestMunger) Munge(req *http.Request) error {
+	apiVersion, requestPath := m.detectAPIVersion(req.URL.Path)
+	if apiVersion == "" {
+		// Special case for /_ping
+		return nil
+	}
+	logrus.WithField("path", requestPath).Debug("foo")
+
+	return nil
+}
+
+// detectAPIVersion parses an incoming HTTP request to determine the Docker API
+// version it expects to use, returning the API version (as a string, such as
+// "v1.41") plus the API path (e.g. "/images/json").  The API path will always
+// be prefixed by a slash.  If the request should not be munged, the API version
+// will be returned as an empty string.
+func (m *requestMunger) detectAPIVersion(reqPath string) (string, string) {
+	cleanedReqPath := strings.TrimPrefix(reqPath, "/")
+	slashIndex := strings.Index(cleanedReqPath, "/")
+	if slashIndex < 0 {
+		slashIndex = len(cleanedReqPath)
+	}
+	versionString := cleanedReqPath[:slashIndex]
+	unversionedPath := cleanedReqPath[slashIndex:]
+	if versionString == "_ping" {
+		// Do not munge /_ping; the client looks at the returned `Api-Version`
+		// header to negotiate the API to use.
+		return "", reqPath
+	}
+
+	if !m.apiDetectPattern.MatchString(versionString) {
+		// The first word isn't an API version; use the default version.
+		versionString = defaultDockerVersion
+		unversionedPath = reqPath
+	}
+
+	logrus.WithField("api-version", versionString).
+		WithField("request path", unversionedPath).
+		WithField("input path", reqPath).
+		Debug("parsed request version")
+
+	return versionString, unversionedPath
 }
