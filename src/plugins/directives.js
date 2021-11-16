d6112326991c37f87199b4dcb11a67187b7a4bb9
Phillip Rak <rak.phillip@gmail.com>
2021-11-16 15:57:24 -0700

    Remove other instances for v-focus
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/plugins/directives.js
+++ src/plugins/directives.js
@@ -13,29 +13,7 @@ Vue.directive('focus', {
 const getElement = (vnode) => {
   const { componentInstance, componentOptions: { tag } } = vnode;
 
-  if (tag === 'LabeledInput') {
+  if (tag === 'labeled-input') {
     return componentInstance.$refs.value;
-  }
-
-  if (tag === 'LabeledSelect') {
-    componentInstance.shouldOpen = false;
-
-    return componentInstance.$refs['select-input'].$refs.search;
-  }
-
-  if (tag === 'SelectPrincipal') {
-    const labeledSelect = componentInstance.$refs['labeled-select'];
-
-    labeledSelect.shouldOpen = false;
-
-    return labeledSelect.$refs['select-input'].$refs.search;
-  }
-
-  if (tag === 'TextAreaAutoGrow') {
-    return componentInstance.$refs.ta;
-  }
-
-  if (tag === 'Password') {
-    return componentInstance.$refs.input.$refs.value;
   }
 };
