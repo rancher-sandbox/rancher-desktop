f899aaeac77d7ac68c843093e8a3ee46979c2bdf
Adam Pickering <adamkpickering@gmail.com>
2021-12-02 11:34:09 -0700

    Add LimaConfiguration.networks only if on darwin
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/k8s-engine/lima.ts
+++ src/k8s-engine/lima.ts
@@ -455,7 +455,7 @@ export default class LimaBackend extends events.EventEmitter implements K8s.Kube
   protected async updateConfig(desiredVersion: semver.SemVer) {
     const currentConfig = await this.currentConfig;
     const baseConfig: Partial<LimaConfiguration> = currentConfig || {};
-    const config: LimaConfiguration = merge(baseConfig, DEFAULT_CONFIG as LimaConfiguration, {
+    let config: LimaConfiguration = merge(baseConfig, DEFAULT_CONFIG as LimaConfiguration, {
       images: [{
         location: this.baseDiskImage,
         arch:     this.arch,
@@ -470,6 +470,15 @@ export default class LimaBackend extends events.EventEmitter implements K8s.Kube
       ssh: { localPort: await this.sshPort },
       k3s: { version: desiredVersion.version },
     });
+
+    if (os.platform() === 'darwin') {
+      config.networks = [
+        {
+          'lima': 'shared',
+          'interface': 'rd0',
+        },
+      ]
+    }
 
     this.updateConfigPortForwards(config);
     if (currentConfig) {
