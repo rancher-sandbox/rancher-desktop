bfd1b9c20a27bc64f9de4c35831c3f9d59ec5bb9
Everton Peres <37411123+evertonlperes@users.noreply.github.com>
2022-01-10 16:40:19 -0300

    Adding progress bar waitFor() in main.e2e.spec.ts
    Signed-off-by: Everton Peres <37411123+evertonlperes@users.noreply.github.com>

--- e2e/main.e2e.spec.ts
+++ e2e/main.e2e.spec.ts
@@ -50,7 +50,10 @@ test.describe.serial('Main App Test', () => {
   test('should start loading the background services and hide progress bar', async() => {
     const progressBarSelector = page.locator('.progress');
 
-    await progressBarSelector.waitFor({ state: 'detached', timeout: 120_000 });
+    // Wait until progress bar show up. It takes roughly ~60s to start in CI
+    await progressBarSelector.waitFor({ state: 'visible', timeout: 200_000 });
+    // Wait until progress bar be detached. With that we can make sure the services were started
+    await progressBarSelector.waitFor({ state: 'detached', timeout: 300_000 });
     await expect(progressBarSelector).toBeHidden();
   });
 
