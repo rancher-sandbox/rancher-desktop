dee166e1b4845639c10cdb097285fa1517b6cc46
Phillip Rak <rak.phillip@gmail.com>
2022-01-10 11:58:11 -0700

    Create a new dashboard page
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ src/pages/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    class="btn role-primary"
+    @click="openDashboard"
+  >
+    Open Dashboard
+  </button>
+</template>
+
+<script>
+import { ipcRenderer } from 'electron';
+
+export default {
+  name: 'dashboard',
+
+  methods: {
+    openDashboard() {
+      console.debug('NOT FAIL');
+      ipcRenderer.send('ok:dashboard');
+    }
+  }
+};
+</script>
