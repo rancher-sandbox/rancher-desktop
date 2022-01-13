842dc2660701f85e05f1f0df436e388042fab0ba
Phillip Rak <rak.phillip@gmail.com>
2022-01-13 14:21:45 -0700

    Add a button to header for closing dashboard
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,28 @@
+<script>
+import { ipcRenderer } from 'electron';
+
+export default {
+  name:    'header',
+  methods: {
+    closeDashboard() {
+      ipcRenderer.send('dashboard-close');
+    }
+  }
+};
+</script>
+
 <template>
   <header>
     <div alt="Rancher Desktop" class="logo">
       <img src="@/assets/images/logo.svg">
     </div>
+    <button
+      class="btn role-primary btn-sm"
+      type="button"
+      @click="closeDashboard"
+    >
+      Close Dashbaord
+    </button>
   </header>
 </template>
 
@@ -11,12 +31,12 @@
     height:  var(--header-height);
     background-color: var(--header-bg);
     position: relative;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
 
     .logo {
     height: 40px;
-    position: absolute;
-    top: 8px;
-    left: 0;
     z-index: 2;
 
         img {
