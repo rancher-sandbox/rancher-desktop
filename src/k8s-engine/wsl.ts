140e2b25cc90207b26ed382192485f187f4988df
Jan Dubois <jan.dubois@suse.com>
2022-02-01 14:47:16 -0800

    Bump nerdctl to v0.16.1
    Fixes various regressions with `nerdctl login`
    
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- src/k8s-engine/wsl.ts
+++ src/k8s-engine/wsl.ts
@@ -58,7 +58,7 @@ const DISTRO_BLACKLIST = [
 ];
 
 /** The version of the WSL distro we expect. */
-const DISTRO_VERSION = '0.12';
+const DISTRO_VERSION = '0.12.1';
 
 /**
  * The list of directories that are in the data distribution (persisted across
