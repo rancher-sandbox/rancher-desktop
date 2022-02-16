13e9987f4f7226603fcaedf8bbb8048759e1d43a
Mark Yen <mark.yen@suse.com>
2022-02-15 17:48:47 -0800

    WSL: Convert complex shell commands to multi-line script.
    This makes it easier to figure out our intention, and allows us to use
    constructs that would otherwise be too long for a single line.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/k8s-engine/wsl.ts
+++ src/k8s-engine/wsl.ts
@@ -1349,17 +1349,30 @@ export default class WSLBackend extends events.EventEmitter implements K8s.Kuber
       (async() => {
         const linuxPath = await this.wslify(provisioningPath);
 
-        // Run the provisioning job. We need to clobber /etc/local.d as
-        // /etc/init.d/local has no options to use a different directory.
-        await this.execCommand('/usr/local/bin/wsl-service', '--ifstarted', 'local', 'stop');
-        await this.execCommand('/bin/rm', '-r', '-f', '/etc/local.d');
-        await this.execCommand('/bin/ln', '-s', '-f', '-T', linuxPath, '/etc/local.d');
-        await this.execCommand(
-          '/bin/sh',
-          '-c',
-          `for f in '${ linuxPath }'/*.start '${ linuxPath }'/*.stop; do [ -f "\${f}" ] && chmod a+x "\${f}"; done`);
-        // This should not be running (because we tore down init), but to be safe...
-        await this.execCommand('/usr/local/bin/wsl-service', 'local', 'start');
+        await this.execCommand('/bin/sh', '-c', `
+          set -o errexit -o nounset
+
+          # Stop the service if it's already running for some reason.
+          # This should never be the case (because we tore down init).
+          /usr/local/bin/wsl-service --ifstarted local stop
+
+          # Clobber /etc/local.d and replace it with a symlink to our desired
+          # path.  This is needed as /etc/init.d/local does not support
+          # overriding the script directory.
+          rm -r -f /etc/local.d
+          ln -s -f -T "${ linuxPath }" /etc/local.d
+
+          # Ensure all scripts are executable; Windows mounts are unlikely to
+          # have it set by default.
+          for f in "${ linuxPath }"/*.start "${ linuxPath }"/*.stop; do
+              if [ -f "\${f}" ]; then
+                  chmod a+x "\${f}"
+              fi
+          done
+
+          # Run the script.
+          exec /usr/local/bin/wsl-service local start
+        `.replace(/\r/g, ''));
       })(),
     ]);
   }
