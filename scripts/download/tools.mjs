0e34ab2ac1fe37edcaacdd87ac5d25b1f0481a40
Phillip Rak <rak.phillip@gmail.com>
2022-03-01 09:11:50 -0700

    Update to latest version of dashboard
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- scripts/download/tools.mjs
+++ scripts/download/tools.mjs
@@ -258,7 +258,7 @@ async function bindKubectlToKuberlr(kuberlrPath, binKubectlPath) {
 
 async function downloadRancherDashboard() {
   // Download Rancher Dashboard
-  const rancherDashboardVersion = 'desktop-v2.6.3.beta.6';
+  const rancherDashboardVersion = 'desktop-v2.6.3.beta.9';
   const rancherDashboardURLBase = `https://github.com/rancher-sandbox/dashboard/releases/download/${ rancherDashboardVersion }`;
   const rancherDashboardExecutable = 'rancher-dashboard-desktop-embed';
   const rancherDashboardURL = `${ rancherDashboardURLBase }/${ rancherDashboardExecutable }.tar.gz`;
