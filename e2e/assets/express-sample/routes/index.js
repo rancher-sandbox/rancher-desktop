2e6653859e5c296be71ba4b699aa52170ef3834a
Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>
2022-03-01 19:45:37 -0800

    Changed var to const as the tests complain
    Signed-off-by: Gunasekhar Matamalam <gunasekhar.matamalam@suse.com>

--- e2e/assets/express-sample/routes/index.js
+++ e2e/assets/express-sample/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- e2e/assets/express-sample/routes/users.js
+++ e2e/assets/express-sample/routes/users.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
