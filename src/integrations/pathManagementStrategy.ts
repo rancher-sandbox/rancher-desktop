1d244ee9b8387286d74bfa91877b310e22d1ac0a
Adam Pickering <adamkpickering@gmail.com>
2022-03-09 14:42:26 -0700

    Write skeleton code for PathManager and impls
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/integrations/pathManagementStrategy.ts
+++ src/integrations/pathManagementStrategy.ts
@@ -1,10 +1,45 @@
+let pathManager: ManualPathManager;
+
+interface PathManager {
+  enforce(): void
+  remove(): void
+}
+
+class ManualPathManager implements PathManager {
+  enforce(): void {
+    console.log('enforce called');
+  }
+  remove(): void {
+    console.log('remove called');
+  }
+}
+
+class RcFilePathManager implements PathManager {
+  constructor() {
+
+  }
+  enforce(): void {
+    console.log('enforce called');
+  }
+  remove(): void {
+    console.log('remove called');
+  }
+}
+
 export enum PathManagementStrategy {
   Manual = "manual",
   RcFiles = "rcfiles",
 }
 
-
 export function setPathManagementStrategy(strategy: PathManagementStrategy): void {
-  console.log("setting mgmt strategy");
-  console.log(strategy);
+  switch (strategy) {
+    case PathManagementStrategy.Manual:
+      pathManager.remove();
+      pathManager = new ManualPathManager();
+      pathManager.enforce();
+    case PathManagementStrategy.RcFiles:
+      pathManager.remove();
+      pathManager = new RcFilePathManager();
+      pathManager.enforce();
+  }
 }
