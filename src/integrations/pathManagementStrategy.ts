6b469f7a87589e5b882645606f4c453c8e13aa39
Adam Pickering <adamkpickering@gmail.com>
2022-03-09 15:53:58 -0700

    Flesh out some of RcFilePathManager
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/integrations/pathManagementStrategy.ts
+++ src/integrations/pathManagementStrategy.ts
@@ -1,55 +1,84 @@
+import path from 'path';
+import paths from '@/utils/paths';
+import { manageLinesInFile } from '@/integrations/rdFileManager';
+
 let pathManager: ManualPathManager;
 
 // PathManager is the interface that anything that manages the
 // PATH variable must implement.
 interface PathManager {
   // Makes real any changes to the system. Should be idempotent.
-  enforce(): void
+  enforce(): Promise<void>
   // Removes any changes that the PathManager may have made.
   // Should be idempotent.
-  remove(): void
+  remove(): Promise<void>
 }
 
 // ManualPathManager is for when the user has chosen to manage
 // their PATH themselves.
 class ManualPathManager implements PathManager {
-  enforce(): void {
-    return;
-  }
-  remove(): void {
-    return;
-  }
+  async enforce(): Promise<void> {}
+  async remove(): Promise<void> {}
 }
 
 // RcFilePathManager is for when the user wants Rancher Desktop to
 // make changes to their PATH by putting the necessary lines in their
-// .rc files.
+// shell .rc files.
 class RcFilePathManager implements PathManager {
   constructor() {
+    if (process.env['HOME'] === undefined) {
+      throw new Error('HOME is undefined');
+    }
+  }
 
+  async enforce(): Promise<void> {
+    await this.managePosix(true);
+    await this.manageCsh(true);
+    this.manageFish(true);
   }
-  enforce(): void {
-    console.log('enforce called');
+
+  async remove(): Promise<void> {
+    await this.managePosix(false);
+    await this.manageCsh(false);
+    this.manageFish(false);
   }
-  remove(): void {
-    console.log('remove called');
+
+  protected async managePosix(desiredPresent: boolean): Promise<void> {
+    const pathLine = `PATH=\${PATH}:${ paths.integration }`;
+    const bashrcPath = path.join(process.env['HOME']!, '.bashrc');
+
+    await manageLinesInFile(bashrcPath, [pathLine], desiredPresent);
+  }
+
+  protected async manageCsh(desiredPresent: boolean): Promise<void> {
+    const pathLine = `set path=($path ${ paths.integration })`;
+
+    await Promise.all(['.cshrc', '.tcshrc'].map((rcName) => {
+      const rcPath = path.join(process.env['HOME']!, rcName);
+
+      return manageLinesInFile(rcPath, [pathLine], desiredPresent);
+    }));
+  }
+
+  protected manageFish(desiredPresent: boolean): void {
+    console.log(desiredPresent);
   }
 }
 
 export enum PathManagementStrategy {
-  Manual = "manual",
-  RcFiles = "rcfiles",
+  Manual = 'manual',
+  RcFiles = 'rcfiles',
 }
 
-// Changes the path manager to match a PathManagementStrategy and realizes the 
+// Changes the path manager to match a PathManagementStrategy and realizes the
 // changes that the new path manager represents.
 export function setPathManagementStrategy(strategy: PathManagementStrategy): void {
   pathManager.remove();
   switch (strategy) {
-    case PathManagementStrategy.Manual:
-      pathManager = new ManualPathManager();
-    case PathManagementStrategy.RcFiles:
-      pathManager = new RcFilePathManager();
+  case PathManagementStrategy.Manual:
+    pathManager = new ManualPathManager();
+  case PathManagementStrategy.RcFiles:
+    pathManager = new RcFilePathManager();
   }
   pathManager.enforce();
 }
