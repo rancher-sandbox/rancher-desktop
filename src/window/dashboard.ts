0ecd43f6869b7b6a7a4d3b25be6dbe7f8b4de91d
Phillip Rak <rak.phillip@gmail.com>
2022-03-11 10:53:15 -0700

    Check if dashboard window is already opened before creating a new one
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/window/dashboard.ts
+++ src/window/dashboard.ts
@@ -1,5 +1,5 @@
 import { BrowserView, BrowserWindow } from 'electron';
-import { windowMapping } from '.';
+import { windowMapping, didRestoreWindow } from '.';
 
 const dashboardURL = 'http://127.0.0.1:9080/dashboard/c/local/explorer';
 
@@ -8,6 +8,11 @@ const getDashboardWindow = () => ('dashboard' in windowMapping) ? BrowserWindow.
 export function openDashboard() {
   let window = getDashboardWindow();
 
+  if (didRestoreWindow(window)) {
+    return window;
+  }
+
+  window = new BrowserWindow({
     title:  'Rancher Dashboard',
     width:  800,
     height: 600
