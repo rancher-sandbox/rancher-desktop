7f9df26a61b18b98da04cb1a74dbde846edf8c80
Adam Pickering <adamkpickering@gmail.com>
2022-03-11 16:16:51 -0700

    Flesh out IntegrationManager.ensureIntegrationDir
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/integrations/symlinkManager.ts
+++ src/integrations/symlinkManager.ts
@@ -1,19 +1,26 @@
+import fs from 'fs';
+import paths from '@/utils/paths';
+
 class IntegrationManager {
 
-  enforce() {
-    this.ensureIntegrationDir(true);
+  async enforce() {
+    await this.ensureIntegrationDir(true);
     this.ensureIntegrationSymlinks(true);
     this.ensureDockerCliSymlinks(true);
   }
 
-  remove() {
-    this.ensureDockerCliSymlinks(false);
+  async remove() {
+    await this.ensureDockerCliSymlinks(false);
     this.ensureIntegrationSymlinks(false);
     this.ensureIntegrationDir(false);
   }
 
   protected async ensureIntegrationDir(desiredPresent: boolean): Promise<void> {
-    console.log(desiredPresent);
+    if (desiredPresent) {
+      await fs.promises.mkdir(paths.integration);
+    } else {
+      await fs.promises.rmdir(paths.integration);
+    }
   }
 
   protected async ensureIntegrationSymlinks(desiredPresent: boolean): Promise<void> {
