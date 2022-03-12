ea40c2c4490f57d7ab4f36b634a00f7e63dfe74a
Adam Pickering <adamkpickering@gmail.com>
2022-03-11 17:11:59 -0700

    Fleshed out ensureDockerCliSymlinks
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/integrations/symlinkManager.ts
+++ src/integrations/symlinkManager.ts
@@ -1,40 +1,49 @@
 import fs from 'fs';
+import os from 'os';
 import path from 'path';
 import paths from '@/utils/paths';
 import resources from '@/resources';
 
 class IntegrationManager {
+  protected resourcesDir: string;
+  protected integrationDir: string;
+  protected dockerCliPluginDir: string;
+
+  constructor(resourcesDir: string, integrationDir: string, dockerCliPluginDir: string) {
+    this.resourcesDir = resourcesDir;
+    this.integrationDir = integrationDir;
+    this.dockerCliPluginDir = dockerCliPluginDir;
+  }
 
   async enforce() {
     await this.ensureIntegrationDir(true);
-    this.ensureIntegrationSymlinks(true);
-    this.ensureDockerCliSymlinks(true);
+    await this.ensureIntegrationSymlinks(true);
+    await this.ensureDockerCliSymlinks(true);
   }
 
   async remove() {
     await this.ensureDockerCliSymlinks(false);
-    this.ensureIntegrationSymlinks(false);
-    this.ensureIntegrationDir(false);
+    await this.ensureIntegrationSymlinks(false);
+    await this.ensureIntegrationDir(false);
   }
 
   protected async ensureIntegrationDir(desiredPresent: boolean): Promise<void> {
     if (desiredPresent) {
-      await fs.promises.mkdir(paths.integration);
+      await fs.promises.mkdir(this.integrationDir);
     } else {
-      await fs.promises.rmdir(paths.integration);
+      await fs.promises.rmdir(this.integrationDir);
     }
   }
 
   protected async ensureIntegrationSymlinks(desiredPresent: boolean): Promise<void> {
     // get list of integrations in the resources directory
-    const resourcesDir = path.join(process.resourcesPath, 'resources');
-    const integrationNames = (await fs.promises.readdir(resourcesDir)).filter((name) => {
+    const integrationNames = (await fs.promises.readdir(this.resourcesDir)).filter((name) => {
       return !['steve', 'trivy'].includes(name);
     });
 
     // create or remove the integrations
     integrationNames.forEach(async(name: string) => {
-      const realizedPath = path.join(paths.integration, name);
+      const realizedPath = path.join(this.integrationDir, name);
       if (desiredPresent) {
         await fs.promises.symlink(resources.executable(name), realizedPath);
       } else {
@@ -44,6 +53,24 @@ class IntegrationManager {
   }
 
   protected async ensureDockerCliSymlinks(desiredPresent: boolean): Promise<void> {
-    console.log(desiredPresent);
+    // get a list of docker plugins (should be in form docker-*)
+    const pluginNames = (await fs.promises.readdir(this.resourcesDir)).filter((name) => {
+      return name.startsWith('docker-');
+    });
+
+    // create or remove the plugin links
+    pluginNames.forEach(async(name) => {
+      const integrationPath = path.join(this.integrationDir, name);
+      const dockerCliPluginPath = path.join(this.dockerCliPluginDir, name);
+      if (desiredPresent) {
+        await fs.promises.symlink(integrationPath, dockerCliPluginPath);
+      } else {
+        // FIXME
+        await fs.promises.rm(dockerCliPluginPath, {force: true});
+      }
+    });
   }
 }
+
+//const resourcesDir = path.join(process.resourcesPath, 'resources');
+//const dockerCliPluginDir = path.join(os.homedir(), '.docker', 'cli-plugins');
