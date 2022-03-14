a518b9db11c025ce20384ec6fdfe0b410703352a
Adam Pickering <adamkpickering@gmail.com>
2022-03-14 14:25:23 -0600

    Add code to remove legacy symlinks
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/integrations/symlinkManager.ts
+++ src/integrations/symlinkManager.ts
@@ -8,20 +8,24 @@ class IntegrationManager {
   protected resourcesDir: string;
   protected integrationDir: string;
   protected dockerCliPluginDir: string;
+  protected legacyIntegrationDir: string;
 
-  constructor(resourcesDir: string, integrationDir: string, dockerCliPluginDir: string) {
+  constructor(resourcesDir: string, integrationDir: string, dockerCliPluginDir: string, legacyIntegrationDir: string) {
     this.resourcesDir = resourcesDir;
     this.integrationDir = integrationDir;
     this.dockerCliPluginDir = dockerCliPluginDir;
+    this.legacyIntegrationDir = legacyIntegrationDir;
   }
 
   async enforce() {
+    await this.removeLegacySymlinks();
     await this.ensureIntegrationDir(true);
     await this.ensureIntegrationSymlinks(true);
     await this.ensureDockerCliSymlinks(true);
   }
 
   async remove() {
+    await this.removeLegacySymlinks();
     await this.ensureDockerCliSymlinks(false);
     await this.ensureIntegrationSymlinks(false);
     await this.ensureIntegrationDir(false);
@@ -45,9 +49,7 @@ class IntegrationManager {
 
   protected async ensureIntegrationSymlinks(desiredPresent: boolean): Promise<void> {
     // get list of integrations in the resources directory
-    const integrationNames = (await fs.promises.readdir(this.resourcesDir)).filter((name) => {
-      return !['steve', 'trivy'].includes(name);
-    });
+    const integrationNames = await this.getIntegrationNames();
 
     // create or remove the integrations
     integrationNames.forEach(async(name: string) => {
@@ -61,10 +63,8 @@ class IntegrationManager {
   }
 
   protected async ensureDockerCliSymlinks(desiredPresent: boolean): Promise<void> {
-    // get a list of docker plugins (should be in form docker-*)
-    const pluginNames = (await fs.promises.readdir(this.resourcesDir)).filter((name) => {
-      return name.startsWith('docker-');
-    });
+    // get a list of docker plugins
+    const pluginNames = await this.getDockerCliPluginNames();
 
     // create or remove the plugin links
     pluginNames.forEach(async(name) => {
@@ -78,7 +78,61 @@ class IntegrationManager {
       }
     });
   }
-}
 
-//const resourcesDir = path.join(process.resourcesPath, 'resources');
-//const dockerCliPluginDir = path.join(os.homedir(), '.docker', 'cli-plugins');
+  protected async removeLegacySymlinks(): Promise<void> {
+    const integrationNames = await this.getIntegrationNames();
+
+    integrationNames.forEach(async(name) => {
+      const linkPath = path.join(this.legacyIntegrationDir, name);
+      if (await this.isLegacyIntegration(linkPath)) {
+        try {
+          console.debug(`Attempting to remove legacy symlink ${ linkPath }`);
+          await fs.promises.unlink(linkPath);
+        } catch (error: any) {
+          if (error.code !== 'ENOENT') {
+            console.error(`Error unlinking symlink ${ linkPath }: ${ error.message }`);
+          }
+        }
+      }
+    });
+  }
+
+  protected async getIntegrationNames(): Promise<string[]> {
+    return (await fs.promises.readdir(this.resourcesDir)).filter((name) => {
+      return !['steve', 'trivy'].includes(name);
+    });
+  }
+
+  // should be in form docker-*
+  protected async getDockerCliPluginNames(): Promise<string[]> {
+    return (await fs.promises.readdir(this.resourcesDir)).filter((name) => {
+      return name.startsWith('docker-');
+    });
+  }
+
+  // Tests whether a path is a legacy integration symlink that is safe to delete.
+  // @param pathToCheck -- absolute path to the file that we want to check
+  protected async isLegacyIntegration(pathToCheck: string): Promise<boolean> {
+    let linkedTo: string;
+
+    try {
+      linkedTo = await fs.promises.readlink(pathToCheck);
+    } catch (error: any) {
+      if (error.code !== 'ENOENT') {
+        console.warn(`Error getting info about node ${ pathToCheck }: ${ error.message }`);
+      }
+
+      return false;
+    }
+
+    // We need to determine whether the symlink points to something that was
+    // in a Rancher Desktop installation. Due to the range of possibilities
+    // here, I think the best we can do is to match the symlink on the string
+    // resources/<platform>/bin, since the location of the symlink can vary
+    // across packaging formats and operating systems. This should be good enough
+    // to keep it from matching symlinks that do not pertain to RD.
+    const platform = os.platform();
+    const searchString = path.join('resources', platform, 'bin');
+    return linkedTo.includes(searchString);
+  }
+}
