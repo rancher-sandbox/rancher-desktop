e3944cb58e04b7f395d464062f7bc2d71025d725
Adam Pickering <adamkpickering@gmail.com>
2022-03-17 16:06:03 -0600

    Add comments to binDir and internalDir
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- scripts/download/tools.mjs
+++ scripts/download/tools.mjs
@@ -105,7 +105,9 @@ export default async function main(platform) {
     win32:  'windows',
   }[platform];
   const resourcesDir = path.join(process.cwd(), 'resources', platform);
+  // binDir is for binaries that the user will execute
   const binDir = path.join(resourcesDir, 'bin');
+  // internalDir is for binaries that RD will execute behind the scenes
   const internalDir = path.join(resources, 'internal')
   const onWindows = kubePlatform === 'windows';
   const cpu = process.env.M1 ? 'arm64' : 'amd64';
