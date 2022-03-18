5a25615641077dd25c6cf6fd000cbfaa03b911e7
Adam Pickering <adamkpickering@gmail.com>
2022-03-18 10:37:24 -0600

    Remove resources.get
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/resources.ts
+++ src/resources.ts
@@ -1,20 +1,7 @@
 import os from 'os';
 import path from 'path';
-import { app } from 'electron';
 import memoize from 'lodash/memoize';
 import paths from '@/utils/paths';
-
-/**
- * Get the path to a resource file
- * @param pathParts Path relative to the resource directory
- */
-export function get(...pathParts: string[]) {
-  if (app.isPackaged) {
-    return path.join(process.resourcesPath, 'resources', ...pathParts);
-  }
-
-  return path.join(app.getAppPath(), 'resources', ...pathParts);
-}
 
 /**
  * Get the path to an executable binary
@@ -26,4 +13,4 @@ function _executable(name: string) {
 }
 export const executable = memoize(_executable);
 
-export default { get, executable };
+export default executable;
