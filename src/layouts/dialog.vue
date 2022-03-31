ea4faba6b0c40d92c26e5975b42fed88f07aafac
Phillip Rak <rak.phillip@gmail.com>
2022-03-31 15:08:37 -0700

    Remove any sort of height specification
    We need to remove this so that electron can properly identify the size of our
    content, so that we can automatically fit the window to the size of our content
    without scrolling.
    
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/layouts/dialog.vue
+++ src/layouts/dialog.vue
@@ -16,18 +16,15 @@ export default Vue.extend({
     // the "dark" part will be a dynamic pref.
     // See https://github.com/rancher/dashboard/blob/3454590ff6a825f7e739356069576fbae4afaebc/layouts/default.vue#L227 for an example
     return { bodyAttrs: { class: 'theme-dark' } };
-  },
-  mounted() {
-    const wrapper = this.$refs.wrapper as HTMLDialogElement;
-
-    // Dynamically resize the window to fit the contents.
-    // We need a bit extra on the width to ensure we don't get scroll bars.
-    window.resizeBy(
-      wrapper.offsetWidth - document.documentElement.offsetWidth + 14,
-      wrapper.offsetHeight - document.documentElement.offsetHeight);
   }
 });
 </script>
+
+<style lang="scss">
+  html {
+    height: initial;
+  }
+</style>
 
 <style lang="scss" scoped>
 @import "@/assets/styles/app.scss";
@@ -36,11 +33,9 @@ export default Vue.extend({
   background-color: var(--body-bg);
   border: none;
   color: var(--body-text);
-  height: 100vh;
 }
 
 .body {
-  height: 100%;
   display: flex;
   flex-flow: column;
   gap: 1rem;
