85b8a010bf72aa87c667fd8c40991f33576b4e37
Phillip Rak <rak.phillip@gmail.com>
2022-04-01 16:21:39 -0700

    Add supporting utils component
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ src/components/SupportingUtils.vue
@@ -0,0 +1,59 @@
+<script>
+import RadioGroup from '@/components/form/RadioGroup';
+
+export default {
+  components: { RadioGroup },
+  props:      {
+    value: {
+      type:     String,
+      required: true
+    },
+    row: {
+      type:    Boolean,
+      default: false
+    }
+  },
+  computed: {
+    labelsAndValues() {
+      return [
+        {
+          label:       'Automatic (sudo-less)',
+          value:       'A',
+          description: 'Rancher Desktop edits your shell profile for you. Restart any open shells for changes to take effect.',
+        },
+        {
+          label:       'Automatic (requires sudo)',
+          value:       'B',
+          description: 'Adds an entry for Rancher Desktop utilities to /etc/paths.d/',
+        },
+        {
+          label:       'Manual',
+          value:       'C',
+          description: 'Rancher Desktop will not change your PATH configuration; use your favorite editor to add ~/.rd/bin to your path manually.',
+        },
+      ];
+    },
+    options() {
+      return this.labelsAndValues;
+    }
+  },
+  methods: {
+    updateVal(value) {
+      this.$emit('input', value);
+    },
+  }
+};
+</script>
+
+<template>
+  <div class="engine-selector">
+    <radio-group
+      name="supportingUtils"
+      label="Configure PATH"
+      :value="value"
+      :options="options"
+      :row="row"
+      @input="updateVal"
+    />
+  </div>
+</template>
