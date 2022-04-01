a44e0150174bc5cb29d7d191fce45515c0bc7635
Adam Pickering <adamkpickering@gmail.com>
2022-04-01 11:26:32 -0600

    Simplify i18n Vue augmentation
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/typings/vue-i18n.ts
+++ src/typings/vue-i18n.ts
@@ -1,11 +1,14 @@
 import Vue from 'vue';
 
+// This is required to keep typescript from complaining. It is required for
+// our i18n plugin. For more info see:
+// https://v2.vuejs.org/v2/guide/typescript.html?redirect=true#Augmenting-Types-for-Use-with-Plugins
 declare module 'vue/types/vue' {
-  interface t {
-    (key: string, args?: any, raw?: boolean): string,
-  }
-
   interface Vue {
-    t: t;
+      /**
+       * Lookup a given string with the given arguments
+       * @param raw if set, do not do HTML escaping.
+       */
+      t: (key: string, args?: Record<string, any>, raw?: boolean) => string,
   }
 }
