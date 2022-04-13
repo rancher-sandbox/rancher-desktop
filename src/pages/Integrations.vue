820fa4e0fe274af54e0eebcdaaf68e8f80ba490f
Adam Pickering <adamkpickering@gmail.com>
2022-04-13 10:28:50 -0600

    Make Integrations page specific to Windows
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/pages/Integrations.vue
+++ src/pages/Integrations.vue
@@ -1,5 +1,5 @@
 <router lang="yaml">
-  name: Supporting Utilities
+  name: WSL Integration
 </router>
 
 <script lang="ts">
@@ -7,7 +7,6 @@ import os from 'os';
 import { ipcRenderer } from 'electron';
 import Vue from 'vue';
 import Integration from '@/components/Integration.vue';
-import paths from '@/utils/paths';
 
 export default Vue.extend({
   components: { Integration },
@@ -15,26 +14,16 @@ export default Vue.extend({
     return {
       /** @type Record<string, boolean | string> */
       integrations:        {},
-      /** @type Record<string, Array<string>> */
-      integrationWarnings: {},
     };
   },
   computed:   {
     hasIntegration() {
-      return /^win|^darwin|^linux$/.test(os.platform());
+      return os.platform().startsWith('win');
     },
     integrationTitle(): string {
-      if (os.platform() !== 'win32') {
-        return this.t('integrations.darwin.title');
-      }
-
       return this.t('integrations.windows.title');
     },
     integrationDescription(): string {
-      if (os.platform() !== 'win32') {
-        return `${ this.t('integrations.darwin.description') } ${ paths.integration }`;
-      }
-
       return this.t('integrations.windows.description', { }, true);
     },
   },
@@ -48,20 +37,10 @@ export default Vue.extend({
     );
   },
   mounted() {
-    ipcRenderer.on('k8s-integrations', (event, integrations) => {
+    ipcRenderer.on('k8s-integrations', (_, integrations) => {
       this.$data.integrations = integrations;
     });
     ipcRenderer.send('k8s-integrations');
-    ipcRenderer.on('k8s-integration-warnings', (event, name, warnings) => {
-      if (warnings.length === 0) {
-        this.$delete(this.integrationWarnings, name);
-      } else {
-        this.$set(this.integrationWarnings, name, warnings);
-      }
-    });
-    this.$nextTick(() => {
-      ipcRenderer.send('k8s-integration-warnings');
-    });
   },
   methods: {
     handleSetIntegration(distro: any, value: any) {
@@ -76,7 +55,6 @@ export default Vue.extend({
   <integration
     v-if="hasIntegration"
     :integrations="integrations"
-    :integration-warnings="integrationWarnings"
     :title="integrationTitle"
     @integration-set="handleSetIntegration"
   />
