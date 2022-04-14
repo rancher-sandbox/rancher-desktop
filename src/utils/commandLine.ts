1eb162fcc9b2225d2ca3220a80805195797330d8
Eric Promislow <epromislow@suse.com>
2022-04-14 12:50:01 -0700

    Fix code determining if we're running an e2e test.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/utils/commandLine.ts
+++ src/utils/commandLine.ts
@@ -26,14 +26,14 @@ export default function getCommandLineArgs(): string[] {
     const idx = process.argv.findIndex(arg => /[\\\/]dev.mjs$/.test(arg));
 
     return idx >= 0 ? process.argv.slice(idx + 1) : [];
-  } else if ((process.env.NODE_ENV ?? '').startsWith('e2e')) {
+  } else if ((process.env.npm_lifecycle_event ?? '').startsWith('test:e2e')) {
     // Note there are comments in the e2e tests near this arg warning any modifications need to take
     // this line into consideration.
     const idx = process.argv.indexOf('--disable-dev-shm-usage');
 
     return idx > -1 ? process.argv.slice(idx + 1) : [];
   }
-  console.log(`Couldn't figure out how we're being run: ENV[npm_lifecycle_event] is ${ process.env.npm_lifecycle_event ?? 'unset' }`);
+  console.log(`Couldn't figure out how we're being run: ENV[NODE_ENV] = ${ process.env.NODE_ENV }, ENV[npm_lifecycle_event] = ${ process.env.npm_lifecycle_event ?? 'unset' }`);
 
   return [];
 }
