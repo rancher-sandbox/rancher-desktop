0fa3460a85acbe9c56ff8dbaea36ed91bcf8eda3
Phillip Rak <rak.phillip@gmail.com>
2022-04-21 12:45:07 -0700

    Tick Steve version number
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- scripts/download/tools.mjs
+++ scripts/download/tools.mjs
@@ -207,7 +207,7 @@ export default async function main(platform) {
   await downloadTarGZ(trivyURL, trivyPath, { expectedChecksum: trivySHA });
 
   // Download Steve
-  const steveVersion = 'v0.1.0-beta7';
+  const steveVersion = 'v0.1.0-beta8';
   const steveURLBase = `https://github.com/rancher-sandbox/rancher-desktop-steve/releases/download/${ steveVersion }`;
   const steveCPU = process.env.M1 ? 'arm64' : 'amd64';
   const steveExecutable = `steve-${ kubePlatform }-${ steveCPU }`;
