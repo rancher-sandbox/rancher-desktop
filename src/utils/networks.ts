5650f30489f7a89758ba9d226c350341d706f2a6
Eric Promislow <epromislow@suse.com>
2022-05-17 15:27:51 -0700

    Use better network-lookup code than parsing localizable output.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/utils/networks.ts
+++ src/utils/networks.ts
@@ -1,7 +1,7 @@
 import os from 'os';
 
 export function wslHostIPv4Address(): string | undefined {
-  const iface = os.networkInterfaces()['vEthernet (WSL)'] ?? [];
+  const iface = os.networkInterfaces()['vEthernet (WSL)'];
 
-  return iface.find(addr => addr.family === 'IPv4')?.address;
+  return (iface ?? []).find(addr => addr.family === 'IPv4')?.address;
 }
