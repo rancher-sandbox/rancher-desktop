ca39f26083b606f0710a4cdd2472f790ec2fc43a
Eric Promislow <epromislow@suse.com>
2022-05-17 16:54:31 -0700

    Make stdin handling in `child_process.spawnFile` more flexible.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/main/credentialServer/__tests__/httpCredentialHelperServer.spec.ts
+++ src/main/credentialServer/__tests__/httpCredentialHelperServer.spec.ts
@@ -5,8 +5,9 @@ const subject = new HttpCredentialHelperServer();
 describe(HttpCredentialHelperServer, () => {
   describe('server', () => {
     it("should complain when the named helper utility doesn't exist", async() => {
-      await expect(subject['runWithInput']('', 'no-such-helper', ['list'])).rejects
-        .toHaveProperty('stderr', 'Error: spawn no-such-helper ENOENT');
+      await expect(() => subject['runWithInput']('', 'no-such-helper', ['list']))
+        .rejects.toHaveProperty('code', 'ENOENT');
+      // Skip testing for prop.path == 'no-such-helper' and prop.syscall == 'spawn no-such-helper'
     });
   });
 });
