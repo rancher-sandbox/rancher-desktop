a0b0b9e80b0c13e82469ab5b02b10c8f5e3b7cf2
Jan Dubois <jan.dubois@suse.com>
2022-05-20 12:28:20 -0700

    Bump docker-buildx, docker-compose, helm, and trivvy
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- scripts/download/tools.mjs
+++ scripts/download/tools.mjs
@@ -136,7 +136,7 @@ export default async function main(platform) {
   }
 
   // Download Helm. It is a tar.gz file that needs to be expanded and file moved.
-  const helmVersion = '3.8.2';
+  const helmVersion = '3.9.0';
   const helmURL = `https://get.helm.sh/helm-v${ helmVersion }-${ kubePlatform }-${ cpu }.tar.gz`;
 
   await downloadTarGZ(helmURL, path.join(binDir, exeName('helm')), {
@@ -155,7 +155,7 @@ export default async function main(platform) {
   await download(dockerURL, dockerPath, { expectedChecksum: dockerSHA });
 
   // Download the Docker-Buildx Plug-In
-  const dockerBuildxVersion = 'v0.7.1';
+  const dockerBuildxVersion = 'v0.8.2';
   const dockerBuildxURLBase = `https://github.com/docker/buildx/releases/download/${ dockerBuildxVersion }`;
   const dockerBuildxExecutable = exeName(`buildx-${ dockerBuildxVersion }.${ kubePlatform }-${ cpu }`);
   const dockerBuildxURL = `${ dockerBuildxURLBase }/${ dockerBuildxExecutable }`;
@@ -170,7 +170,7 @@ export default async function main(platform) {
   await download(dockerBuildxURL, dockerBuildxPath, dockerBuildxOptions);
 
   // Download the Docker-Compose Plug-In
-  const dockerComposeVersion = 'v2.2.3';
+  const dockerComposeVersion = 'v2.5.1';
   const dockerComposeURLBase = `https://github.com/docker/compose/releases/download/${ dockerComposeVersion }`;
   const dockerComposeCPU = process.env.M1 ? 'aarch64' : 'x86_64';
   const dockerComposeExecutable = exeName(`docker-compose-${ kubePlatform }-${ dockerComposeCPU }`);
@@ -189,7 +189,7 @@ export default async function main(platform) {
   // https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_checksums.txt
   // https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_macOS-64bit.tar.gz
 
-  const trivyVersionWithV = 'v0.25.3';
+  const trivyVersionWithV = 'v0.28.0';
   const trivyURLBase = `https://github.com/aquasecurity/trivy/releases`;
   const trivyVersion = trivyVersionWithV.replace(/^v/, '');
   const trivyOS = cpu === 'amd64' ? 'Linux-64bit' : 'Linux-ARM64';
