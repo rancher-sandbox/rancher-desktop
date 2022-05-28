c504f7f6c56c7b6cfb7a09fc8dc9fe50c6c19628
Mark Yen <mark.yen@suse.com>
2022-05-27 17:18:06 -0700

    WSL: Reuse background process class.
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/k8s-engine/wsl.ts
+++ src/k8s-engine/wsl.ts
@@ -31,6 +31,7 @@ import INSTALL_WSL_HELPERS_SCRIPT from '@/assets/scripts/install-wsl-helpers';
 import WSL_INIT_SCRIPT from '@/assets/scripts/wsl-init';
 import SCRIPT_DATA_WSL_CONF from '@/assets/scripts/wsl-data.conf';
 import mainEvents from '@/main/mainEvents';
+import BackgroundProcess from '@/utils/backgroundProcess';
 import * as childProcess from '@/utils/childProcess';
 import Logging from '@/utils/logging';
 import paths from '@/utils/paths';
@@ -91,127 +92,6 @@ function defined<T>(input: T | undefined | null): input is T {
   return typeof input !== 'undefined' && input !== null;
 }
 
-/**
- * This manages a given persistent background process that must be kept running
- * while the Kubernetes backend is running.
- */
-class BackgroundProcess {
-  /**
-   * The process being managed.
-   */
-  protected process: childProcess.ChildProcess | null = null;
-
-  /**
-   * A descriptive name of this process, for logging.
-   */
-  protected name: string;
-
-  /**
-   * The owning backend.
-   */
-  protected backend: K8s.KubernetesBackend;
-
-  /**
-   * A function which will spawn the process to be monitored.
-   */
-  protected spawn: () => Promise<childProcess.ChildProcess>;
-
-  /** A function which will terminate the process. */
-  protected destroy: (child: childProcess.ChildProcess) => Promise<void>;
-
-  /**
-   * Whether the process should be running.
-   */
-  protected shouldRun = false;
-
-  /**
-   * Timer used to restart the process;
-   */
-  protected timer: NodeJS.Timeout | null = null;
-
-  /**
-   *
-   * @param backend The owning Kubernetes backend; this is used to avoid running in an invalid state.
-   * @param name A descriptive name of the process for logging.
-   * @param spawn A function to create the underlying child process.
-   * @param destroy Optional function to stop the underlying child process.
-   */
-  constructor(backend: K8s.KubernetesBackend, name: string, spawn: typeof BackgroundProcess.prototype.spawn, destroy?: typeof BackgroundProcess.prototype.destroy) {
-    this.backend = backend;
-    this.name = name;
-    this.spawn = spawn;
-    this.destroy = destroy ?? ((process) => {
-      process?.kill('SIGTERM');
-
-      return Promise.resolve();
-    });
-  }
-
-  /**
-   * Start the process asynchronously if it does not already exist, and attempt
-   * to keep it running indefinitely.
-   */
-  start() {
-    this.shouldRun = true;
-    this.restart();
-  }
-
-  /**
-   * Attempt to start the process once.
-   */
-  protected async restart() {
-    if (!this.shouldRun || ![K8s.State.STARTING, K8s.State.STARTED, K8s.State.DISABLED].includes(this.backend.state)) {
-      console.debug(`Not restarting ${ this.name }: ${ this.shouldRun } / ${ this.backend.state }`);
-      await this.stop();
-
-      return;
-    }
-    if (this.process) {
-      await this.destroy(this.process);
-    }
-    if (this.timer) {
-      // Ideally, we should use this.timer.refresh(); however, it does not
-      // appear to actually trigger.
-      timers.clearTimeout(this.timer);
-      this.timer = null;
-    }
-    console.log(`Launching background process ${ this.name }.`);
-    const process = await this.spawn();
-
-    this.process = process;
-    process.on('exit', (status, signal) => {
-      if ([0, null].includes(status) && ['SIGTERM', null].includes(signal)) {
-        console.log(`Background process ${ this.name } exited gracefully.`);
-      } else {
-        console.log(`Background process ${ this.name } exited with status ${ status } signal ${ signal }`);
-      }
-      if (!Object.is(process, this.process)) {
-        console.log(`Not current ${ this.name } process; nothing to be done.`);
-
-        return;
-      }
-      if (this.shouldRun) {
-        this.timer = timers.setTimeout(this.restart.bind(this), 1_000);
-        console.debug(`Background process ${ this.name } will restart.`);
-      }
-    });
-  }
-
-  /**
-   * Stop the process and do not restart it.
-   */
-  async stop() {
-    console.log(`Stopping background process ${ this.name }.`);
-    this.shouldRun = false;
-    if (this.timer) {
-      clearTimeout(this.timer);
-    }
-    if (this.process) {
-      await this.destroy(this.process);
-    }
-  }
-}
-
 export default class WSLBackend extends events.EventEmitter implements K8s.KubernetesBackend {
   constructor() {
     super();
@@ -224,17 +104,20 @@ export default class WSLBackend extends events.EventEmitter implements K8s.Kuber
       this.progress = progress;
       this.emit('progress');
     });
-    this.resolverHostProcess = new BackgroundProcess(this, 'host-resolver vsock host', async() => {
-      const exe = path.join(paths.resources, 'win32', 'internal', 'host-resolver.exe');
-      const stream = await Logging['host-resolver-host'].fdStream;
-      const wslHostAddr = wslHostIPv4Address();
+    this.resolverHostProcess = new BackgroundProcess('host-resolver vsock host', {
+      spawn: async() => {
+        const exe = path.join(paths.resources, 'win32', 'internal', 'host-resolver.exe');
+        const stream = await Logging['host-resolver-host'].fdStream;
+        const wslHostAddr = wslHostIPv4Address();
 
-      return childProcess.spawn(exe, ['vsock-host',
-        '--built-in-hosts',
-        `host.rancher-desktop.internal=${ wslHostAddr },host.docker.internal=${ wslHostAddr }`], {
-        stdio:       ['ignore', stream, stream],
-        windowsHide: true,
-      });
+        return childProcess.spawn(exe, ['vsock-host',
+          '--built-in-hosts',
+          `host.rancher-desktop.internal=${ wslHostAddr },host.docker.internal=${ wslHostAddr }`], {
+          stdio:       ['ignore', stream, stream],
+          windowsHide: true,
+        });
+      },
+      shouldRun: () => Promise.resolve([K8s.State.STARTING, K8s.State.STARTED, K8s.State.DISABLED].includes(this.state)),
     });
   }
 
