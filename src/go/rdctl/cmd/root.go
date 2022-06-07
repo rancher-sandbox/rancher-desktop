c410536357aacfc099258d27168de8ee01b97dfd
Eric Promislow <epromislow@suse.com>
2022-06-07 13:31:43 -0700

    Move the config-related code to its own package.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/go/rdctl/cmd/root.go
+++ src/go/rdctl/cmd/root.go
@@ -19,16 +19,13 @@ package cmd
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
-	"path/filepath"
-	"strconv"
 	"strings"
 
+	"github.com/rancher-sandbox/rancher-desktop/src/go/rdctl/pkg/config"
 	"github.com/spf13/cobra"
 )
 
@@ -36,18 +33,6 @@ type APIError struct {
 	Message          *string `json:"message,omitifempty"`
 	DocumentationUrl *string `json:"documentation_url,omitifempty"`
 }
-
-var (
-	// Used for flags and config
-	configDir           string
-	configPath          string
-	defaultConfigPath   string
-	user                string
-	host                string
-	port                string
-	password            string
-	deferredConfigError error
-)
 
 const clientVersion = "1.1.0"
 const apiVersion = "v0"
@@ -69,8 +54,6 @@ func Execute() {
 }
 
 func init() {
-	var err error
-
 	if len(os.Args) > 1 {
 		mainCommand := os.Args[1]
 		if mainCommand == "-h" || mainCommand == "help" || mainCommand == "--help" {
@@ -82,17 +65,8 @@ func init() {
 			return
 		}
 	}
-	cobra.OnInitialize(initConfig)
-	configDir, err = os.UserConfigDir()
-	if err != nil {
-		log.Fatal("Can't get config-dir: ", err)
-	}
-	defaultConfigPath = filepath.Join(configDir, "rancher-desktop", "rd-engine.json")
-	rootCmd.PersistentFlags().StringVar(&configPath, "config-path", "", fmt.Sprintf("config file (default %s)", defaultConfigPath))
-	rootCmd.PersistentFlags().StringVar(&user, "user", "", "overrides the user setting in the config file")
-	rootCmd.PersistentFlags().StringVar(&host, "host", "", "default is localhost; most useful for WSL")
-	rootCmd.PersistentFlags().StringVar(&port, "port", "", "overrides the port setting in the config file")
-	rootCmd.PersistentFlags().StringVar(&password, "password", "", "overrides the password setting in the config file")
+	cobra.OnInitialize(config.InitConfig)
+	config.DefineGlobalFlags(rootCmd)
 }
 
 func versionCommand(version string, command string) string {
@@ -118,35 +92,33 @@ func doRequest(method string, command string) (*http.Response, error) {
 }
 
 func doRequestWithPayload(method string, command string, payload *bytes.Buffer) (*http.Response, error) {
-	if deferredConfigError != nil && insufficientConnectionInfo() {
-		return nil, deferredConfigError
-	}
-	req, err := http.NewRequest(method, makeURL(host, port, command), payload)
+	err := config.GetPossibleConfigError()
 	if err != nil {
 		return nil, err
 	}
-	req.SetBasicAuth(user, password)
+	req, err := http.NewRequest(method, makeURL(config.Host, config.Port, command), payload)
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth(config.User, config.Password)
 	req.Header.Add("Content-Type", "application/json")
 	req.Close = true
 	return http.DefaultClient.Do(req)
 }
 
 func getRequestObject(method string, command string) (*http.Request, error) {
-	if deferredConfigError != nil && insufficientConnectionInfo() {
-		return nil, deferredConfigError
-	}
-	req, err := http.NewRequest(method, makeURL(host, port, command), nil)
+	err := config.GetPossibleConfigError()
 	if err != nil {
 		return nil, err
 	}
-	req.SetBasicAuth(user, password)
+	req, err := http.NewRequest(method, makeURL(config.Host, config.Port, command), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth(config.User, config.Password)
 	req.Header.Add("Content-Type", "text/plain")
 	req.Close = true
 	return req, nil
-}
-
-func insufficientConnectionInfo() bool {
-	return port == "" || user == "" || password == ""
 }
 
 func processRequestForAPI(response *http.Response, err error) ([]byte, *APIError, error) {
@@ -206,42 +178,4 @@ func processRequestForUtility(response *http.Response, err error) ([]byte, error
 		return nil, fmt.Errorf("%s", string(body))
 	}
 	return body, nil
-}
-
-// The CLIConfig struct is used to store the json data read from the config file.
-type CLIConfig struct {
-	User     string
-	Password string
-	Port     int
-}
-
-func initConfig() {
-	if configPath == "" {
-		configPath = defaultConfigPath
-	}
-	if host == "" {
-		host = "localhost"
-	}
-	content, err := ioutil.ReadFile(configPath)
-	if err != nil {
-		deferredConfigError = fmt.Errorf("trying to read config file: %v", err)
-		return
-	}
-
-	var settings CLIConfig
-	err = json.Unmarshal(content, &settings)
-	if err != nil {
-		deferredConfigError = fmt.Errorf("trying to json-load file %s: %v", configPath, err)
-		return
-	}
-
-	if user == "" {
-		user = settings.User
-	}
-	if password == "" {
-		password = settings.Password
-	}
-	if port == "" {
-		port = strconv.Itoa(settings.Port)
-	}
 }
---
+++ src/go/rdctl/pkg/config/config.go
@@ -0,0 +1,117 @@
+/*
+Copyright © 2022 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package config handles all the config-related parts of rdctl
+
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/spf13/cobra"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
+// The CLIConfig struct is used to store the json data read from the config file.
+type CLIConfig struct {
+	User     string
+	Password string
+	Port     int
+}
+
+var (
+	User     string
+	Host     string
+	Port     string
+	Password string
+
+	configDir           string
+	configPath          string
+	defaultConfigPath   string
+	deferredConfigError error
+)
+
+// DefineGlobalFlags sets up the global flags, available for all sub-commands
+func DefineGlobalFlags(rootCmd *cobra.Command) {
+	var err error
+
+	configDir, err = os.UserConfigDir()
+	if err != nil {
+		log.Fatal("Can't get config-dir: ", err)
+	}
+	defaultConfigPath = filepath.Join(configDir, "rancher-desktop", "rd-engine.json")
+	rootCmd.PersistentFlags().StringVar(&configPath, "config-path", "", fmt.Sprintf("config file (default %s)", defaultConfigPath))
+	rootCmd.PersistentFlags().StringVar(&User, "user", "", "overrides the user setting in the config file")
+	rootCmd.PersistentFlags().StringVar(&Host, "host", "", "default is localhost; most useful for WSL")
+	rootCmd.PersistentFlags().StringVar(&Port, "port", "", "overrides the port setting in the config file")
+	rootCmd.PersistentFlags().StringVar(&Password, "password", "", "overrides the password setting in the config file")
+}
+
+// GetPossibleConfigError returns a deferred config-related error if there is one and the user
+// didn't explicitly specify all the connection-related flags
+func GetPossibleConfigError() error {
+	if deferredConfigError != nil && insufficientConnectionInfo() {
+		return deferredConfigError
+	}
+	return nil
+}
+
+// InitConfig is run after all modules are loaded and before the appropriate Execute function is invoked
+func InitConfig() {
+	if configPath == "" {
+		configPath = defaultConfigPath
+	}
+	if Host == "" {
+		Host = "localhost"
+	}
+	content, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		// If the default config file isn't available, it might not have been created yet, so don't complain.
+		// But if the user specified their own --config-path and it's not readable, complain immediately.
+		if configPath != defaultConfigPath {
+			// This code does the same as `log.Fatalf` without emitting the leading timestamp.
+			fmt.Fprintf(os.Stderr, "Error: trying to read config file: %v", err)
+			os.Exit(1)
+		}
+		deferredConfigError = fmt.Errorf("trying to read config file: %v", err)
+		return
+	}
+
+	var settings CLIConfig
+	err = json.Unmarshal(content, &settings)
+	if err != nil {
+		deferredConfigError = fmt.Errorf("trying to json-load file %s: %v", configPath, err)
+		return
+	}
+
+	if User == "" {
+		User = settings.User
+	}
+	if Password == "" {
+		Password = settings.Password
+	}
+	if Port == "" {
+		Port = strconv.Itoa(settings.Port)
+	}
+}
+
+func insufficientConnectionInfo() bool {
+	return Port == "" || User == "" || Password == ""
+}
