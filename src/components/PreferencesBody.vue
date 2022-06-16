b11c815532f3e64bd5ae3ea9e18cd72d96d4af1d
Phillip Rak <rak.phillip@gmail.com>
2022-06-16 16:44:41 -0700

    Implement overall layout for preferences modal
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ src/components/PreferencesBody.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    NOT FAIL
+  </div>
+</template>
--- src/components/PreferencesModal.vue
+++ src/components/PreferencesModal.vue
@@ -2,10 +2,13 @@
 import Vue from 'vue';
 import PreferencesHeader from '@/components/PreferencesHeader.vue';
 import PreferencesNav from '@/components/PreferencesNav.vue';
+import PreferencesBody from '@/components/PreferencesBody.vue';
 
 export default Vue.extend({
   name:       'preferences-modal',
-  components: { PreferencesHeader, PreferencesNav }
+  components: {
+    PreferencesHeader, PreferencesNav, PreferencesBody
+  }
 });
 </script>
 
@@ -14,13 +17,38 @@ export default Vue.extend({
     name="preferences"
     class="modal"
   >
-    <preferences-header />
-    <preferences-nav />
+    <div class="modal-grid">
+      <preferences-header class="grid-header" />
+      <preferences-nav class="grid-nav" />
+      <preferences-body class="grid-body" />
+    </div>
   </modal>
 </template>
 
 <style lang="scss">
   .modal .vm--modal {
     background-color: var(--body-bg);
+  }
+
+  .grid-header {
+    grid-area: header;
+  }
+
+  .grid-nav {
+    grid-area: nav;
+  }
+
+  .grid-body {
+    grid-area: body;
+  }
+
+  .modal-grid {
+    height: 100%;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-template-rows: auto 1fr;
+    grid-template-areas:
+      "header header"
+      "nav body";
   }
 </style>
