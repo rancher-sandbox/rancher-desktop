3d5216a38e8e9960ba7ebeb176a71c0025de50ec
Phillip Rak <rak.phillip@gmail.com>
2022-06-21 15:16:35 -0700

    Add inputs for Kubernetes preferences
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/components/PreferencesBodyKubernetes.vue
+++ src/components/PreferencesBodyKubernetes.vue
@@ -1,10 +1,150 @@
 <script lang="ts">
 import Vue from 'vue';
-export default Vue.extend({ name: 'preferences-body-kubernetes' });
+import { ipcRenderer } from 'electron';
+import Checkbox from '@/components/form/Checkbox.vue';
+import { Settings } from '@/config/settings';
+import { VersionEntry } from '@/k8s-engine/k8s';
+
+export default Vue.extend({
+  name:       'preferences-body-kubernetes',
+  components: { Checkbox },
+  data() {
+    return {
+      enableKubernetes: true,
+      enableTraefik:    true,
+      kubernetesPort:   6443,
+      settings:         { kubernetes: {} } as Settings,
+      versions:         [] as VersionEntry[],
+    };
+  },
+  computed: {
+    defaultVersion(): VersionEntry {
+      const version = this.recommendedVersions.find(v => (v.channels ?? []).includes('stable'));
+
+      return version ?? (this.recommendedVersions ?? this.nonRecommendedVersions)[0];
+    },
+    /** Versions that are the tip of a channel */
+    recommendedVersions(): VersionEntry[] {
+      return this.versions.filter(v => !!v.channels);
+    },
+    /** Versions that are not supported by a channel. */
+    nonRecommendedVersions(): VersionEntry[] {
+      return this.versions.filter(v => !v.channels);
+    },
+  },
+  beforeMount() {
+    ipcRenderer.on('k8s-versions', (event, versions) => {
+      this.versions = versions;
+      this.settings.kubernetes.version = this.defaultVersion.version.version;
+    });
+
+    ipcRenderer.send('k8s-versions');
+  },
+  methods: {
+    onChange() {
+      this.$emit('change:kubernetes-version');
+    },
+    /**
+     * Get the display name of a given version.
+     * @param version The version to format.
+     */
+    versionName(version: VersionEntry) {
+      const names = (version.channels ?? []).filter(ch => !/^v?\d+/.test(ch));
+
+      if (names.length > 0) {
+        return `v${ version.version.version } (${ names.join(', ') })`;
+      }
+
+      return `v${ version.version.version }`;
+    },
+  }
+});
 </script>
 
 <template>
-  <div>
-    NOT FAIL - Kubernetes
+  <div class="preferences-body">
+    <div>
+      <div class="checkbox-title">
+        <span>Kubernetes</span>
+      </div>
+      <checkbox
+        v-model="enableKubernetes"
+        label="Enable Kubernetes"
+      />
+    </div>
+    <label class="w-1/2">
+      <div class="checkbox-title">
+        Kubernetes Version
+      </div>
+      <select
+        v-model="settings.kubernetes.version"
+        class="select-k8s-version"
+        @change="onChange"
+      >
+        <!--
+            - On macOS Chrome / Electron can't style the <option> elements.
+            - We do the best we can by instead using <optgroup> for a recommended section.
+            -->
+        <optgroup v-if="recommendedVersions.length > 0" label="Recommended Versions">
+          <option
+            v-for="item in recommendedVersions"
+            :key="item.version.version"
+            :value="item.version.version"
+            :selected="item.version.version === defaultVersion.version.version"
+          >
+            {{ versionName(item) }}
+          </option>
+        </optgroup>
+        <optgroup v-if="nonRecommendedVersions.length > 0" label="Other Versions">
+          <option
+            v-for="item in nonRecommendedVersions"
+            :key="item.version.version"
+            :value="item.version.version"
+            :selected="item.version.version === defaultVersion.version.version"
+          >
+            v{{ item.version.version }}
+          </option>
+        </optgroup>
+      </select>
+    </label>
+    <div class="w-1/2">
+      <div class="checkbox-title">
+        Kubernetes Port
+      </div>
+      <input
+        v-model="kubernetesPort"
+        type="number"
+      />
+    </div>
+    <div>
+      <div class="checkbox-title">
+        <span>Traefik</span>
+      </div>
+      <checkbox
+        v-model="enableTraefik"
+        label="Enable Traefik"
+      />
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .checkbox-title {
+    font-size: 1rem;
+    line-height: 1.5rem;
+    padding-bottom: 0.5rem;
+  }
+
+  .preferences-body {
+    padding: 0.75rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .w-1\/2 {
+    width: 50%;
+    max-width: 20rem;
+    min-width: 20rem;
+  }
+</style>
