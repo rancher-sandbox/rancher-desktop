cf50aefce1a718fc4fd38d1a7f22fade0e603df4
Mark Yen <mark.yen@suse.com>
2022-06-22 16:26:30 -0700

    KubernetesBackend: add tests for backendRestartReasons()
    We write this as an E2E test as currently backends are difficult to test
    otherwise.  A new testing-only API endpoint allows us to examine the
    internal state.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

---
+++ e2e/backend.e2e.spec.ts
@@ -0,0 +1,195 @@
+import os from 'os';
+import path from 'path';
+
+import _ from 'lodash';
+import { ElectronApplication, BrowserContext, _electron, Page } from 'playwright';
+import { test, expect } from '@playwright/test';
+
+import { createDefaultSettings, packageLogs, reportAsset } from './utils/TestUtils';
+import { NavPage } from './pages/nav-page';
+import { Settings, ContainerEngine } from '@/config/settings';
+import { spawnFile } from '@/utils/childProcess';
+import { RecursivePartial } from '@/utils/typeUtils';
+
+type KubeSettings = Settings['kubernetes'];
+
+test.describe.serial('KuberentesBackend', () => {
+  let electronApp: ElectronApplication;
+  let context: BrowserContext;
+  let page: Page;
+
+  test.beforeAll(async() => {
+    createDefaultSettings();
+
+    electronApp = await _electron.launch({
+      args: [
+        path.join(__dirname, '../'),
+        '--disable-gpu',
+        '--whitelisted-ips=',
+        // See src/utils/commandLine.ts before changing the next item as the final option.
+        '--disable-dev-shm-usage',
+        '--no-modal-dialogs',
+      ],
+      env: {
+        ...process.env,
+        RD_LOGS_DIR: reportAsset(__filename, 'log'),
+      },
+    });
+    context = electronApp.context();
+
+    await context.tracing.start({ screenshots: true, snapshots: true });
+    page = await electronApp.firstWindow();
+  });
+
+  test.afterAll(async() => {
+    await context.tracing.stop({ path: reportAsset(__filename) });
+    await packageLogs(__filename);
+    await electronApp.close();
+  });
+
+  test('should start loading the background services and hide progress bar', async() => {
+    const navPage = new NavPage(page);
+
+    await navPage.progressBecomesReady();
+    await expect(navPage.progressBar).toBeHidden();
+  });
+
+  test.describe('requiresRestartReasons', () => {
+    const rdctlPath = path.join(path.dirname(__dirname), 'resources', os.platform(), 'bin', os.platform() === 'win32' ? 'rdctl.exe' : 'rdctl');
+
+    async function get(requestPath: string) {
+      const { stdout } = await spawnFile(rdctlPath, ['api', '-X', 'GET', requestPath],
+        { stdio: ['ignore', 'pipe', 'inherit'] });
+
+      return JSON.parse(stdout);
+    }
+
+    /**
+     * getOldSettings returns the subset of settings that are being modified.
+     */
+    function getOldSettings(oldSettings: Settings, newSettings: RecursivePartial<Settings>): RecursivePartial<Settings> {
+      /**
+       * keysDeep returns the list of paths of all elements of the given object.
+       */
+      const keysDeep: (obj: any, path: string[]) => string[][] = (obj, path = []) => {
+        return Object.keys(obj).flatMap((k) => {
+          return _.isObjectLike(obj[k]) ? keysDeep(obj[k], path.concat(k)) : [path.concat(k)];
+        });
+      };
+
+      // The typing for _.pick is incorrect: paths are a (string | string[])[] argument, not a rest argument.
+      return _.pick(oldSettings, keysDeep(newSettings, []) as any);
+    }
+
+    async function putSettings(newSettings: RecursivePartial<Settings>) {
+      await spawnFile(rdctlPath, ['api', '-X', 'PUT', '--body', JSON.stringify(newSettings), '/v0/settings'], { stdio: 'pipe' });
+    }
+
+    test('should not have any reasons to restart', async() => {
+      await expect(get('/v0/testBackendRestartReasons')).resolves.toEqual({});
+    });
+
+    test('should detect port changes', async() => {
+      const currentSettings = (await get('/v0/settings')) as Settings;
+      /**
+       * getAlt returns the setting that isn't the same as the existing setting.
+       */
+      const getAlt = <K extends keyof KubeSettings>(setting: K, altOne: KubeSettings[K], altTwo: KubeSettings[K]) => {
+        return currentSettings.kubernetes[setting] === altOne ? altTwo : altOne;
+      };
+      const getAlt2 = <K1 extends keyof KubeSettings, K2 extends keyof KubeSettings[K1]>(k1: K1, k2: K2, altOne: KubeSettings[K1][K2], altTwo: KubeSettings[K1][K2]) => {
+        return currentSettings.kubernetes[k1][k2] === altOne ? altTwo : altOne;
+      };
+      const newSettings: RecursivePartial<Settings> = {
+        kubernetes: {
+          version:                  getAlt('version', '1.23.6', '1.23.5'),
+          port:                     getAlt('port', 6443, 6444),
+          containerEngine:          getAlt('containerEngine', ContainerEngine.CONTAINERD, ContainerEngine.MOBY),
+          enabled:                  getAlt('enabled', true, false),
+          options:                  {
+            traefik: getAlt2('options', 'traefik', true, false),
+            flannel: getAlt2('options', 'flannel', true, false),
+          }
+        }
+      };
+      const platformSettings: Record<string, RecursivePartial<Settings>> = {
+        win32: { kubernetes: { experimentalHostResolver: getAlt('experimentalHostResolver', true, false) } },
+        lima:  {
+          kubernetes: {
+            numberCPUs:   getAlt('numberCPUs', 1, 2),
+            memoryInGB:   getAlt('memoryInGB', 3, 4),
+            suppressSudo: getAlt('suppressSudo', true, false),
+          },
+        },
+      };
+
+      _.merge(newSettings, platformSettings[os.platform() === 'win32' ? 'win32' : 'lima']);
+      const buildExpected = <K extends keyof Settings['kubernetes']>(setting: K, visible = false) => {
+        return {
+          current: currentSettings.kubernetes[setting],
+          desired: newSettings.kubernetes?.[setting],
+          visible,
+        };
+      };
+      const buildExpected2 = <K1 extends keyof KubeSettings, K2 extends keyof KubeSettings[K1]>(k1: K1, k2: K2, visible = false) => {
+        return {
+          current: currentSettings.kubernetes[k1][k2],
+          desired: (newSettings.kubernetes as KubeSettings)[k1][k2],
+          visible
+        };
+      };
+      const expected = {
+        version:           buildExpected('version'),
+        port:              buildExpected('port'),
+        containerEngine:   buildExpected('containerEngine'),
+        enabled:           buildExpected('enabled'),
+        'options.traefik': buildExpected2('options', 'traefik'),
+        'options.flannel': buildExpected2('options', 'flannel'),
+      };
+
+      if (os.platform() === 'win32') {
+        _.merge(expected, { 'host-resolver': buildExpected('experimentalHostResolver') });
+      } else {
+        _.merge(expected, {
+          sudo:   buildExpected('suppressSudo'),
+          cpu:    buildExpected('numberCPUs', true),
+          memory: buildExpected('memoryInGB', true),
+        });
+      }
+
+      await expect(putSettings(newSettings)).resolves.toBeUndefined();
+      try {
+        await expect(get('/v0/testBackendRestartReasons')).resolves.toEqual(expected);
+      } finally {
+        await expect(putSettings(getOldSettings(currentSettings, newSettings))).resolves.toBeUndefined();
+      }
+    });
+
+    test('should handle WSL integrations', async() => {
+      test.skip(os.platform() !== 'win32', 'WSL integration only supported on Windows');
+      const currentSettings = (await get('/v0/settings')) as Settings;
+      const random = `${ Date.now() }${ Math.random() }`;
+      const newSettings: RecursivePartial<Settings> = {
+        kubernetes: {
+          WSLIntegrations: {
+            [`true-${ random }`]:  true,
+            [`false-${ random }`]: false,
+          }
+        }
+      };
+
+      await expect(putSettings(newSettings)).resolves.toBeUndefined();
+      try {
+        await expect(get('/v0/testBackendRestartReasons')).resolves.toMatchObject({
+          WSLIntegrations: {
+            current: {},
+            desired: newSettings.kubernetes?.WSLIntegrations,
+            visible: false,
+          }
+        });
+      } finally {
+        await expect(putSettings(getOldSettings(currentSettings, newSettings))).resolves.toBeUndefined();
+      }
+    });
+  });
+});
