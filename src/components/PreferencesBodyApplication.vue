7012e93319c0fed63fa2e6659b65a36aa8a8b43a
Phillip Rak <rak.phillip@gmail.com>
2022-06-22 15:59:17 -0700

    Update tab styles
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/components/PreferencesBodyApplication.vue
+++ src/components/PreferencesBodyApplication.vue
@@ -49,4 +49,33 @@ export default Vue.extend({
   .application-content {
     padding: 0.75rem;
   }
+
+  .action-tabs::v-deep li.tab {
+    margin-right: 0;
+    padding-right: 0;
+    border-bottom: 1px solid;
+    border-color: var(--border);
+
+    A {
+      color: var(--muted);
+    }
+  }
+
+  .action-tabs::v-deep .tabs .tab.active {
+    border-color: var(--primary);
+    background-color: transparent;
+
+    A {
+      color: var(--link);
+    }
+  }
+
+  .action-tabs::v-deep ul {
+    border-bottom: 1px solid;
+    border-color: var(--border);
+  }
+
+  .action-tabs::v-deep .tab-container {
+    background-color: transparent;
+  }
 </style>
