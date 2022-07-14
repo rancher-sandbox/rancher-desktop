b9292115345b22e0e87e79ad1459b0e2818139b6
Mark Yen <mark.yen@suse.com>
2022-07-14 14:03:24 -0700

    E2E: increase global timeout on CI
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- e2e/config/playwright-config.ts
+++ e2e/config/playwright-config.ts
@@ -3,12 +3,13 @@ import type { Config, PlaywrightTestOptions } from '@playwright/test';
 
 const outputDir = path.join(__dirname, '..', 'e2e', 'test-results');
 const testDir = path.join(__dirname, '..', '..', 'e2e');
+const timeScale = process.env.CI ? 2 : 1;
 
 const config: Config<PlaywrightTestOptions> = {
   testDir,
   outputDir,
-  timeout:       process.env.CI ? 600_000 : 300_000,
-  globalTimeout: 900_000,
+  timeout:       300_000 * timeScale,
+  globalTimeout: 900_000 * timeScale,
   workers:       1,
   reporter:      'list',
 };
