0f56fb577960ed04aed535baa39ba48814ddc822
Phillip Rak <rak.phillip@gmail.com>
2022-07-18 16:09:48 -0700

    De-emphasize preferences button by default
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -46,11 +46,14 @@ export default Vue.extend({
     flex: 1;
     height: 40px;
     z-index: 2;
-
-        img {
-            height: 40px;
-        }
+      img {
+        height: 40px;
+      }
     }
 
+    .header-actions {
+      display: flex;
+      align-items: center;
+    }
   }
 </style>
--- src/components/Preferences/ButtonOpen.vue
+++ src/components/Preferences/ButtonOpen.vue
@@ -11,37 +11,38 @@ export default Vue.extend({
 </script>
 
 <template>
-  <button
-    class="btn role-secondary btn-sm"
-    type="button"
+  <span
+    class="icon icon-gear ripple"
     aria-label="Open Preferences"
+    role="button"
     @click="openPreferences"
-  >
-    <i class="icon icon-gear" />
-  </button>
+    @keydown.enter="openPreferences"
+    @keydown.space="openPreferences"
+  />
 </template>
 
 <style lang="scss" scoped>
-  button.btn {
-    background: none;
-    border: none;
-    box-shadow: none;
-    .icon {
-      background: var(--primary);
-      color: var(--primary-text);
-      width: 32px;
-      height: 32px;
-      border-radius: 50%;
-      padding-top: 5px;
-      &::before {
-        font-size: 22px;
-        margin-left: 1px;
-      }
-    }
-    &:focus {
-      .icon {
-        outline: var(--outline-width) dashed var(--primary-active-bg);
-      }
-    }
+  .icon-gear {
+    cursor: pointer;
+    font-size: 1.875rem;
+    // line-height: 2.25rem;
+    padding: 0.5rem;
+    transition: background 400ms;
+    border-radius: 50%;
+  }
+
+  .ripple {
+    background-position: center;
+    transition: background 0.8s;
+  }
+
+  .ripple:hover {
+    background: var(--primary-hover-bg) radial-gradient(circle, transparent 1%, var(--primary-hover-bg) 1%) center/15000%;
+  }
+
+  .ripple:active {
+    background-color: var(--primary);
+    background-size: 100%;
+    transition: background 0s;
   }
 </style>
