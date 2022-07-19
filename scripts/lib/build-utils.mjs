2a16fd91e87f94a51af9376bdf56e456d43667bf
Eric Promislow <epromislow@suse.com>
2022-07-19 15:23:20 -0700

    Revert "Build rdctl for linux"
    This reverts commit 94135970aac4f1309da9a434147b1fbb53f16776.

--- scripts/lib/build-utils.mjs
+++ scripts/lib/build-utils.mjs
@@ -319,8 +319,7 @@ export default {
       tasks.push(() => this.buildVtunnel('win32'));
       tasks.push(() => this.buildVtunnel('linux'));
     }
-    tasks.push(() => this.buildUtility('rdctl', 'windows'));
-    tasks.push(() => this.buildUtility('rdctl', 'linux'));
+    tasks.push(() => this.buildUtility('rdctl', os.platform()));
     tasks.push(() => this.buildUtility('docker-credential-none', os.platform()));
 
     return this.wait(...tasks);
