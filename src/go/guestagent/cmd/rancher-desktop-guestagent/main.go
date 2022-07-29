2bc7efbc265e4a450a90428e60eb4aece39f80f2
Mark Yen <mark.yen@suse.com>
2022-07-28 18:08:10 -0700

    Add a Kubernetes watcher.
    This component connects to a Kubernetes cluster using a provided kubeconfig
    file (defaulting to the k3s one), and watches for services.  This is used
    to handle newer versions of Kubernetes which has stopped listening on the
    local interface for NodePort services.
    
    This is the initial proof of concept; much needs to change for actual use.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/go/guestagent/cmd/rancher-desktop-guestagent/main.go
+++ src/go/guestagent/cmd/rancher-desktop-guestagent/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/Masterminds/log-go"
 	"github.com/rancher-sandbox/rancher-desktop-agent/pkg/iptables"
+	"github.com/rancher-sandbox/rancher-desktop-agent/pkg/kube"
+	"golang.org/x/sync/errgroup"
 )
 
 var debug = flag.Bool("debug", false, "display debug output")
+var configPath = flag.String("config-path", "/etc/rancher/k3s/k3s.yaml", "path to kubeconfig")
 
 func main() {
 
@@ -27,9 +32,24 @@ func main() {
 		log.Fatal("agent must run as root")
 	}
 
-	// Forward ports
-	err := iptables.ForwardPorts(3 * time.Second)
-	if err != nil {
-		log.Errorf("Error mapping ports: %s", err)
+	group, ctx := errgroup.WithContext(context.Background())
+	group.Go(func() error {
+		// Forward ports
+		err := iptables.ForwardPorts(3 * time.Second)
+		if err != nil {
+			return fmt.Errorf("error mapping ports: %w", err)
+		}
+		return nil
+	})
+	group.Go(func() error {
+		// Watch for kube
+		err := kube.WatchForNodePortServices(ctx, *configPath)
+		if err != nil {
+			return fmt.Errorf("error watching services: %w", err)
+		}
+		return nil
+	})
+	if err := group.Wait(); err != nil {
+		log.Fatal(err)
 	}
 }
