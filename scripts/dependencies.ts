11160d859329e1acf39a8cceb8e1fe46a45cba33
Adam Pickering <adamkpickering@gmail.com>
2022-08-05 10:51:40 -0600

    Update dependencies script for all platforms
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- scripts/dependencies.ts
+++ scripts/dependencies.ts
@@ -1,15 +1,35 @@
+import { execFileSync } from 'child_process';
+import os from 'os';
 import DependencyVersions from './download/dependencies';
 import downloadDependencies from './download/tools';
 
 async function runScripts(): Promise<void> {
   // load desired versions of dependencies
   const depVersions = await DependencyVersions.fromJSONFile('dependencies.json');
-  console.log(deps);
-  
+
   // download the desired versions
-  await downloadDependencies(depVersions): Promise<void>;
+  await (await import('./download/moby-openapi.mjs')).default();
+  switch (os.platform()) {
+  case 'linux':
+    await downloadDependencies('linux', depVersions);
+    await (await import('./download/lima.mjs')).default();
+    break;
+  case 'darwin':
+    await downloadDependencies('darwin', depVersions);
+    await (await import('./download/lima.mjs')).default();
+    break;
+  case 'win32':
+    await downloadDependencies('win32', depVersions);
+    await downloadDependencies('wsl', depVersions);
+    await (await import('./download/wsl.mjs')).default();
+    break;
+  }
 }
 
 runScripts().then(() => {
-  console.log('done');
+  execFileSync('node', ['node_modules/electron-builder/out/cli/cli.js', 'install-app-deps'], { stdio: 'inherit' });
 })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
