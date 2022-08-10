04f6f6660f190cef70ef5221157c4380b1f039fd
Phillip Rak <rak.phillip@gmail.com>
2022-08-10 15:46:44 -0700

    Add basic table for diagnostics
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/components/DiagnosticsBody.vue
+++ src/components/DiagnosticsBody.vue
@@ -1,25 +1,97 @@
 <script lang="ts">
 import Vue from 'vue';
-export default Vue.extend({ name: 'DiagnosticsBody' });
+
+import BadgeState from '@/components/BadgeState.vue';
+import SortableTable from '@/components/SortableTable/index.vue';
+
+export default Vue.extend({
+  name:       'DiagnosticsBody',
+  components: {
+    SortableTable,
+    BadgeState,
+  },
+  data() {
+    return {
+      headers: [
+        {
+          name:  'name',
+          label: 'Name',
+        },
+        {
+          name:  'documentation',
+          label: 'Documentation',
+        },
+        {
+          name:  'category',
+          label: 'Category',
+        },
+        {
+          name:  'mute',
+          label: 'Mute',
+        },
+      ],
+      rows: [
+        {
+          name:          'The ~/.rd/bin directory has not been added to the PATH, so commandline utilities are not configured in your back shell',
+          documentation: 'Some link',
+          category:      'kubernetes',
+          mute:          false,
+        },
+      ],
+    };
+  },
+});
 </script>
 
 <template>
-  <div class="diagnostics-status">
-    <div class="item-results">
-      6 failed (0 muted) Hide muted
+  <div class="diagnostics">
+    <div class="status">
+      <div class="item-results">
+        6 failed (0 muted) Hide muted
+      </div>
+      <div class="diagnostics-status-history">
+        Last run: 52 minutes ago
+      </div>
     </div>
-    <div class="diagnostics-status-history">
-      Last run: 52 minutes ago
-    </div>
+    <sortable-table
+      :headers="headers"
+      :rows="rows"
+      :search="false"
+      :table-actions="false"
+      :row-actions="false"
+      :sub-rows="true"
+      :sub-expandable="true"
+      :sub-expand-column="true"
+    >
+      <template #col:documentation="{row}">
+        <td>
+          <a :href="row.documentation"><span class="icon icon-external-link" /></a>
+        </td>
+      </template>
+      <template #col:category="{row}">
+        <td>
+          <badge-state
+            :label="row.category"
+            color="bg-warning"
+          />
+        </td>
+      </template>
+    </sortable-table>
   </div>
 </template>
 
 <style lang="scss" scoped>
-  .diagnostics-status {
+  .diagnostics {
     display: flex;
+    flex-direction: column;
+    gap: 2rem;
 
-    .item-results {
-      flex: 1;
+    .status {
+      display: flex;
+
+      .item-results {
+        flex: 1;
+      }
     }
   }
 </style>
