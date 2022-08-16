19dc25d3fdbd72347165e45ec1658d454f13c151
Mark Yen <mark.yen@suse.com>
2022-08-16 14:52:47 -0700

    WSL: Catch wsl.exe responding with usage error.
    On Windows 11 (and possibly recent Windows 10), `wsl.exe` exists but is a
    stub that is just used for installing the main package (which will
    effectively replace `wsl.exe`).  When we run it initially, check for usage
    messages in addition to checking for the exception being ENOENT to catch
    when it's not installed.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/backend/wsl.ts
+++ src/backend/wsl.ts
@@ -941,16 +941,20 @@ export default class WSLBackend extends events.EventEmitter implements K8s.Kuber
     // Check if wsl.exe is available
     try {
       await this.isDistroRegistered();
-    } catch (ex) {
-      if ((ex as NodeJS.ErrnoException).code === 'ENOENT') {
+    } catch (ex: any) {
+      const stdout = String(ex.stdout || '');
+      const isWSLMissing = (ex as NodeJS.ErrnoException).code === 'ENOENT';
+      const isInvalidUsageError = stdout.includes('Usage: ') && !stdout.includes('--exec');
+
+      if (isWSLMissing || isInvalidUsageError) {
         console.log('Error launching WSL: it does not appear to be installed.');
         const message = `
           Windows Subsystem for Linux does not appear to be installed.
 
           Please install it manually:
 
-          https://docs.microsoft.com/en-us/windows/wsl/install-win10
-        `.replace(/[ \t]{2,}/g, '');
+          https://docs.microsoft.com/en-us/windows/wsl/install
+        `.replace(/[ \t]{2,}/g, '').trim();
 
         return new K8s.KubernetesError('Error: WSL Not Installed', message, true);
       }
