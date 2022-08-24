e99fcf9c13780a5fb44de2210f02064116919025
Adam Pickering <adamkpickering@gmail.com>
2022-08-24 15:26:27 -0600

    Add code to read WSL distro version from dependencies.yaml
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- src/backend/wsl.ts
+++ src/backend/wsl.ts
@@ -51,6 +51,7 @@ import paths from '@/utils/paths';
 import { jsonStringifyWithWhiteSpace } from '@/utils/stringify';
 import { defined, RecursivePartial, RecursiveReadonly } from '@/utils/typeUtils';
 import { showMessageBox } from '@/window';
+import * as YAML from 'yaml';
 
 const console = Logging.wsl;
 const INSTANCE_NAME = 'rancher-desktop';
@@ -72,7 +73,17 @@ enum Action {
 }
 
 /** The version of the WSL distro we expect. */
-const DISTRO_VERSION = '0.26';
+async function getDependencyVersion(key: string): Promise<string> {
+  const dependenciesPath = path.join(paths.resources, 'dependencies.yaml');
+  const rawVersions = await fs.promises.readFile(dependenciesPath, 'utf-8');
+  const dependencyVersions = YAML.parse(rawVersions);
+  const version = dependencyVersions[key];
+  if (typeof version !== 'string') {
+     throw new Error(`Key "${ key }" was not found in dependency version file or was of wrong type`);
+  }
+  return version;
+}
+const DISTRO_VERSION = await getDependencyVersion('WSLDistro');
 
 /**
  * The list of directories that are in the data distribution (persisted across
