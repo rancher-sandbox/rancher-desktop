1981b29e71edab3106d35876c10c4e3eba3885b8
Phillip Rak <rak.phillip@gmail.com>
2022-09-02 15:15:28 -0700

    Example using express & http-proxy-middleware
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/main/dashboardServer/index.ts
+++ src/main/dashboardServer/index.ts
@@ -1,57 +1,20 @@
-import fs from 'fs';
-import http from 'http';
 import path from 'path';
 
 import { app } from 'electron';
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
-const host = 'localhost';
+const host = '127.0.0.1';
 const port = 6120;
 
-const STATIC_PATH = path.join(app.getAppPath(), 'resources', 'rancher-dashboard');
+const dashboardServer = express();
 
-const MIME_TYPES: Record<string, string> = {
-  html: 'text/html; charset=UTF-8',
-  js:   'application/javascript; charset=UTF-8',
-  css:  'text/css',
-  json: 'application/json',
-  png:  'image/png',
-  jpg:  'image/jpeg',
-  jpeg: 'image/jpeg',
-  ico:  'image/x-icon',
-  svg:  'image/svg+xml',
-};
-
-const serveFile = (name: string) => {
-  const filePath = path.join(STATIC_PATH, name);
-
-  if (!filePath.startsWith(STATIC_PATH)) {
-    console.log(`Can't be served: ${ name }`);
-
-    return null;
-  }
-  const stream = fs.createReadStream(filePath);
-
-  console.log(`Served: ${ name }`);
-
-  return stream;
-};
+dashboardServer
+  .use(express.static(path.join(app.getAppPath(), 'resources', 'rancher-dashboard')))
+  .use('/v1', createProxyMiddleware({ target: 'http://127.0.0.1:9080', changeOrigin: true }));
 
 export const init = () => {
-  http
-    .createServer((req, res) => {
-      const { url } = req;
-
-      if (req.method === 'GET') {
-        const fileExt = path.extname(url || '').substring(1);
-        const mimeType = MIME_TYPES[fileExt] || MIME_TYPES.html;
-
-        res.writeHead(200, { 'Content-Type': mimeType });
-        const stream = fileExt === '' ? serveFile('/index.html') : serveFile(url || '');
-
-        if (stream) {
-          stream.pipe(res);
-        }
-      }
-    })
-    .listen(port, host);
+  dashboardServer.listen(port, host, () => {
+    console.log('Server Running');
+  });
 };
