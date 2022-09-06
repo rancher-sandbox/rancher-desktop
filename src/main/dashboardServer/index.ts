cc9f66993d01b92d1cb1d948c378874574e69b81
Phillip Rak <rak.phillip@gmail.com>
2022-09-06 15:00:04 -0700

    Set secure = false for proxied endpoints
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/main/dashboardServer/index.ts
+++ src/main/dashboardServer/index.ts
@@ -2,29 +2,29 @@ import path from 'path';
 
 import { app } from 'electron';
 import express from 'express';
-// import httpProxy from 'http-proxy';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const host = '127.0.0.1';
 const port = 6120;
-// const apiProxy = httpProxy.createProxyServer();
 
 const dashboardServer = express();
 
 dashboardServer
-  // .all('/v1/*', (req, res) => {
-  //   console.log('redirecting to STEVE');
-  //   apiProxy.web(req, res, { target: 'http://127.0.0.1:9080' });
-  // })
-  // .all('/v3/*', (req, res) => {
-  //   console.log('redirecting to STEVE');
-  //   apiProxy.web(req, res, { target: 'http://127.0.0.1:9080' });
-  // })
   .use(
     '/v1',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:9080',
-      // changeOrigin: true,
+      target:       'https://127.0.0.1:9443',
+      secure:       false,
+      logLevel:     'debug',
+      changeOrigin: true,
+    }))
+  .use(
+    '/v3',
+    createProxyMiddleware({
+      target:       'https://127.0.0.1:9443',
+      secure:       false,
+      logLevel:     'debug',
+      changeOrigin: true,
     }))
   .use(
     express.static(
@@ -32,7 +32,7 @@ dashboardServer
     ));
 
 export const init = () => {
-  // dashboardServer.listen(port, host, () => {
-  //   console.log('Server Running');
-  // });
+  dashboardServer.listen(port, host, () => {
+    console.log('Server Running');
+  });
 };
