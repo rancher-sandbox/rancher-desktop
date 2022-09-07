f884a3846652e3097e5ebd5e97d8956249002982
Adam Pickering <adamkpickering@gmail.com>
2022-09-07 11:32:21 -0600

    Get no-op version of script working
    Signed-off-by: Adam Pickering <adamkpickering@gmail.com>

--- scripts/depversions.ts
+++ scripts/depversions.ts
@@ -7,12 +7,12 @@ import { LimaAndQemu, AlpineLimaISO } from 'scripts/dependencies/lima';
 import { MobyOpenAPISpec } from 'scripts/dependencies/moby-openapi';
 import * as tools from 'scripts/dependencies/tools';
 import { WSLDistro, HostResolverHost, HostResolverPeer } from 'scripts/dependencies/wsl';
-import { DependencyVersions, Dependency, AlpineLimaISOVersion } from 'scripts/lib/dependencies';
+import { DependencyVersions, Dependency, AlpineLimaISOVersion, getOctokit } from 'scripts/lib/dependencies';
 
-type VersionComparison = {
+type VersionComparison<Type extends string | AlpineLimaISOVersion> = {
   name: string;
-  currentVersion: string;
-  latestVersion: string;
+  currentVersion: Type;
+  latestVersion: Type;
 }
 
 const dependencies: Dependency[] = [
@@ -47,7 +47,7 @@ async function checkDependencies(): Promise<void> {
 
   await Promise.all(promises);
 
-  const versionComparisons: VersionComparison[] = [];
+  const versionComparisons: VersionComparison<string | AlpineLimaISOVersion>[] = [];
 
   for (const [name, latestVersion] of Object.entries(latestVersions)) {
     const currentVersion = currentVersions[name as keyof DependencyVersions];
@@ -58,7 +58,24 @@ async function checkDependencies(): Promise<void> {
       latestVersion,
     });
   }
-  console.log(JSON.stringify(versionComparisons));
+
+  for (const {name, currentVersion, latestVersion} of versionComparisons) {
+    if (JSON.stringify(currentVersion) !== JSON.stringify(latestVersion)) {
+      // find PR for this combo of name, current version and latest version
+      const branchName = `rddepman-bump-${ name }-from-${ currentVersion }-to-${ latestVersion }`;
+      const response = await getOctokit().rest.pulls.list({owner: 'rancher-sandbox', repo: 'rancher-desktop', base: branchName})
+      const prs = response.data;
+      if (prs.length === 0) {
+        console.log(`Creating PR to bump dependency "${ name }" from "${ currentVersion }" to "${ latestVersion }"`);
+      } else if (prs.length === 1) {
+        console.log(`Found PR that bumps dependency "${ name }" from "${ currentVersion }" to "${ latestVersion }"; doing nothing`)
+      } else if (prs.length >= 2) {
+        throw new Error(`Multiple branches bumping dependency "${ name }" from "${ currentVersion }" to "${ latestVersion }" found`);
+      }
+    } else {
+      console.log(`Current version and latest version for dependency "${ name }" are equal; doing nothing`);
+    }
+  }
 }
 
 checkDependencies().catch((e) => {
