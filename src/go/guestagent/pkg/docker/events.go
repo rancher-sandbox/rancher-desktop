9fbdd1648d13dda2ebc4e39e8298a0209a7a9038
Nino Kodabande <nkodabande@suse.com>
2022-09-07 15:57:06 -0700

    Add docker API client to monitor container events
    Signed-off-by: Nino Kodabande <nkodabande@suse.com>

---
+++ src/go/guestagent/pkg/docker/events.go
@@ -0,0 +1,80 @@
+/*
+Copyright © 2022 SUSE LLC
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package docker handles port binding events from docker events API
+package docker
+
+import (
+	"context"
+
+	"github.com/Masterminds/log-go"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
+	"github.com/rancher-sandbox/rancher-desktop-agent/pkg/tracker"
+)
+
+const (
+	startEvent = "start"
+	stopEvent  = "stop"
+)
+
+// EventMonitor monitors the Docker engine's Event API
+// for container events
+type EventMonitor struct {
+	dockerClient *client.Client
+}
+
+// NewEventMonitor creates and returns a new Docker API client
+func NewEventMonitor() (*EventMonitor, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+	return &EventMonitor{
+		dockerClient: cli,
+	}, nil
+}
+
+// MonitorPorts scans Docker's event stream API
+// for container start/stop events
+func (e *EventMonitor) MonitorPorts(ctx context.Context, portTracker *tracker.PortTracker) {
+	msgCh, errCh := e.dockerClient.Events(ctx, types.EventsOptions{Filters: filters.NewArgs(
+		filters.Arg("type", "container"),
+		filters.Arg("event", startEvent),
+		filters.Arg("event", stopEvent))})
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Errorf("context cancellation: %w", ctx.Err())
+			return
+		case event := <-msgCh:
+			log.Debugf("received an event: %+v", event)
+			container, err := e.dockerClient.ContainerInspect(ctx, event.ID)
+			if err != nil {
+				log.Errorf("inspecting container failed: %w", err)
+				continue
+			}
+			switch event.Action {
+			case startEvent:
+				portTracker.Add(container.ID, container.NetworkSettings.NetworkSettingsBase.Ports)
+			case stopEvent:
+				portTracker.Remove(container.ID)
+			}
+		case err := <-errCh:
+			log.Errorf("receiving container event failed: %w", err)
+			return
+		}
+	}
+}
