8f36f5a48af8000c0d37e9239ff7e7c280b59df7
Phillip Rak <rak.phillip@gmail.com>
2022-09-08 12:17:46 -0700

    Proxy endpoints for Rancher backend
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- src/main/dashboardServer/index.ts
+++ src/main/dashboardServer/index.ts
@@ -2,30 +2,50 @@ import path from 'path';
 
 import { app } from 'electron';
 import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import { createProxyMiddleware, Options } from 'http-proxy-middleware';
 
 const host = '127.0.0.1';
 const port = 6120;
 
+const api = 'https://127.0.0.1:9443';
+const proxyOpts = (target: string): Options => {
+  return {
+    target,
+    followRedirects: true,
+    secure:          false,
+    logLevel:        'debug',
+  };
+};
+
+const proxyWsOpts = (target: string): Options => {
+  return {
+    ...proxyOpts(target),
+    ws:           true,
+    changeOrigin: true,
+  };
+};
+
+const proxyMetaOpts = (target: string): Options => {
+  return {
+    target,
+    followRedirects: true,
+    secure:          false,
+  };
+};
+
 const dashboardServer = express();
 
 dashboardServer
-  .use(
-    '/v1',
-    createProxyMiddleware({
-      target:       'https://127.0.0.1:9443',
-      secure:       false,
-      logLevel:     'debug',
-      changeOrigin: true,
-    }))
-  .use(
-    '/v3',
-    createProxyMiddleware({
-      target:       'https://127.0.0.1:9443',
-      secure:       false,
-      logLevel:     'debug',
-      changeOrigin: true,
-    }))
+  .use('/k8s', createProxyMiddleware(proxyWsOpts(api)))
+  .use('/pp', createProxyMiddleware(proxyWsOpts(api)))
+  .use('/api', createProxyMiddleware(proxyWsOpts(api)))
+  .use('/apis', createProxyMiddleware(proxyWsOpts(api)))
+  .use('/v1', createProxyMiddleware(proxyWsOpts(api)))
+  .use('/v3', createProxyMiddleware(proxyWsOpts(api)))
+  .use('/v3-public', createProxyMiddleware(proxyOpts(api)))
+  .use('/api-ui', createProxyMiddleware(proxyOpts(api)))
+  .use('/meta', createProxyMiddleware(proxyMetaOpts(api)))
+  .use('/v1-*', createProxyMiddleware(proxyOpts(api)))
   .use(
     express.static(
       path.join(app.getAppPath(), 'resources', 'rancher-dashboard'),
