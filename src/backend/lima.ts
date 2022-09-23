252f7e8be7dbd52b3a4fece9259176b63d09263e
Mark Yen <3977982+mook-as@users.noreply.github.com>
2022-09-23 13:30:26 -0700

    Lima: fix typo in comment
    Addresses:
    https://github.com/rancher-sandbox/rancher-desktop/pull/3002#discussion_r979042550
    
    Co-authored-by: Jan Dubois <jan@jandubois.com>
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/backend/lima.ts
+++ src/backend/lima.ts
@@ -551,7 +551,7 @@ export default class LimaBackend extends events.EventEmitter implements VMBacken
     });
 
     // RD used to store additional keys in lima.yaml that are not supported by lima (and no longer used by RD).
-    // They must be removed because lime intends to switch to strict YAML parsing, so typos can be detected.
+    // They must be removed because lima intends to switch to strict YAML parsing, so typos can be detected.
     delete (config as Record<string, unknown>).k3s;
     delete (config as Record<string, unknown>).paths;
 
