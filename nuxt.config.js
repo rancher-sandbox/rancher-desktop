d531b3e66ed23ab2e754986b8ce915c76c62e03f
Phillip Rak <rak.phillip@gmail.com>
2022-10-14 15:35:54 -0700

    Add comment to root-level nuxt.config
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+/**
+ * This root-level nuxt.config manages configuration for @rancher/shell and
+ * supports Rancher Extensions development.
+ */
 import config from '@rancher/shell/nuxt.config';
 
 export default config(__dirname, {
