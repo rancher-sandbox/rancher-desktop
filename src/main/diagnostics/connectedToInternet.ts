2e5c06ec135f867b898cd8115d012b35ddd41fa1
Adam Pickering <adam.pickering@suse.com>
2022-10-26 16:04:19 -0600

    Extend timeout for network connectivity diagnostic
    Signed-off-by: Adam Pickering <adam.pickering@suse.com>

--- src/main/diagnostics/connectedToInternet.ts
+++ src/main/diagnostics/connectedToInternet.ts
@@ -2,19 +2,45 @@ import fetch from 'node-fetch';
 
 import { DiagnosticsCategory, DiagnosticsChecker } from './types';
 
+import Logging from '@/utils/logging';
+
+const console = Logging.diagnostics;
+
+// Returns the timeout, in milliseconds, for the network connectivity check.
+function getTimeout(): number {
+  if (process.env.RD_CONNECTED_TO_INTERNET_TIMEOUT) {
+    const parsedTimeout = parseInt(process.env.RD_CONNECTED_TO_INTERNET_TIMEOUT);
+
+    if (parsedTimeout > 0) {
+      return parsedTimeout;
+    }
+  }
+
+  return 5000;
+}
+
 /**
  * Checks whether we can perform an HTTP request to a host on the internet,
  * with a reasonably short timeout.
  */
 async function checkNetworkConnectivity(): Promise<boolean> {
   const controller = new AbortController();
-  const timeoutId = setTimeout(() => controller.abort(), 2000);
+  const timeout = getTimeout();
+  const timeoutId = setTimeout(() => controller.abort(), timeout);
   let connected: boolean;
 
+  console.log(`Running connectivity test with timeout of ${ timeout } ms`);
   try {
     await fetch('https://example.com/', { signal: controller.signal });
+    console.log('Connection test completed successfully');
     connected = true;
   } catch (error: any) {
+    let errorMessage = error;
+
+    if (error.name === 'AbortError') {
+      errorMessage = `timed out after ${ timeout } ms`;
+    }
+    console.log(`Got error while checking connectivity: ${ errorMessage }`);
     connected = false;
   } finally {
     clearTimeout(timeoutId);
