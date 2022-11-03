10c925d3fbafe12c0ffad04c359b38dc180ebb1e
Francesco Torchia <francesco.torchia@suse.com>
2022-11-03 15:56:33 +0100

    Allowed images list: add alert
    - add an alert message to notify the Allowed images is enabled
    
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

---
+++ pkg/rancher-desktop/components/Alert.vue
@@ -0,0 +1,55 @@
+<script lang="ts">
+import { Banner } from '@rancher/components';
+import Vue from 'vue';
+
+export default Vue.extend({
+  name:       'alert',
+  components: { Banner },
+  props:      {
+    icon: {
+      type:     String,
+      required: true,
+    },
+    bannerText: {
+      type:     String,
+      required: true,
+    },
+    color: {
+      type:     String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <transition
+    name="fade"
+    appear
+  >
+    <banner
+      class="banner-notify"
+      :color="color"
+    >
+      <span
+        class="icon"
+        :class="icon"
+      />
+      {{ bannerText }}
+    </banner>
+  </transition>
+</template>
+
+<style lang="scss" scoped>
+  .banner-notify {
+    margin: 0;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-active {
+    transition: all 0.25s ease-in;
+  }
+</style>
