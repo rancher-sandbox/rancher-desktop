5e3fdc68bfda010fc45181e91539db859db01618
Adam Pickering <adam.pickering@suse.com>
2022-11-15 16:02:23 -0700

    Add RancherDesktopRepository
    Also refactor docker-cli-monitor to use RancherDesktopRepository.
    
    Signed-off-by: Adam Pickering <adam.pickering@suse.com>

--- scripts/docker-cli-monitor.ts
+++ scripts/docker-cli-monitor.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 
-import { Octokit } from 'octokit';
-import { readDependencyVersions, getOctokit } from 'scripts/lib/dependencies';
+import { readDependencyVersions, getOctokit, RancherDesktopRepository, IssueOrPullRequest } from 'scripts/lib/dependencies';
 import semver from 'semver';
 
 const GITHUB_OWNER = process.env.GITHUB_REPOSITORY?.split('/')[0] || 'rancher-sandbox';
@@ -10,8 +9,7 @@ const DOCKER_CLI_OWNER = process.env.DOCKER_CLI_OWNER || 'docker';
 const DOCKER_CLI_REPO = process.env.DOCKER_CLI_REPO || 'cli';
 const TAG_REGEX = /^v[0-9]+\.[0-9]+\.[0-9]+$/;
 const SCRIPT_NAME = 'docker-cli-monitor';
-
-type Issue = Awaited<ReturnType<Octokit['rest']['search']['issuesAndPullRequests']>>['data']['items'][0];
+const mainRepo = new RancherDesktopRepository(GITHUB_OWNER, GITHUB_REPO);
 
 async function getLatestDockerCliVersion(): Promise<string> {
   const result = await getOctokit().rest.repos.listTags({
@@ -34,33 +32,7 @@ async function getLatestDockerCliVersion(): Promise<string> {
   return latestTag.name;
 }
 
-async function reopenIssue(issue: Issue): Promise<void> {
-  await getOctokit().rest.issues.update({
-    owner: GITHUB_OWNER, repo: GITHUB_REPO, issue_number: issue.number, state: 'open',
-  });
-  console.log(`Reopened issue #${ issue.number }: "${ issue.title }"`);
-}
-
-async function closeIssue(issue: Issue): Promise<void> {
-  await getOctokit().rest.issues.update({
-    owner: GITHUB_OWNER, repo: GITHUB_REPO, issue_number: issue.number, state: 'closed',
-  });
-  console.log(`Closed issue #${ issue.number }: "${ issue.title }"`);
-}
-
-async function createIssue(latestTagName: string): Promise<void> {
-  const title = `${ SCRIPT_NAME }: make rancher-desktop-docker-cli release for version ${ latestTagName }`;
-  const body = `The Docker CLI monitor has detected a new release of docker/cli.
-Please make a corresponding release in rancher-desktop-docker-cli to keep it up to date in Rancher Desktop.`;
-  const result = await getOctokit().rest.issues.create({
-    owner: GITHUB_OWNER, repo: GITHUB_REPO, title, body,
-  });
-  const issue = result.data;
-
-  console.log(`Created issue #${ issue.number }: "${ issue.title }"`);
-}
-
-async function getDockerCliIssues(): Promise<Issue[]> {
+async function getDockerCliIssues(): Promise<IssueOrPullRequest[]> {
   const query = `type:issue repo:${ GITHUB_OWNER }/${ GITHUB_REPO } sort:updated in:title ${ SCRIPT_NAME }`;
   const result = await getOctokit().rest.search.issuesAndPullRequests({ q: query });
 
@@ -89,14 +61,18 @@ async function checkDockerCli(): Promise<void> {
     if (issue.title.endsWith(` ${ latestTagName }`)) {
       issueFound = true;
       if (issue.state === 'closed') {
-        await reopenIssue(issue);
+        await mainRepo.reopenIssue(issue);
       }
     } else if (issue.state === 'open') {
-      await closeIssue(issue);
+      await mainRepo.closeIssue(issue);
     }
   }));
   if (!issueFound) {
-    await createIssue(latestTagName);
+    const title = `${ SCRIPT_NAME }: make rancher-desktop-docker-cli release for version ${ latestTagName }`;
+    const body = `The Docker CLI monitor has detected a new release of docker/cli. ` +
+      `Please make a corresponding release in rancher-desktop-docker-cli to keep it up to date in Rancher Desktop.`;
+
+    await mainRepo.createIssue(title, body);
   }
 }
 
