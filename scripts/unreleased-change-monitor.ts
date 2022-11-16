5b2e9a208992f87cde746bd8415e7e9f432f278c
Adam Pickering <adam.pickering@suse.com>
2022-11-16 10:28:03 -0700

    Refactor ucmonitor to use RancherDesktopRepository
    Signed-off-by: Adam Pickering <adam.pickering@suse.com>

--- scripts/unreleased-change-monitor.ts
+++ scripts/unreleased-change-monitor.ts
@@ -2,13 +2,16 @@ import { Octokit } from 'octokit';
 import { LimaAndQemu, AlpineLimaISO } from 'scripts/dependencies/lima';
 import * as tools from 'scripts/dependencies/tools';
 import { WSLDistro, HostResolverHost } from 'scripts/dependencies/wsl';
-import { Dependency, UnreleasedChangeMonitor, HasUnreleasedChangesResult, getOctokit } from 'scripts/lib/dependencies';
+import {
+  Dependency, UnreleasedChangeMonitor, HasUnreleasedChangesResult, getOctokit, RancherDesktopRepository,
+} from 'scripts/lib/dependencies';
 
 const GITHUB_OWNER = process.env.GITHUB_REPOSITORY?.split('/')[0] || 'rancher-sandbox';
 const GITHUB_REPO = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'rancher-desktop';
 // a (hopefully) unique and communicative key that is used to find issues created by
 // this script by filtering them down to the ones that have it in their title
 const UCMONITOR = 'ucmonitor';
+const mainRepo = new RancherDesktopRepository(GITHUB_OWNER, GITHUB_REPO);
 
 type UnreleasedChangeMonitoringDependency = Dependency & UnreleasedChangeMonitor;
 
@@ -32,32 +35,6 @@ async function getExistingIssuesFor(dependencyName: string): Promise<Issue[]> {
   const response = await getOctokit().rest.search.issuesAndPullRequests({ q: queryString });
 
   return response.data.items;
-}
-
-async function reopenIssue(issue: Issue): Promise<void> {
-  await getOctokit().rest.issues.update({
-    owner: GITHUB_OWNER, repo: GITHUB_REPO, issue_number: issue.number, state: 'open',
-  });
-  console.log(`Reopened issue #${ issue.number }: "${ issue.title }"`);
-}
-
-async function closeIssue(issue: Issue): Promise<void> {
-  await getOctokit().rest.issues.update({
-    owner: GITHUB_OWNER, repo: GITHUB_REPO, issue_number: issue.number, state: 'closed',
-  });
-  console.log(`Closed issue #${ issue.number }: "${ issue.title }"`);
-}
-
-async function createIssue(latestReleaseTag: string, dependency: UnreleasedChangeMonitoringDependency): Promise<void> {
-  const title = `${ UCMONITOR }: ${ dependency.name } has changes since ${ latestReleaseTag }`;
-  const body = `Unreleased Change Monitor has detected changes to ${ dependency.name } since its last release, ${ latestReleaseTag }.` +
-    `\n\nThis is a reminder to release these changes so they make it into the next Rancher Desktop release.`;
-  const response = await getOctokit().rest.issues.create({
-    owner: GITHUB_OWNER, repo: GITHUB_REPO, title, body,
-  });
-  const issue = response.data;
-
-  console.log(`Created issue #${ issue.number }: "${ issue.title }"`);
 }
 
 // Creates issues in the main Rancher Desktop repo for external
@@ -87,23 +64,27 @@ async function checkForUnreleasedChanges(): Promise<void> {
         if (existingIssue.state === 'closed' && issueTitleMatchesLatestReleaseTag) {
           // issue is closed, but it is the same as the one we would create; open it
           issueExists = true;
-          await reopenIssue(existingIssue);
+          await mainRepo.reopenIssue(existingIssue);
         } else if (existingIssue.state === 'open' && issueTitleMatchesLatestReleaseTag) {
           // we have an issue that is open that we want to be open
           issueExists = true;
         } else if (existingIssue.state === 'open' && !issueTitleMatchesLatestReleaseTag) {
           // this is an open issue that does not match this release; close it
-          await closeIssue(existingIssue);
+          await mainRepo.closeIssue(existingIssue);
         }
       }));
       if (!issueExists) {
-        await createIssue(dependencyState.latestReleaseTag, dependency);
+        const title = `${ UCMONITOR }: ${ dependency.name } has changes since ${ dependencyState.latestReleaseTag }`;
+        const body = `Unreleased Change Monitor has detected changes to ${ dependency.name } since its last release, ${ dependencyState.latestReleaseTag }.` +
+          `\n\nThis is a reminder to release these changes so they make it into the next Rancher Desktop release.`;
+
+        await mainRepo.createIssue(title, body);
       }
     } else {
       await Promise.all(existingIssues.map(async(existingIssue) => {
         if (existingIssue.state === 'open') {
           // there should be no open issues; close this one
-          await closeIssue(existingIssue);
+          await mainRepo.closeIssue(existingIssue);
         }
       }));
     }
