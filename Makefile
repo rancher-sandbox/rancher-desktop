1420aec67e251f29037c7866939ac870091ee633
Nino Kodabande <nkodabande@suse.com>
2023-02-22 13:25:18 -0800

    Handle the loop for tx and rx more gracefully
    Signed-off-by: Nino Kodabande <nkodabande@suse.com>

--- Makefile
+++ Makefile
@@ -1,13 +1,19 @@
 LDFLAGS = -ldflags '-s -w'
 
 .PHONY: build
-build: host-switch
+build: host-switch vm-switch
 
 bin/host-switch.exe:
 	GOOS=windows go build $(LDFLAGS) -o $@ ./cmd/host
 
 .PHONY: host-switch
 host-switch: bin/host-switch.exe
+
+bin/vm-switch:
+	GOOS=linux go build $(LDFLAGS) -o $@ ./cmd/vm
+
+.PHONY: vm-switch
+vm-switch: bin/vm-switch
 
 .PHONY: fmt
 fmt:
--- cmd/vm/switch_linux.go
+++ cmd/vm/switch_linux.go
@@ -14,6 +14,7 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"encoding/binary"
 	"flag"
 	"fmt"
@@ -21,6 +22,8 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/google/gopacket"
@@ -40,7 +43,7 @@ var (
 const (
 	defaultTapDevice = "eth0"
 	defaultMacAddr   = "5a:94:ef:e4:0c:ee"
-	defaultMTU       = 4000
+	maxMTU           = 4000
 )
 
 func main() {
@@ -55,18 +58,41 @@ func main() {
 	// the FD is passed-in as an extra arg from exec.Command
 	// of the parent process. This is for the AF_VSOCK connection that
 	// is handed over from the default namespace to Rancher Desktop's
-	// network namespace (rd1), the logic behind this approach was the
-	// lack of support for network namespaces in the AF_VSOCK libs.
-	connFile := os.NewFile(uintptr(3), "RancherDesktop-AFVsock-Connection")
-	defer connFile.Close()
+	// network namespace, the logic behind this approach is because
+	// AF_VSOCK is affected by network namespaces, therefore we need
+	// to open it before entering a new namespace (via unshare/nsenter)
+	connFile := os.NewFile(uintptr(3), "vsock connection")
+	connFile.Close()
 
 	logrus.Debugf("using a AF_VSOCK connection file from default namespace: %v", connFile)
 
 	// this should never happen
-	if err := checkForExsitingIf(tapIface); err != nil {
+	if err := checkForExistingIface(tapIface); err != nil {
 		logrus.Fatal(err)
 	}
 
+	// catch user issued signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	// try every second until we get DHCP
+	retryTicker := time.NewTicker(time.Second)
+	for {
+		ctx, cancel := context.WithCancel(context.Background())
+		select {
+		case s := <-sigChan:
+			logrus.Errorf("signal caught: %v", s)
+			cancel()
+			os.Exit(1)
+		case <-retryTicker.C:
+			if err := run(ctx, cancel, connFile); err != nil {
+				logrus.Error(err)
+			}
+		}
+	}
+}
+
+func run(ctx context.Context, cancel context.CancelFunc, connFile io.ReadWriteCloser) error {
 	tap, err := water.New(water.Config{
 		DeviceType: water.TAP,
 		PlatformSpecificParams: water.PlatformSpecificParams{
@@ -77,7 +103,9 @@ func main() {
 		logrus.Fatalf("creating tap device %v failed: %s", tapIface, err)
 	}
 	logrus.Debugf("created tap device %s: %v", tapIface, tap)
+
 	defer func() {
+		connFile.Close()
 		tap.Close()
 		logrus.Debugf("closed tap device: %s", tapIface)
 	}()
@@ -91,21 +119,13 @@ func main() {
 
 	logrus.Debugf("setup complete for tap interface %s(%s) + loopback", tapIface, defaultMacAddr)
 
-	for {
-		if err := run(connFile, tap); err != nil {
-			logrus.Error(err)
-		}
-		time.Sleep(time.Second)
-	}
-}
-
-func run(connFile *os.File, tap *water.Interface) error {
 	errCh := make(chan error, 1)
-	go tx(connFile, tap, errCh, defaultMTU)
-	go rx(connFile, tap, errCh, defaultMTU)
+	go tx(ctx, connFile, tap, errCh, maxMTU)
+	go rx(ctx, connFile, tap, errCh, maxMTU)
 	go func() {
-		if err := dhcp(tapIface); err != nil {
+		if err := dhcp(ctx, tapIface); err != nil {
 			errCh <- fmt.Errorf("dhcp error: %w", err)
+			cancel()
 		}
 	}()
 
@@ -141,93 +161,105 @@ func linkUp(iface, mac string) error {
 	return netlink.LinkSetUp(link)
 }
 
-func dhcp(iface string) error {
+func dhcp(ctx context.Context, iface string) error {
 	if _, err := exec.LookPath("udhcpc"); err == nil { // busybox dhcp client
-		cmd := exec.Command("udhcpc", "-f", "-q", "-i", iface, "-v")
+		cmd := exec.CommandContext(ctx, "udhcpc", "-f", "-q", "-i", iface, "-v")
 		cmd.Stderr = os.Stderr
 		cmd.Stdout = os.Stdout
 		return cmd.Run()
 	}
-	cmd := exec.Command("dhclient", "-4", "-d", "-v", iface)
+	cmd := exec.CommandContext(ctx, "dhclient", "-4", "-d", "-v", iface)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
 	return cmd.Run()
 }
 
-func rx(conn io.Writer, tap *water.Interface, errCh chan error, mtu int) {
+func rx(ctx context.Context, conn io.Writer, tap *water.Interface, errCh chan error, mtu int) {
 	logrus.Info("waiting for packets...")
 	var frame ethernet.Frame
 	for {
-		frame.Resize(mtu)
-		n, err := tap.Read([]byte(frame))
-		if err != nil {
-			errCh <- fmt.Errorf("reading packet from tap failed: %w", err)
+		select {
+		case <-ctx.Done():
+			logrus.Info("exiting rx goroutine")
 			return
-		}
-		frame = frame[:n]
+		default:
+			frame.Resize(mtu)
+			n, err := tap.Read([]byte(frame))
+			if err != nil {
+				errCh <- fmt.Errorf("reading packet from tap failed: %w", err)
+				return
+			}
+			frame = frame[:n]
 
-		size := make([]byte, 2)
-		binary.LittleEndian.PutUint16(size, uint16(n))
+			size := make([]byte, 2)
+			binary.LittleEndian.PutUint16(size, uint16(n))
 
-		if _, err := conn.Write(size); err != nil {
-			errCh <- fmt.Errorf("writing size to the socket failed: %w", err)
-			return
-		}
-		if _, err := conn.Write(frame); err != nil {
-			errCh <- fmt.Errorf("writing packet to the socket failed: %w", err)
-			return
-		}
+			if _, err := conn.Write(size); err != nil {
+				errCh <- fmt.Errorf("writing size to the socket failed: %w", err)
+				return
+			}
+			if _, err := conn.Write(frame); err != nil {
+				errCh <- fmt.Errorf("writing packet to the socket failed: %w", err)
+				return
+			}
 
-		if debug {
-			packet := gopacket.NewPacket(frame, layers.LayerTypeEthernet, gopacket.Default)
-			logrus.Infof("wrote packet (vm -> host %d): %s", size, packet.String())
+			if debug {
+				packet := gopacket.NewPacket(frame, layers.LayerTypeEthernet, gopacket.Default)
+				logrus.Infof("wrote packet (vm -> host %d): %s", size, packet.String())
+			}
 		}
 	}
 }
 
-func tx(conn io.Reader, tap *water.Interface, errCh chan error, mtu int) {
+func tx(ctx context.Context, conn io.Reader, tap *water.Interface, errCh chan error, mtu int) {
 	sizeBuf := make([]byte, 2)
 	buf := make([]byte, mtu+header.EthernetMinimumSize)
 
 	for {
-		n, err := io.ReadFull(conn, sizeBuf)
-		if err != nil {
-			errCh <- fmt.Errorf("reading size from socket failed: %w", err)
+		select {
+		case <-ctx.Done():
+			logrus.Info("exiting tx goroutine")
 			return
-		}
-		if n != 2 {
-			errCh <- fmt.Errorf("unexpected size %d", n)
-			return
-		}
-		size := int(binary.LittleEndian.Uint16(sizeBuf[0:2]))
+		default:
+			n, err := io.ReadFull(conn, sizeBuf)
+			if err != nil {
+				errCh <- fmt.Errorf("reading size from socket failed: %w", err)
+				return
+			}
+			if n != 2 {
+				errCh <- fmt.Errorf("unexpected size %d", n)
+				return
+			}
+			size := int(binary.LittleEndian.Uint16(sizeBuf[0:2]))
 
-		if cap(buf) < size {
-			buf = make([]byte, size)
-		}
+			if cap(buf) < size {
+				buf = make([]byte, size)
+			}
 
-		n, err = io.ReadFull(conn, buf[:size])
-		if err != nil {
-			errCh <- fmt.Errorf("reading payload from socket failed: %w", err)
-			return
-		}
-		if n == 0 || n != size {
-			errCh <- fmt.Errorf("unexpected size %d != %d", n, size)
-			return
-		}
+			n, err = io.ReadFull(conn, buf[:size])
+			if err != nil {
+				errCh <- fmt.Errorf("reading payload from socket failed: %w", err)
+				return
+			}
+			if n == 0 || n != size {
+				errCh <- fmt.Errorf("unexpected size %d != %d", n, size)
+				return
+			}
 
-		if _, err := tap.Write(buf[:size]); err != nil {
-			errCh <- fmt.Errorf("writing packet to tap failed: %w", err)
-			return
-		}
+			if _, err := tap.Write(buf[:size]); err != nil {
+				errCh <- fmt.Errorf("writing packet to tap failed: %w", err)
+				return
+			}
 
-		if debug {
-			packet := gopacket.NewPacket(buf[:size], layers.LayerTypeEthernet, gopacket.Default)
-			logrus.Infof("read packet (host -> vm %d): %s", size, packet.String())
+			if debug {
+				packet := gopacket.NewPacket(buf[:size], layers.LayerTypeEthernet, gopacket.Default)
+				logrus.Infof("read packet (host -> vm %d): %s", size, packet.String())
+			}
 		}
 	}
 }
 
-func checkForExsitingIf(ifName string) error {
+func checkForExistingIface(ifName string) error {
 	// equivalent to: `ip link show`
 	links, err := netlink.LinkList()
 	if err != nil {
