6d3715a35ad2a38e756fe14c8265ee4ada836cc0
Francesco Torchia <francesco.torchia@suse.com>
2023-03-02 15:23:04 +0100

    add missing rdNetworking rename
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/rancher-desktop/backend/kube/wsl.ts
+++ pkg/rancher-desktop/backend/kube/wsl.ts
@@ -228,7 +228,7 @@ export default class WSLKubernetesBackend extends events.EventEmitter implements
           }
           await util.promisify(timers.setTimeout)(1_000);
         }
-        const rdNetworking = `--rd-networking=${ config?.experimental.rdNetworking }`;
+        const rdNetworking = `--rd-networking=${ config?.experimental.virtualMachine.networkingTunnel }`;
 
         await this.k3sHelper.updateKubeconfig(
           async() => await this.vm.execCommand({ capture: true }, await this.vm.getWSLHelperPath(), 'k3s', 'kubeconfig', rdNetworking));
