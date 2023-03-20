431885fe664e20f8e7f33e391f8704932a88237d
Eric Promislow <epromislow@suse.com>
2023-03-20 15:39:53 -0700

    Distinguish the main-process log stream from the renderer-process stream.
    This technique works for both dev-builds, packages builds, and e2e test runs.
    
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- pkg/rancher-desktop/window/index.ts
+++ pkg/rancher-desktop/window/index.ts
@@ -12,7 +12,7 @@ import paths from '@pkg/utils/paths';
 import { CommandOrControl, Shortcuts } from '@pkg/utils/shortcuts';
 import { openPreferences } from '@pkg/window/preferences';
 
-const console = Logging[`window-${ process.pid }`];
+const console = Logging[`window_${ process.type || 'main' }`];
 
 /**
  * A mapping of window key (which is our own construct) to a window ID (which is
