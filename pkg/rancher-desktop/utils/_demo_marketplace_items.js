84260cda61685dfbb12edef5a1da6e92e5bc4116
scures <scurescu@suse.com>
2023-03-31 17:46:14 +0200

    feat: init marketplace
    Signed-off-by: scures <scurescu@suse.com>

---
+++ pkg/rancher-desktop/utils/_demo_marketplace_items.js
@@ -0,0 +1,29 @@
+export const demoMarketplace = {
+  summaries: [
+    {
+      id:                 '',
+      name:               'Epinio',
+      slug:               'splatform/epinio-docker-desktop',
+      type:               'extension',
+      publisher:          { name: 'Rancher by SUSE' },
+      created_at:         '2022-05-06T00:44:00Z',
+      updated_at:         '2023-02-24T10:40:43.666252815Z',
+      short_description:  'Push from source to Kubernetes in one step',
+      source:             'community',
+      extension_reviewed: true,
+      popularity:         0,
+      categories:         null,
+      operating_systems:  [],
+      architectures:      [],
+      logo_url:           {
+        large:
+          'https://desktop.docker.com/extensions/splatform_epinio-docker-desktop/epinio_io/images/icon-epinio.svg',
+        small:
+          'https://desktop.docker.com/extensions/splatform_epinio-docker-desktop/epinio_io/images/icon-epinio.svg',
+      },
+      certification_status: '',
+      star_count:           0,
+      filter_type:          'community',
+    },
+  ],
+};
