338d2997f3c6d37d90479202348be0186a379e3b
Mark Yen <mark.yen@suse.com>
2023-04-04 14:51:09 -0700

    RDX: Address review comments
    - Move ddClient.extension.vm.service.request implementation to method
      https://github.com/rancher-sandbox/rancher-desktop/pull/4361#discussion_r1157742269
    - Add explicit return type on makeRequest
      https://github.com/rancher-sandbox/rancher-desktop/pull/4361#discussion_r1157747943
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- pkg/rancher-desktop/preload/extensions.ts
+++ pkg/rancher-desktop/preload/extensions.ts
@@ -261,37 +261,39 @@ class Client implements v1.DockerDesktopClient {
    * makeRequest is a helper for ddClient.extension.vm.service.<HTTP method>
    * that wraps ddClient.extension.vm.service.request().
    */
-  protected makeRequest(method: string, url: string, data?: any) {
-    return this.extension.vm?.service?.request({
+  protected makeRequest(method: string, url: string, data?: any): Promise<unknown> {
+    return this.request({
       method, url, data, headers: {},
-    }) ?? Promise.reject(`Failed to ${ method } ${ url }: properties missing`);
+    });
+  }
+
+  protected async request(config: v1.RequestConfig): Promise<unknown> {
+    try {
+      const result = await ipcRenderer.invoke('extensions/vm/http-fetch', config);
+
+      // Parse as JSON if possible (API is unclear).
+      try {
+        return JSON.parse(result);
+      } catch {
+        return result;
+      }
+    } catch (ex) {
+      console.debug(`${ config.method } ${ config.url } error:`, ex);
+      throw ex;
+    }
   }
 
   extension: v1.Extension = {
     vm: {
       cli:     { exec: getExec('container') },
       service: {
-        request: async(config: v1.RequestConfig): Promise<unknown> => {
-          try {
-            const result = await ipcRenderer.invoke('extensions/vm/http-fetch', config);
-
-            // Parse as JSON if possible (API is unclear).
-            try {
-              return JSON.parse(result);
-            } catch {
-              return result;
-            }
-          } catch (ex) {
-            console.debug(`${ config.method } ${ config.url } error:`, ex);
-            throw ex;
-          }
-        },
-        get:    (url: string) => this.makeRequest('GET', url),
-        post:   (url: string, data: any) => this.makeRequest('POST', url, data),
-        put:    (url: string, data: any) => this.makeRequest('PUT', url, data),
-        patch:  (url: string, data: any) => this.makeRequest('PATCH', url, data),
-        delete: (url: string) => this.makeRequest('DELETE', url),
-        head:   (url: string) => this.makeRequest('HEAD', url),
+        request: (config: v1.RequestConfig) => this.request(config),
+        get:     (url: string) => this.makeRequest('GET', url),
+        post:    (url: string, data: any) => this.makeRequest('POST', url, data),
+        put:     (url: string, data: any) => this.makeRequest('PUT', url, data),
+        patch:   (url: string, data: any) => this.makeRequest('PATCH', url, data),
+        delete:  (url: string) => this.makeRequest('DELETE', url),
+        head:    (url: string) => this.makeRequest('HEAD', url),
       },
     },
     host:  { cli: { exec: getExec('host') } },
