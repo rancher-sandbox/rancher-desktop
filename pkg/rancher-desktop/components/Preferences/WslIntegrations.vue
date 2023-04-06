d2994f7147bb30f210bd28d88bdcdc2c27831aa6
Tatjana Dehler <tdehler@suse.com>
2023-04-06 16:20:11 +0200

    Add WSL -> Integrations tab
    Add Integrations tab and move WSL settings into it.
    
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

---
+++ pkg/rancher-desktop/components/Preferences/WslIntegrations.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+
+import Vue from 'vue';
+import { mapGetters } from 'vuex';
+
+import WslIntegration from '@pkg/components/WSLIntegration.vue';
+import RdFieldset from '@pkg/components/form/RdFieldset.vue';
+import { Settings } from '@pkg/config/settings';
+import { RecursiveTypes } from '@pkg/utils/typeUtils';
+
+import type { PropType } from 'vue';
+
+export default Vue.extend({
+  name:       'preferences-wsl-integrations',
+  components: { WslIntegration, RdFieldset },
+  props:      {
+    preferences: {
+      type:     Object as PropType<Settings>,
+      required: true,
+    },
+  },
+  computed: { ...mapGetters('preferences', ['getWslIntegrations']) },
+  methods:  {
+    onChange(distro: string, value: boolean) {
+      const property: keyof RecursiveTypes<Settings> = `WSL.integrations["${ distro }"]` as any;
+
+      this.$store.dispatch('preferences/updateWslIntegrations', { distribution: `["${ distro }"]`, value });
+      this.$store.dispatch('preferences/updatePreferencesData', { property, value });
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="wsl-integrations">
+    <rd-fieldset
+      :legend-text="t('integrations.windows.description', { }, true)"
+    >
+      <wsl-integration
+        :integrations="getWslIntegrations"
+        @integration-set="onChange"
+      />
+    </rd-fieldset>
+  </div>
+</template>
+
+<style scoped>
+  .wsl-integrations {
+    padding: var(--preferences-content-padding);
+  }
+</style>
