8cc9f744b95ac6e72fa5259dcab548544ffaa29e
Phillip Rak <rak.phillip@gmail.com>
2023-04-07 08:44:22 -0700

    Move marketplace catalog into new component
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-desktop/pages/Marketplace.vue
+++ pkg/rancher-desktop/pages/Marketplace.vue
@@ -1,50 +1,23 @@
 <template>
   <div class="extensions-page">
-    <nuxt-child />
-    <input v-model="searchValue" type="text" placeholder="Search" />
-    <div v-if="filteredExtensions.length === 0" class="extensions-content-missing">
-      {{ t('marketplace.noResults') }}
-    </div>
-    <div class="extensions-content">
-      <div v-for="item in filteredExtensions" :key="item.slug" :v-if="filteredExtensions">
-        <MarketplaceCard :extension="item" />
-      </div>
-    </div>
+    <marketplace-catalog />
   </div>
 </template>
 
 <script>
-import { demoMarketplace } from '../utils/_demo_marketplace_items.js';
 
-import MarketplaceCard from '@pkg/components/MarketplaceCard.vue';
+import MarketplaceCatalog from '@pkg/components/MarketplaceCatalog.vue';
 import { defaultSettings } from '@pkg/config/settings';
 
 export default {
-  components: { MarketplaceCard },
   title:      'Marketplace',
+  components: { MarketplaceCatalog },
   data() {
     return {
       settings:           defaultSettings,
-      extensions:         demoMarketplace.summaries.slice(0, 2),
       imageNamespaces:    [],
       supportsNamespaces: true,
-      searchValue:        '',
     };
-  },
-  computed: {
-    filteredExtensions() {
-      let tempExtensions = this.extensions;
-
-      if (this.searchValue) {
-        tempExtensions = tempExtensions.filter((item) => {
-          return item.name
-            .toLowerCase()
-            .includes(this.searchValue.toLowerCase());
-        });
-      }
-
-      return tempExtensions;
-    },
   },
   mounted() {
     this.$store.dispatch('page/setHeader', {
