4cd34e9c8a84a87fc079820661b3b8c04a2723c0
Phillip Rak <rak.phillip@gmail.com>
2023-04-11 11:30:00 -0700

    Use versioned extension to check `isInstalled`
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

--- pkg/rancher-desktop/pages/marketplace/details.vue
+++ pkg/rancher-desktop/pages/marketplace/details.vue
@@ -127,7 +127,7 @@ export default {
       }
 
       response.json().then((res) => {
-        this.isInstalled = Object.keys(res).includes(this.extension);
+        this.isInstalled = Object.keys(res).includes(this.versionedExtension);
       });
     });
   },
