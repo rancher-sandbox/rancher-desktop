95c4f018d13540cf09b3b2109869c7fc730f7a56
Tatjana Dehler <tdehler@suse.com>
2023-04-19 10:58:50 +0200

    Add badge form component
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

---
+++ pkg/rancher-desktop/components/form/LabeledBadge.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+
+import { BadgeState } from '@rancher/components';
+import Vue from 'vue';
+
+import type { PropType } from 'vue';
+
+export default Vue.extend({
+  name:       'labeled-badge',
+  components: { BadgeState },
+  props:      {
+    color: {
+      type: String as PropType<'bg-primary' | 'bg-default' | 'bg-darker' | 'bg-success' | 'bg-info' | 'bg-warning'
+        | 'bg-error'>,
+      default: 'bg-darker',
+    },
+    text: {
+      type:     String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <badge-state
+    class="badge"
+    :color="color"
+    :label="text"
+  />
+</template>
+
+<style lang="scss" scoped>
+  .badge {
+    line-height: initial;
+    letter-spacing: initial;
+    font-size: 10px;
+  }
+</style>
