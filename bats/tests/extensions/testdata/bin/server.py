56411ab19934b621175a5a687a32e5a008c32cf5
Mark Yen <mark.yen@suse.com>
2023-04-21 16:25:19 -0700

    BATS: fix extensions test image
    When we're _not_ using the backend socket, the test no longer started
    because the mount point is missing.  Fix things by checking that case.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>
    Fixes: c087f2b2b48bee3a9b11099ddc29fe852181e42d

--- bats/tests/extensions/testdata/bin/server.py
+++ bats/tests/extensions/testdata/bin/server.py
@@ -4,35 +4,56 @@
 # `/run/guest-services/hello.sock` (see `everything.json`) to exercise the
 # ability for the front end to talk to the back end.
 
+from functools import partial
 from http import server
 import os
+import signal
 import socketserver
+import sys
 
 
 class RequestHandler(server.SimpleHTTPRequestHandler):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs, directory="/")
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs, directory="/")
 
-  def address_string(self):
-    # SimpleHTTPRequestHandler assumes TCP; fix up the client address if it's
-    # just a string (because Unix socket).
-    if isinstance(self.client_address, str):
-      self.client_address = [self.client_address, 0]
-    return super().address_string()
+    def address_string(self):
+        # SimpleHTTPRequestHandler assumes TCP; fix up the client address if it's
+        # just a string (because Unix socket).
+        if isinstance(self.client_address, str):
+            self.client_address = [self.client_address, 0]
+        return super().address_string()
+
+
+def make_unix_socket_server():
+    addr = '/run/guest-services/hello.sock'
+    try:
+        os.unlink(addr)
+    except Exception as ex:
+        print(ex)
+    httpd = socketserver.ThreadingUnixStreamServer(addr, RequestHandler)
+    return (addr, httpd)
+
+
+def make_tcp_server():
+    handler_class = partial(server.SimpleHTTPRequestHandler,
+                            directory="/")
+    httpd = server.ThreadingHTTPServer(("", 0), handler_class)
+    host, port = httpd.socket.getsockname()[:2]
+    addr = f"http://{host}:{port}"
+    return (addr, httpd)
 
 
 if __name__ == '__main__':
-  addr = '/run/guest-services/hello.sock'
-  try:
-    os.unlink(addr)
-  except Exception as ex:
-    print(ex)
-
-  print("Starting HTTP server...")
-  with socketserver.ThreadingUnixStreamServer(addr, RequestHandler) as httpd:
-    print(f"Serving HTTP on {addr}")
+    signal.signal(signal.SIGTERM, lambda *args: sys.exit(0))
+    signal.signal(signal.SIGINT, lambda *args: sys.exit(0))
     try:
-      httpd.serve_forever()
-    except KeyboardInterrupt:
-      print("\nexiting...")
-      pass
+        print("Starting HTTP server...")
+        try:
+            addr, httpd = make_unix_socket_server()
+        except FileNotFoundError:
+            addr, httpd = make_tcp_server()
+        print(f"Serving HTTP on {addr}")
+        httpd.serve_forever()
+
+    except SystemExit:
+        print("\nexiting...")
