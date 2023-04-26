99766058098632d090cf6566b6620ae09eeb0c37
Mark Yen <mark.yen@suse.com>
2023-04-26 13:05:06 -0700

    BATS: wait_for_apiserver: add timeout
    This ensures we fail the test in a reasonable amount of time (currently
    always ten minutes).  If it takes longer it's probably stuck.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- bats/tests/helpers/kubernetes.bash
+++ bats/tests/helpers/kubernetes.bash
@@ -1,8 +1,12 @@
 wait_for_apiserver() {
     local desired_version="${1:-$RD_KUBERNETES_PREV_VERSION}"
+    local timeout="$(($(date +%s) + 10 * 60))"
     while true; do
-        until kubectl get --raw /readyz &>/dev/null; do sleep 1; done
-        sleep 1
+        until kubectl get --raw /readyz &>/dev/null; do
+            assert [ "$(date +%s)" -lt "${timeout}" ]
+            sleep 1
+        done
+        assert [ "$(date +%s)" -lt "${timeout}" ]
         run kubectl get node -o jsonpath="{.items[0].status.nodeInfo.kubeletVersion}"
         if [ "$status" -eq 0 ]; then
             # Turn "v1.23.4+k3s1" into "1.23.4"
