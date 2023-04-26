44fc012ec3f2ee8d55940cdb11427fc03962c8a5
Phillip Rak <rak.phillip@gmail.com>
2023-04-26 14:21:13 -0700

    Create component for displaying an extension uninstalled message
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ pkg/rancher-desktop/components/ExtensionsUninstalled.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+import Vue from 'vue';
+
+import EmptyState from '@pkg/components/EmptyState.vue';
+
+export default Vue.extend({
+  name:       'extensions-uninstalled',
+  components: { EmptyState },
+  props:      {
+    extensionId: {
+      required: true,
+      type:     String,
+    },
+  },
+  computed: {
+    emptyStateIcon(): string {
+      return this.t('extensions.icon');
+    },
+    emptyStateHeading(): string {
+      return this.t('extensions.view.emptyState.heading');
+    },
+    emptyStateBody(): string {
+      return this.t('extensions.view.emptyState.body');
+    },
+  },
+  methods: {
+    browseExtensions() {
+      this.$emit('click:browse');
+    },
+  },
+});
+</script>
+
+<template>
+  <empty-state
+    :icon="emptyStateIcon"
+    :heading="emptyStateHeading"
+  >
+    <template #body>
+      <code>{{ extensionId }}</code> {{ emptyStateBody }}
+    </template>
+    <template #primary-action>
+      <button
+        class="btn role-primary"
+        @click="browseExtensions"
+      >
+        {{ t('extensions.installed.emptyState.button.text') }}
+      </button>
+    </template>
+  </empty-state>
+</template>
