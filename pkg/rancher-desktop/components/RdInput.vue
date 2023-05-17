b62db911ae544d808db18c4c2f26398001f40224
Phillip Rak <rak.phillip@gmail.com>
2023-05-17 13:26:34 -0700

    Create lockable `rd-input` wrapper component
    Signed-off-by: Phillip Rak <rak.phillip@gmail.com>

---
+++ pkg/rancher-desktop/components/RdInput.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name:         'rd-input',
+  inheritAttrs: false,
+  props:        {
+    value: {
+      type:    [String, Number],
+      default: null,
+    },
+    isLocked: {
+      type:    Boolean,
+      default: false,
+    },
+    tooltip: {
+      type:    String,
+      default: null,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="rd-input-container">
+    <input
+      :value="value"
+      :disabled="$attrs.disabled || isLocked"
+      v-bind="$attrs"
+      v-on="$listeners"
+    />
+    <slot name="after">
+      <i
+        v-if="isLocked"
+        v-tooltip="{
+          content: tooltip || t('preferences.locked.tooltip'),
+          placement: 'right'
+        }"
+        class="icon icon-lock icon-lg"
+      />
+    </slot>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .rd-input-container {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+</style>
