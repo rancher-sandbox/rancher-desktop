c31a714c7db5b7f8a3cb9a4c673f7a714b1dd62c
Jan Dubois <jan.dubois@suse.com>
2023-05-26 18:56:20 -0700

    Add scripts/install-latest-ci.sh
    The script will find the latest successful build from a branch (`main`
    by default), and install it locally in user mode.
    
    Then it will download the corresponding BATS tests and install them into
    /tmp/bats.
    
    Currently only works on macOS.
    
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

---
+++ scripts/install-latest-ci.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+set -o xtrace
+
+OWNER=rancher-sandbox
+REPO=rancher-desktop
+BRANCH=main
+WORKFLOW=Package
+RESULTS=30
+
+# Get a list of all successful workflow runs for this repo.
+# I couldn't find a way to filter by workflow name, so get the last $RESULTS
+# runs and hope that at least one of them was for $WORKFLOW.
+API="repos/$OWNER/$REPO/actions/runs?branch=$BRANCH&status=success&per_page=$RESULTS"
+FILTER="[.workflow_runs[] | select(.name == \"$WORKFLOW\")] | .[0].id"
+
+ID=$(gh api "$API" --jq "$FILTER")
+if [ -z "$ID" ]; then
+    echo "No successful $WORKFLOW run found"
+    exit 1
+fi
+
+download_artifact() {
+    FILENAME=$1
+
+    # Get the artifact id for the package
+    API="repos/$OWNER/$REPO/actions/runs/$ID/artifacts"
+    FILTER=".artifacts[] | select(.name == \"$FILENAME\").id"
+
+    ARTIFACT_ID=$(gh api "$API" --jq "$FILTER")
+    if [ -z "$ARTIFACT_ID" ]; then
+        echo "No download url for '$FILENAME' from $WORKFLOW run $ID"
+        exit 1
+    fi
+
+    # Download the package. It requires authentication, so use gh instead of curl.
+    API=repos/$OWNER/$REPO/actions/artifacts/$ARTIFACT_ID/zip
+    gh api "$API" > "/tmp/$FILENAME"
+}
+
+download_artifact "Rancher Desktop-mac.x86_64.zip"
+
+# Artifacts are zipped, so extract inner ZIP file from outer wrapper.
+# The outer ZIP has a predictable name like "Rancher Desktop-mac.x86_64.zip"
+# but the inner one has version string: "Rancher Desktop-1.7.0-1061-g91ab3831-mac.zip"
+# Extract filename from:
+# ---------------------------------------------------------------------------
+# $ unzip -l Rancher\ Desktop-mac.x86_64.zip
+# Archive:  Rancher Desktop-mac.x86_64.zip
+#   Length      Date    Time    Name
+# ---------  ---------- -----   ----
+# 617701842  04-18-2023 23:56   Rancher Desktop-1.7.0-1061-g91ab3831-mac.zip
+# ---------                     -------
+# 617701842                     1 file
+# ---------------------------------------------------------------------------
+ZIP=$(unzip -l "/tmp/$FILENAME" | perl -ne 'print if s/^\d.*Rancher/Rancher/')
+if [ -z "$ZIP" ]; then
+    echo "Cannot find inner archive in /tmp/$FILENAME"
+    exit 1
+fi
+
+unzip -o "/tmp/$FILENAME" "$ZIP" -d /tmp
+
+# Extract from inner archive into ~/Applications
+APP="Rancher Desktop.app"
+rm -rf "$HOME/Applications/$APP"
+unzip -o "/tmp/$ZIP" "$APP/*" -d $HOME/Applications >/dev/null
+
+download_artifact "bats.tar.gz"
+
+# Despite the name, the downloaded bats.tar.gz is actually a ZIP file
+unzip -o /tmp/bats.tar.gz bats.tar.gz -d /tmp
+
+# Unpack bats into /tmp/bats
+rm -rf /tmp/bats
+mkdir /tmp/bats
+tar xfz /tmp/bats.tar.gz -C /tmp/bats
