178ed3726b18e7dda20e33e36d62801b0e7a894e
Francesco Torchia <francesco.torchia@suse.com>
2023-05-29 10:11:08 +0200

    Update screenshots for new WSL tabs
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- e2e/pages/preferences/wsl.ts
+++ e2e/pages/preferences/wsl.ts
@@ -4,9 +4,11 @@ export class WslNav {
   readonly page: Page;
   readonly nav: Locator;
   readonly networkingTunnel: Locator;
+  readonly wslIntegrations: Locator;
+  readonly addressTitle: Locator;
   readonly tabIntegrations: Locator;
   readonly tabNetwork: Locator;
-  readonly wslIntegrations: Locator;
+  readonly tabProxy: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -14,6 +16,8 @@ export class WslNav {
     this.networkingTunnel = page.locator('[data-test="networkingTunnel"]');
     this.tabIntegrations = page.locator('.tab >> text=Integrations');
     this.tabNetwork = page.locator('.tab >> text=Network');
+    this.tabProxy = page.locator('.tab >> text=Proxy');
     this.wslIntegrations = page.locator('[data-test="wslIntegrations"]');
+    this.addressTitle = page.locator('[data-test="addressTitle"]');
   }
 }
