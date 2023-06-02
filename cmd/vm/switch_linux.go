f196864c07435b06c5735d5025ea4f67199ac91a
Nino Kodabande <nkodabande@suse.com>
2023-06-02 15:52:13 -0700

    VM-Switch no longer creates iptable rules
    We only require these rules for the moby backend,
    therefore, the rule creation has moved to the guest agent.
    
    Signed-off-by: Nino Kodabande <nkodabande@suse.com>

--- cmd/vm/switch_linux.go
+++ cmd/vm/switch_linux.go
@@ -47,10 +47,8 @@ var (
 )
 
 const (
-	defaultTapDevice        = "eth0"
-	maxMTU                  = 4000
-	appendToChain    string = "append"
-	deleteFromChain  string = "delete"
+	defaultTapDevice = "eth0"
+	maxMTU           = 4000
 )
 
 func main() {
@@ -122,9 +120,6 @@ func run(ctx context.Context, cancel context.CancelFunc, connFile io.ReadWriteCl
 
 	defer func() {
 		connFile.Close()
-		if err := loopbackRules(deleteFromChain, subnet); err != nil {
-			logrus.Errorf("clearing iptable rules failed: %s", err)
-		}
 		tap.Close()
 		logrus.Debugf("closed tap device: %s", tapIface)
 	}()
@@ -134,9 +129,6 @@ func run(ctx context.Context, cancel context.CancelFunc, connFile io.ReadWriteCl
 	}
 	if err := loopbackUp(); err != nil {
 		logrus.Fatalf("enabling loop back device failed: %s", err)
-	}
-	if err := loopbackRules(appendToChain, subnet); err != nil {
-		logrus.Fatalf("setting up forwarding iptables rules for loopback interface failed: %s", err)
 	}
 
 	logrus.Debugf("setup complete for tap interface %s(%s) + loopback", tapIface, tapDeviceMacAddr)
@@ -152,34 +144,6 @@ func run(ctx context.Context, cancel context.CancelFunc, connFile io.ReadWriteCl
 	}()
 
 	return <-errCh
-}
-
-func loopbackRules(chainOP, subnet string) error {
-	chainOP = fmt.Sprintf("--%s", chainOP)
-	ip, _, err := net.ParseCIDR(subnet)
-	if err != nil {
-		return err
-	}
-	tapDeviceIP := config.TapDeviceIP(ip.To4())
-	rules := map[string][]string{
-		"PREROUTING":  {"--table", "nat", chainOP, "PREROUTING", "--destination", tapDeviceIP, "--jump", "DNAT", "--to-destination", "127.0.0.1"},
-		"POSTROUTING": {"--table", "nat", chainOP, "POSTROUTING", "--out-interface", tapIface, "--jump", "MASQUERADE"},
-	}
-
-	for key, args := range rules {
-		logrus.Debugf("running %s iptable rules: iptables %s", key, args)
-		if err := execIPTable(args); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func execIPTable(args []string) error {
-	cmd := exec.Command("iptables", args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
 }
 
 func loopbackUp() error {
