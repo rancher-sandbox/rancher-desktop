7e9532afb990ef97d1c7a1139f94e752baa810e9
Mark Yen <mark.yen@suse.com>
2023-06-09 18:13:46 -0700

    ipcMainProxy: fix unregistering listeners
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- pkg/rancher-desktop/main/ipcMain.ts
+++ pkg/rancher-desktop/main/ipcMain.ts
@@ -62,35 +62,103 @@ interface IpcMainProxy {
   removeHandler<eventName extends keyof IpcMainInvokeEvents>(channel: eventName): void;
 }
 
-export function getIpcMainProxy(logger: Log) {
-  return new Proxy(Electron.ipcMain, {
-    get: (target, property) => {
-      if (property === 'on') {
-        return (channel: string, listener: (event: Electron.IpcMainEvent, ...args: any[]) => void) => {
-          const newListener = (event: Electron.IpcMainEvent, ...args: any[]) => {
-            const printableArgs = makeArgsPrintable(args);
+type Listener = (event: Electron.IpcMainEvent, ...args: any) => void;
+type Handler = (event: Electron.IpcMainInvokeEvent, ...args: any) => Promise<unknown>;
 
-            logger.debug(`ipcMain: "${ channel }" triggered with arguments: ${ printableArgs.join(', ') }`);
-            listener(event, ...args);
-          };
+class IpcMainProxyImpl implements IpcMainProxy {
+  constructor(logger: Log) {
+    this.logger = logger;
+  }
 
-          return target[property](channel, newListener);
-        };
-      } else if (property === 'handle') {
-        return (channel: string, listener: (event: Electron.IpcMainInvokeEvent, ...args: any[]) => any) => {
-          const newListener = (event: Electron.IpcMainInvokeEvent, ...args: any[]): any => {
-            const printableArgs = makeArgsPrintable(args);
+  protected logger: Log;
 
-            logger.debug(`ipcMain: "${ channel }" handle called with: ${ printableArgs.join(', ') }`);
+  // Bijective weak maps between the user-provided listener and the wrapper that
+  // introduces logging.  We do not keep strong references to either; the user-
+  // provided listener is only kept alive by the wrapper, which the underlying
+  // IpcMain has a strong reference to.
+  protected listenerWrapperToRaw = new WeakMap<Listener, WeakRef<Listener>>();
+  protected listenerRawToWrapper = new WeakMap<Listener, WeakRef<Listener>>();
 
-            return listener(event, ...args);
-          };
+  on(channel: string, listener: Listener): this {
+    const wrapper: Listener = (event, ...args) => {
+      const printableArgs = makeArgsPrintable(args);
 
-          target[property](channel, newListener);
-        };
-      }
+      this.logger.debug(`ipcMain: "${ channel }" triggered with arguments: ${ printableArgs.join(', ') }`);
+      listener(event, ...args);
+    };
 
-      return Reflect.get(target, property);
-    },
-  }) as IpcMainProxy;
+    this.listenerWrapperToRaw.set(wrapper, new WeakRef(listener));
+    this.listenerRawToWrapper.set(listener, new WeakRef(wrapper));
+    Electron.ipcMain.on(channel, wrapper);
+
+    return this;
+  }
+
+  once(channel: string, listener: Listener): this {
+    const wrapper: Listener = (event, ...args) => {
+      const printableArgs = makeArgsPrintable(args);
+
+      this.logger.debug(`ipcMain: "${ channel }" triggered with arguments: ${ printableArgs.join(', ') }`);
+      listener(event, ...args);
+    };
+
+    this.listenerWrapperToRaw.set(wrapper, new WeakRef(listener));
+    this.listenerRawToWrapper.set(listener, new WeakRef(wrapper));
+    Electron.ipcMain.once(channel, wrapper);
+
+    return this;
+  }
+
+  removeListener(channel: string, listener: Listener): this {
+    const wrapper = this.listenerRawToWrapper.get(listener)?.deref();
+
+    if (wrapper) {
+      Electron.ipcMain.removeListener(channel, wrapper);
+      this.listenerWrapperToRaw.delete(wrapper);
+    }
+    this.listenerRawToWrapper.delete(listener);
+
+    return this;
+  }
+
+  removeAllListeners(channel?: string): this {
+    Electron.ipcMain.removeAllListeners(channel);
+
+    return this;
+  }
+
+  // For dealing with handlers, we don't need to keep track of the wrappers
+  // (because removeHandler() doesn't actually take the handler to remove).
+
+  handle(channel: string, handler: Handler): void {
+    const wrapper: Handler = (event, ...args) => {
+      const printableArgs = makeArgsPrintable(args);
+
+      this.logger.debug(`ipcMain: "${ channel }" handle called with: ${ printableArgs.join(', ') }`);
+
+      return handler(event, ...args);
+    };
+
+    Electron.ipcMain.handle(channel, wrapper);
+  }
+
+  handleOnce(channel: string, handler: Handler) {
+    const wrapper: Handler = (event, ...args) => {
+      const printableArgs = makeArgsPrintable(args);
+
+      this.logger.debug(`ipcMain: "${ channel }" handle called with: ${ printableArgs.join(', ') }`);
+
+      return handler(event, ...args);
+    };
+
+    Electron.ipcMain.handleOnce(channel, wrapper);
+  }
+
+  removeHandler(channel: string): void {
+    Electron.ipcMain.removeHandler(channel);
+  }
+}
+
+export function getIpcMainProxy(logger: Log): IpcMainProxy {
+  return new IpcMainProxyImpl(logger);
 }
