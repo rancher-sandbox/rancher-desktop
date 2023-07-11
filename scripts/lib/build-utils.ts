3d7efd5ad984e203efa6256b8c6c5191b604ae9d
Eric Promislow <epromislow@suse.com>
2023-07-11 12:19:00 -0700

    Fix buildGoUtilities signature.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- scripts/lib/build-utils.ts
+++ scripts/lib/build-utils.ts
@@ -462,7 +462,7 @@ export default {
   /**
    * Build the things we build with go
    */
-  async buildGoUtilities(): Promise<void> {
+  async buildGoUtilities() {
     const tasks = [];
 
     if (os.platform().startsWith('win')) {
@@ -478,7 +478,7 @@ export default {
     tasks.push(() => this.buildUtility('docker-credential-none', os.platform(), 'bin'));
     tasks.push(() => this.buildExtensionProxyImage());
 
-    return this.wait(...tasks);
+    return await this.wait(...tasks);
   },
 
 };
