34897593b852b67d338f44597deef5714a705561
Eric Promislow <epromislow@suse.com>
2023-07-13 12:18:15 -0700

    Fix the applicationSettings store: adminAccess not used here.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- pkg/rancher-desktop/store/applicationSettings.ts
+++ pkg/rancher-desktop/store/applicationSettings.ts
@@ -1,37 +1,29 @@
+import _ from 'lodash';
+
 import { ActionContext, MutationsType } from './ts-helpers';
 
-import type { defaultSettings, Settings } from '@pkg/config/settings';
-import { PathManagementStrategy } from '@pkg/integrations/pathManager';
+import { defaultSettings } from '@pkg/config/settings';
+import type { PathManagementStrategy } from '@pkg/integrations/pathManager';
 import { ipcRenderer } from '@pkg/utils/ipcRenderer';
-
-let pathManagementStrategy = PathManagementStrategy.RcFiles;
-let adminAccess = false;
 
 /**
  * State is the type of the state we are maintaining in this store.
  */
 type State = {
   pathManagementStrategy: PathManagementStrategy;
-  adminAccess: boolean;
 };
+
+const cfg = _.cloneDeep(defaultSettings);
 
 export const state: () => State = () => {
   // While we load the settings from disk here, we only otherwise interact with
   // the settings only via ipcRenderer.
-  const cfg = getSettings();
-
-  return {
-    pathManagementStrategy: cfg.application.pathManagementStrategy,
-    adminAccess:            cfg.application.adminAccess,
-  };
+  return { pathManagementStrategy: cfg.application.pathManagementStrategy };
 };
 
 export const mutations: MutationsType<State> = {
   SET_PATH_MANAGEMENT_STRATEGY(state: State, strategy: PathManagementStrategy) {
     state.pathManagementStrategy = strategy;
-  },
-  SET_ADMIN_ACCESS(state: State, allowed: boolean) {
-    state.adminAccess = allowed;
   },
 } as const;
 
@@ -44,27 +36,11 @@ export const actions = {
   async commitPathManagementStrategy({ commit }: AppActionContext, strategy: PathManagementStrategy) {
     commit('SET_PATH_MANAGEMENT_STRATEGY', strategy);
     await ipcRenderer.invoke('settings-write', { application: { pathManagementStrategy: strategy } });
-    ipcRenderer.send('settings-read');
-  },
-  setAdminAccess({ commit, state }: AppActionContext, allowed: boolean) {
-    if (allowed !== state.adminAccess) {
-      commit('SET_ADMIN_ACCESS', allowed);
-    }
-  },
-  async commitAdminAccess({ commit, state }: AppActionContext, allowed: boolean) {
-    if (allowed !== state.adminAccess) {
-      commit('SET_ADMIN_ACCESS', allowed);
-      await ipcRenderer.invoke('settings-write', { application: { adminAccess: allowed } });
-      ipcRenderer.send('settings-read');
-    }
   },
 };
 
 export const getters = {
   pathManagementStrategy({ pathManagementStrategy }: State) {
     return pathManagementStrategy;
-  },
-  adminAccess({ adminAccess }: State) {
-    return adminAccess;
   },
 };
