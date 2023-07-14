1b00adc26d9597456ba68ecae1c7ae034967bee2
Eric Promislow <epromislow@suse.com>
2023-07-14 13:31:39 -0700

    Fix the default-memory problem.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- pkg/rancher-desktop/main/commandServer/__tests__/settingsValidator.spec.ts
+++ pkg/rancher-desktop/main/commandServer/__tests__/settingsValidator.spec.ts
@@ -7,6 +7,7 @@ import _ from 'lodash';
 import SettingsValidator from '../settingsValidator';
 
 import * as settings from '@pkg/config/settings';
+import { getDefaultMemory } from '@pkg/config/settingsImpl';
 import { PathManagementStrategy } from '@pkg/integrations/pathManager';
 import { RecursivePartial } from '@pkg/utils/typeUtils';
 
@@ -17,8 +18,6 @@ const cfg = _.merge(
     kubernetes:  { version: '1.23.4' },
     application: { pathManagementStrategy: PathManagementStrategy.Manual },
   });
-cfg.virtualMachine.memoryInGB ||= getDefaultMemory();
-
 const subject = new SettingsValidator();
 let spyPlatform: jest.SpiedFunction<typeof os.platform>;
 
@@ -30,6 +29,7 @@ afterEach(() => {
   spyPlatform.mockRestore();
 });
 
+cfg.virtualMachine.memoryInGB ||= getDefaultMemory();
 subject.k8sVersions = ['1.23.4', '1.0.0'];
 describe(SettingsValidator, () => {
   it('should do nothing when given existing settings', () => {
