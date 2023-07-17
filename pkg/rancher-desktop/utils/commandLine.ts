40bfd99c3ee7dde79e8d6f1c6deb2a43ff2940b1
Eric Promislow <epromislow@suse.com>
2023-07-17 14:52:00 -0700

    Update the wsl e2e test to work in a prefs-based world.
    * Update the mock-wsl config.json file to incorporate new commands.
    * Update the e2e test to work with the prefs window.
    * Temporarily comment-out code that is supposed to interact with the wslIntegrations prefs panel.
    * Wait for config updates to finish.
    * Don't bother reading in the config after each test. No need for it.
    
    Also comment out the older tests, to be rewritten in
    https://github.com/rancher-sandbox/rancher-desktop/issues/5240
    
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- pkg/rancher-desktop/utils/commandLine.ts
+++ pkg/rancher-desktop/utils/commandLine.ts
@@ -29,9 +29,19 @@ export default function getCommandLineArgs(): string[] {
   } else if ((process.env.npm_lifecycle_event ?? '').startsWith('test:e2e')) {
     // Note there are comments in the e2e tests near this arg warning any modifications need to take
     // this line into consideration.
-    const idx = process.argv.indexOf('--disable-dev-shm-usage');
+    let idx = process.argv.indexOf('--disable-dev-shm-usage');
+    let args = idx > -1 ? process.argv.slice(idx + 1) : [];
 
-    return idx > -1 ? process.argv.slice(idx + 1) : [];
+    idx = process.argv.findIndex(x => x.startsWith('--inspect='));
+    if (idx >= 0) {
+      args = args.slice(idx);
+    }
+    idx = process.argv.findIndex(x => x.startsWith('--remote-debugging-port='));
+    if (idx >= 0) {
+      args = args.slice(idx);
+    }
+
+    return args;
   }
   console.log(`Couldn't figure out how we're being run: ENV[NODE_ENV] = ${ process.env.NODE_ENV }, ENV[npm_lifecycle_event] = ${ process.env.npm_lifecycle_event ?? 'unset' }`);
 
