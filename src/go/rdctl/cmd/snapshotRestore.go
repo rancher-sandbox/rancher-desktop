2f522e5122722b2cd7231ebbcc63431898a56b14
Adam Pickering <adam.pickering@suse.com>
2023-07-20 11:20:29 -0600

    Implement `rdctl snapshot restore` subcommand
    Signed-off-by: Adam Pickering <adam.pickering@suse.com>

---
+++ src/go/rdctl/cmd/snapshotRestore.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/rancher-sandbox/rancher-desktop/src/go/rdctl/pkg/paths"
+	"github.com/rancher-sandbox/rancher-desktop/src/go/rdctl/pkg/snapshot"
+	"github.com/spf13/cobra"
+)
+
+var snapshotRestoreCmd = &cobra.Command{
+	Use:   "restore <id>",
+	Short: "Restore a snapshot",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		paths, err := paths.GetPaths()
+		if err != nil {
+			return fmt.Errorf("failed to get paths: %w", err)
+		}
+		manager := snapshot.NewManager(paths)
+		if err := manager.Restore(args[0]); err != nil {
+			return fmt.Errorf("failed to restore snapshot %q: %w", args[0], err)
+		}
+		return nil
+	},
+}
+
+func init() {
+	snapshotCmd.AddCommand(snapshotRestoreCmd)
+}
