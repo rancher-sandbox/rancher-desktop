ed108eb63e8ffd896528993fef23a11d0e3eb4d2
Tatjana Dehler <tdehler@suse.com>
2023-07-21 10:58:22 +0200

    Add tooltip icon component
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

---
+++ pkg/rancher-desktop/components/form/TooltipIcon.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+export default {
+  props: {
+    icon: {
+      type:    String,
+      default: 'icon-flask',
+    },
+    tooltipText: {
+      type:    String,
+      default: 'prefs.experimental',
+    },
+    tooltipPlacement: {
+      type:    String,
+      default: 'right',
+    },
+  },
+};
+</script>
+
+<template>
+  <i
+    v-tooltip="{
+      content: t(tooltipText),
+      placement: tooltipPlacement,
+    }"
+    :class="`icon ${ icon }`"
+  />
+</template>
+
+<style lang="scss" scoped>
+  .icon {
+    font-size: 0.75rem;
+    background-color: var(--icon-circle);
+    border-radius: 50%;
+    text-align: center;
+    padding: 0.25rem;
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+</style>
