dafacc5dc250560dfb31b295a09f01582f4477ee
Tatjana Dehler <tdehler@suse.com>
2023-07-21 10:59:07 +0200

    Replace experimental badge by tooltip icon
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

--- pkg/rancher-desktop/components/form/RdFieldset.vue
+++ pkg/rancher-desktop/components/form/RdFieldset.vue
@@ -2,12 +2,13 @@
 import Vue from 'vue';
 
 import LabeledBadge from '@pkg/components/form/LabeledBadge.vue';
+import TooltipIcon from '@pkg/components/form/TooltipIcon.vue';
 /**
  * Groups several controls as well as labels
  */
 export default Vue.extend({
   name:       'rd-fieldset',
-  components: { LabeledBadge },
+  components: { TooltipIcon, LabeledBadge },
   props:      {
     legendText: {
       type:    String,
@@ -20,6 +21,10 @@ export default Vue.extend({
     legendTooltip: {
       type:    String,
       default: '',
+    },
+    isExperimental: {
+      type:    Boolean,
+      default: false,
     },
     isLocked: {
       type:    Boolean,
@@ -44,6 +49,9 @@ export default Vue.extend({
         <labeled-badge
           v-if="badgeText"
           :text="badgeText"
+        />
+        <tooltip-icon
+          v-if="isExperimental"
         />
         <i
           v-if="isLocked"
