4810e6d589f7ebe74805008076f59cb949c8ccdf
Tatjana Dehler <tdehler@suse.com>
2023-08-02 12:12:56 +0200

    Improve help icon by using outlined icon
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

--- pkg/rancher-desktop/components/Help.vue
+++ pkg/rancher-desktop/components/Help.vue
@@ -35,24 +35,17 @@ export default Vue.extend({
 
 <template>
   <div class="help-button">
-    <button
+    <i
       v-tooltip="{
         content: tooltip,
         placement: 'left'
       }"
-      class="btn role-fab"
+      class="icon icon-question-mark"
       :class="{
         disabled
       }"
       @click="openUrl"
-    >
-      <span
-        class="icon icon-question-mark"
-        :class="{
-          disabled
-        }"
-      />
-    </button>
+    />
   </div>
 </template>
 
@@ -61,23 +54,13 @@ export default Vue.extend({
   .help-button {
 
     .icon {
-      display: inline-block;
-      background-color: var(--primary);
-      color: var(--body-bg);
-      font-size: 1.3rem;
-      width: 1.4rem;
-      height: 1.4rem;
-      border-radius: 50%;
+      background: transparent;
+      color: var(--primary);
+      font-size: 1.4rem;
       cursor: pointer;
 
-      &:before{
-        padding-top: 5%;
-        padding-left: 2%;
-        display: block;
-      }
-
       &:hover {
-        background: var(--primary-hover-bg);
+        color: var(--primary-hover-bg);
       }
     }
 
@@ -86,21 +69,9 @@ export default Vue.extend({
       color: var(--body-text);
       opacity: 0.2;
       cursor: default;
-    }
 
-    // We make use of the term Floating Action Button (fab) here because the
-    // design of this button is reminiscent of floating actions buttons from
-    // Material Design
-    .role-fab {
-      all: revert;
-
-      border: 0;
-      padding: 0;
-      background: transparent;
-      transition: background 200ms;
-      border-radius: 50%;
-      &.disabled {
-        border: 0;
+      &:hover {
+        color: var(--body-text);
       }
     }
   }
