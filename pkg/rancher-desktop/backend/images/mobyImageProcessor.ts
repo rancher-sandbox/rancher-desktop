b82f284d244858e275c9133d6d3df61c37c85124
Eric Promislow <epromislow@suse.com>
2023-08-24 16:45:44 -0700

    Ensure the Images page operation works when the docker context is changed.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- pkg/rancher-desktop/backend/images/mobyImageProcessor.ts
+++ pkg/rancher-desktop/backend/images/mobyImageProcessor.ts
@@ -31,6 +31,10 @@ export default class MobyImageProcessor extends imageProcessor.ImageProcessor {
   protected async runImagesCommand(args: string[], sendNotifications = true): Promise<imageProcessor.childResultType> {
     const subcommandName = args[0];
 
+    if (this.executor.backend !== 'wsl' && !args.includes('--context')) {
+      args.unshift('--context', 'rancher-desktop');
+    }
+
     return await this.processChildOutput(spawn(executable('docker'), args), subcommandName, sendNotifications);
   }
 
