36657c1f59af648cdcaa28571be8e719e4f05865
Mark Yen <mark.yen@suse.com>
2023-09-19 10:02:20 -0700

    Don't force none cred helper in unit tests on CI.
    We force the "none" credential helper in E2E on CI, but we need to _not_
    do that when running unit tests.  Previously it was just checking for
    CIRRUS_CI, which only worked because we didn't run unit tests on Cirrus.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- pkg/rancher-desktop/utils/dockerDirManager.ts
+++ pkg/rancher-desktop/utils/dockerDirManager.ts
@@ -269,7 +269,11 @@ export default class DockerDirManager {
     if (currentCredsStore && await this.credHelperWorking(currentCredsStore)) {
       return currentCredsStore;
     }
-    if (process.env.CI && await this.credHelperWorking('none')) {
+    // When running E2E tests in CI, use "none".  Note that we use the default
+    // value when running unit tests in CI.
+    const e2eInCI = process.env.CI && process.env.RD_TEST === 'e2e';
+
+    if (e2eInCI && await this.credHelperWorking('none')) {
       return 'none';
     }
 
