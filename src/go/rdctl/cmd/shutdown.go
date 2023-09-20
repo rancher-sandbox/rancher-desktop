14e2cdc4e20d56918bfe1ea1e42f217079a0f74c
Eric Promislow <epromislow@suse.com>
2023-09-20 14:27:26 -0700

    Let shutdown work on the underlying processes regardless of whether RD is running.
    Signed-off-by: Eric Promislow <epromislow@suse.com>

--- src/go/rdctl/cmd/shutdown.go
+++ src/go/rdctl/cmd/shutdown.go
@@ -19,14 +19,10 @@ package cmd
 import (
 	"errors"
 	"fmt"
-	"net/url"
-	"os"
-	"strings"
-
-	rdconfig "github.com/rancher-sandbox/rancher-desktop/src/go/rdctl/pkg/config"
 	"github.com/rancher-sandbox/rancher-desktop/src/go/rdctl/pkg/shutdown"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 type shutdownSettingsStruct struct {
@@ -69,19 +65,12 @@ func init() {
 func doShutdown(shutdownSettings *shutdownSettingsStruct, initiatingCommand shutdown.InitiatingCommand) ([]byte, error) {
 	output, err := processRequestForUtility(doRequest("PUT", versionCommand("", "shutdown")))
 	if err != nil {
+		action := "will look for internal Rancher Desktop processes to end."
 		if errors.Is(err, os.ErrNotExist) {
-			if strings.Contains(err.Error(), rdconfig.DefaultConfigPath) {
-				logrus.Debugf("Can't find default config file %s, assuming Rancher Desktop isn't running.\n", rdconfig.DefaultConfigPath)
-				// It's probably not running, so shutdown is a no-op
-				return nil, nil
-			}
-			return nil, err
+			logrus.Warnf("There's no connection-info file; %s.\n", action)
+		} else {
+			logrus.Warnf("Ignoring error %s; %s.\n", err, action)
 		}
-		urlError := new(url.Error)
-		if errors.As(err, &urlError) {
-			return []byte("Rancher Desktop is currently not running (or can't be shutdown via this command)."), nil
-		}
-		return nil, err
 	}
 	err = shutdown.FinishShutdown(shutdownSettings.WaitForShutdown, initiatingCommand)
 	return output, err
