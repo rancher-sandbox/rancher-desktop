995abbc79ece6907d96feda9186ed9a3a7f23a92
Mark Yen <mark.yen@suse.com>
2023-09-21 12:01:20 -0700

    GitHub runner: monitoring application
    This implements a command-line utility that can be used to keep track of
    repository-scoped GitHub runners and spawn more (via qemu) as needed.  The
    spawned runners are ephemeral (i.e. they are removed after one run) to help
    with isolation between runs.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

---
+++ src/go/github-runner-monitor/cmd/root.go
@@ -0,0 +1,119 @@
+/*
+Copyright © 2023 SUSE LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package cmd handles the command lines.
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/rancher-sandbox/rancher-desktop/src/go/github-runner-monitor/pkg/monitor"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"golang.org/x/sys/unix"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "github-runner-linux",
+	Short: "Manage ephemeral GitHub runners for Rancher Desktop",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		logrus.SetLevel(logrus.InfoLevel + logrus.Level(viper.GetInt("verbose")))
+		logrus.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
+	},
+	Args: cobra.ExactArgs(0),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		ctx, _ := signal.NotifyContext(cmd.Context(), unix.SIGINT, unix.SIGTERM, unix.SIGHUP)
+
+		config := monitor.Config{
+			RunnerCount:   viper.GetInt("runner-count"),
+			CheckInterval: viper.GetDuration("check-interval"),
+			AuthToken:     viper.GetString("github-auth-token"),
+			Owner:         viper.GetString("owner"),
+			Repo:          viper.GetString("repo"),
+			Labels:        viper.GetStringSlice("labels"),
+			Cpus:          viper.GetInt("cpus"),
+			Memory:        viper.GetInt("memory"),
+			Disk:          viper.GetString("disk"),
+		}
+
+		if config.Disk == "" {
+			executable, err := os.Executable()
+			if err != nil {
+				return fmt.Errorf("failed to find executable: %w", err)
+			}
+			dir := filepath.Dir(executable)
+			files, err := os.ReadDir(dir)
+			if err != nil {
+				return fmt.Errorf("failed to find disk image: %w", err)
+			}
+			for _, file := range files {
+				if !file.IsDir() && filepath.Ext(file.Name()) == ".qcow2" {
+					config.Disk = filepath.Join(dir, file.Name())
+					break
+				}
+			}
+			if config.Disk == "" {
+				return fmt.Errorf("failed to auto-detect disk image: %w", err)
+			}
+		} else {
+			if config.Disk, err = filepath.Abs(config.Disk); err != nil {
+				return fmt.Errorf("failed to resolve disk image: %w", err)
+			}
+		}
+
+		if err = monitor.Monitor(ctx, config); err != nil {
+			return fmt.Errorf("failed to monitor: %w", err)
+		}
+
+		return nil
+	},
+}
+
+// Execute the command line.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	rootCmd.PersistentFlags().CountP("verbose", "v", "Enable extra logging")
+	flags := rootCmd.Flags()
+	flags.IntP("runner-count", "c", 1, "Number of runners to keep at once")
+	flags.Duration("check-interval", time.Minute, "Interval between checking for runners")
+	flags.String("github-auth-token", "", "GitHub authentication token with \"repo\" scope")
+	flags.StringP("owner", "o", "rancher-sandbox", "GitHub owner")
+	flags.StringP("repo", "r", "rancher-desktop", "GitHub repository")
+	flags.StringSliceP("labels", "l", []string{"self-hosted", "Linux", "X64", "ephemeral"}, "Labels to apply to the runners")
+	flags.Int("cpus", 3, "Number of vCPUs per runner")
+	flags.Int("memory", 6*1024, "Memory amount per runner, in megabytes")
+	flags.String("disk", "", "Disk image to use for the VM")
+	flags.SortFlags = false
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	viper.AutomaticEnv()
+	if err := viper.BindPFlags(rootCmd.Flags()); err != nil {
+		logrus.WithError(err).Fatal("Failed to set up flags")
+	}
+}
---
+++ src/go/github-runner-monitor/go.mod
@@ -0,0 +1,33 @@
+module github.com/rancher-sandbox/rancher-desktop/src/go/github-runner-monitor
+
+go 1.21
+
+require (
+	github.com/google/go-github/v55 v55.0.0
+	github.com/kata-containers/kata-containers/src/runtime v0.0.0-20230914182355-2e8b41f39c66
+	github.com/sirupsen/logrus v1.9.0
+	github.com/spf13/cobra v1.7.0
+	github.com/spf13/viper v1.16.0
+	golang.org/x/sys v0.11.0
+)
+
+require (
+	github.com/ProtonMail/go-crypto v0.0.0-20230217124315-7d5c6f04bbb8 // indirect
+	github.com/cloudflare/circl v1.3.3 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/spf13/afero v1.9.5 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
