4597e3b59491c4410862ebccf29427c1f1baae2b
Tatjana Dehler <tdehler@suse.com>
2023-09-29 15:49:19 +0200

    Add possibility to lock noproxy field
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

--- pkg/rancher-desktop/components/Preferences/WslProxy.vue
+++ pkg/rancher-desktop/components/Preferences/WslProxy.vue
@@ -39,6 +39,9 @@ export default Vue.extend({
     noproxyErrorMessages(): { duplicate: string } {
       return { duplicate: this.t('virtualMachine.proxy.noproxy.errors.duplicate') };
     },
+    isNoProxyFieldReadOnly() {
+      return this.isFieldDisabled || this.isPreferenceLocked('experimental.virtualMachine.proxy.noproxy');
+    },
   },
   methods: {
     onChange<P extends keyof RecursiveTypes<Settings>>(property: P, value: RecursiveTypes<Settings>[P]) {
@@ -119,10 +122,11 @@ export default Vue.extend({
       <div class="proxy-col">
         <rd-fieldset
           :legend-text="t('virtualMachine.proxy.noproxy.legend', { }, true)"
+          :is-locked="isPreferenceLocked('experimental.virtualMachine.proxy.noproxy')"
         >
           <string-list
             :placeholder="t('virtualMachine.proxy.noproxy.placeholder', { }, true)"
-            :readonly="isFieldDisabled"
+            :readonly="isNoProxyFieldReadOnly"
             :actions-position="'left'"
             :items="preferences.experimental.virtualMachine.proxy.noproxy"
             :error-messages="noproxyErrorMessages"
@@ -157,6 +161,13 @@ export default Vue.extend({
 
       .string-list-footer {
         padding-right: 2rem;
+      }
+
+      &.readonly {
+        background-color: var(--input-disabled-bg);
+        color: var(--input-disabled-text);
+        opacity: 1;
+        cursor: not-allowed;
       }
     }
   }
