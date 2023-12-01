rancher-sandbox/rancher-desktop

commit cb2e4f71a5f899b9168ea1403e906b9c78670c19

WiX: Add status message for custom steps
This adds status messages during the custom installation steps, so the user
can have more details on what is happening.

Signed-off-by: Mark Yen <mark.yen@suse.com>

src/go/wsl-helper/wix/helpers_windows.go
@@ -19,63 +19,49 @@ package main
 import (
 	"fmt"
 	"strings"
-	"sync"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
+type messageType uintptr
 const (
-	INSTALLMESSAGE_INFO = 0x04000000
+	INSTALLMESSAGE_INFO messageType = 0x04000000
+	INSTALLMESSAGE_ACTIONSTART messageType = 0x08000000
 )
+
+func submitMessage(hInstall MSIHANDLE, message messageType, data []string) error {
+	record, _, _ := msiCreateRecord.Call(uintptr(len(data) - 1))
+	if record == 0 {
+		return fmt.Errorf("failed to create record")
+	}
+	defer msiCloseHandle.Call(record)
+	for i, item := range data {
+		buf, err := windows.UTF16PtrFromString(item)
+		if err != nil {
+			return err
+		}
+		_, _, _ = msiRecordSetStringW.Call(record, uintptr(i), uintptr(unsafe.Pointer(buf)))
+	}
+	_, _, _ = msiProcessMessage.Call(uintptr(hInstall), uintptr(message), record)
+	return nil
+}
 
 // msiWriter is an io.Writer that emits to Windows Installer's logging.
 type msiWriter struct {
 	hInstall MSIHANDLE
-	once     sync.Once
-	record   uintptr
 }
 
 func (w *msiWriter) Write(message []byte) (int, error) {
-	var err error
-	w.once.Do(func() {
-		// We always set up a record where *0 is just "[1]" to avoid issues if
-		// the message contains formatting; this is analogous to calling
-		// `Sprintf("%s", ...)``
-		var buf *uint16
-		buf, err = windows.UTF16PtrFromString("[1]")
-		if err != nil {
-			return
-		}
-		w.record, _, _ = msiCreateRecord.Call(1)
-		_, _, _ = msiRecordSetStringW.Call(w.record, 0, uintptr(unsafe.Pointer(buf)))
-	})
+	// We always set up a record where *0 is just "[1]" to avoid issues if
+	// the message contains formatting; this is analogous to calling
+	// `Sprintf("%s", ...)``
+	data := []string{"1", strings.TrimRight(string(message), "\r\n")}
+	err := submitMessage(w.hInstall, INSTALLMESSAGE_INFO, data)
 	if err != nil {
 		return 0, err
 	}
-	buf, err := windows.UTF16PtrFromString(strings.TrimRight(string(message), "\r\n"))
-	if err != nil {
-		return 0, err
-	}
-	_, _, _ = msiRecordSetStringW.Call(
-		w.record,
-		1,
-		uintptr(unsafe.Pointer(buf)),
-	)
-	_, _, _ = msiProcessMessage.Call(
-		uintptr(w.hInstall),
-		uintptr(INSTALLMESSAGE_INFO),
-		w.record,
-	)
-	// As an extra debugging helper, also send it on OutputDebugString
-	_, _, _ = outputDebugStringW.Call(uintptr(unsafe.Pointer(buf)))
 	return len(message), nil
-}
-
-func (w *msiWriter) cleanup() {
-	if w.record != 0 {
-		msiCloseHandle.Call(w.record)
-	}
 }
 
 // setProperty sets a Windows Installer property to the given value.

src/go/wsl-helper/wix/check_windows.go
@@ -33,7 +33,6 @@ func IsWSLInstalledImpl(hInstall MSIHANDLE) uint32 {
 	ctx := context.Background()
 
 	writer := &msiWriter{hInstall: hInstall}
-	defer writer.cleanup()
 	log := logrus.NewEntry(&logrus.Logger{
 		Out:       writer,
 		Formatter: &logrus.TextFormatter{},

src/go/wsl-helper/wix/install_windows.go
@@ -29,7 +29,6 @@ func InstallWindowsFeatureImpl(hInstall MSIHANDLE) uint32 {
 	ctx := context.Background()
 
 	writer := &msiWriter{hInstall: hInstall}
-	defer writer.cleanup()
 	log := logrus.NewEntry(&logrus.Logger{
 		Out:       writer,
 		Formatter: &logrus.TextFormatter{},
@@ -38,6 +37,9 @@ func InstallWindowsFeatureImpl(hInstall MSIHANDLE) uint32 {
 	})
 
 	log.Infof("Installing Windows feature...")
+	submitMessage(hInstall, INSTALLMESSAGE_ACTIONSTART, []string{
+		"", "InstalWindowsFeature", "Installing required Windows features...", "<unused>",
+	})
 	if err := wslutils.DismDoInstall(ctx, log); err != nil {
 		log.WithError(err).Error("Failed to install feature")
 		return 1
@@ -53,7 +55,6 @@ func InstallWSLImpl(hInstall MSIHANDLE) uint32 {
 	ctx := context.Background()
 
 	writer := &msiWriter{hInstall: hInstall}
-	defer writer.cleanup()
 	log := logrus.NewEntry(&logrus.Logger{
 		Out:       writer,
 		Formatter: &logrus.TextFormatter{},
@@ -62,6 +63,9 @@ func InstallWSLImpl(hInstall MSIHANDLE) uint32 {
 	})
 
 	log.Info("Installing WSL...")
+	submitMessage(hInstall, INSTALLMESSAGE_ACTIONSTART, []string{
+		"", "InstallWSL", "Installing Windows Subsystem for Linux...", "<unused>",
+	})
 	if err := wslutils.InstallWSL(ctx, log); err != nil {
 		log.WithError(err).Error("Installing WSL failed")
 		return 1

commit 6a9ff7fd8efb4911d258d44a3c596d48e4a51cf8

WiX: Fix Windows Installer logging
We formatted the string incorrectly, causing all logging to simply print a
literal "1" instead of the log line.

Fixes: cb2e4f71a5f899b9168ea1403e906b9c78670c19
Signed-off-by: Mark Yen <mark.yen@suse.com>

src/go/wsl-helper/wix/helpers_windows.go
@@ -57,7 +57,7 @@ func (w *msiWriter) Write(message []byte) (int, error) {
 	// We always set up a record where *0 is just "[1]" to avoid issues if
 	// the message contains formatting; this is analogous to calling
 	// `Sprintf("%s", ...)``
-	data := []string{"1", strings.TrimRight(string(message), "\r\n")}
+	data := []string{"[1]", strings.TrimRight(string(message), "\r\n")}
 	err := submitMessage(w.hInstall, INSTALLMESSAGE_INFO, data)
 	if err != nil {
 		return 0, err