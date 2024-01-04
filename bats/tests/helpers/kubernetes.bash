bfc5a5c86561901ebd8ab418db5f7438f7dc6775
Jan Dubois <jan.dubois@suse.com>
2024-01-04 14:34:05 -0800

    Wait until kubelet is ready in wait_for_apiserver
    This is the same mechanism used in the backend to determine if the node
    is ready.
    
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- bats/tests/helpers/kubernetes.bash
+++ bats/tests/helpers/kubernetes.bash
@@ -7,13 +7,19 @@ wait_for_apiserver() {
             sleep 1
         done
         assert [ "$(date +%s)" -lt "$timeout" ]
-        run kubectl get node -o jsonpath="{.items[0].status.nodeInfo.kubeletVersion}"
-        if ((status == 0)); then
-            # Turn "v1.23.4+k3s1" into "1.23.4"
-            local version=${output#v}
-            version=${version%+*}
-            if [ "$version" == "$desired_version" ]; then
-                return 0
+
+        # Check that kubelet is Ready
+        run kubectl get node -o jsonpath="{.items[0].status.conditions[?(@.type=='Ready')].status}"
+        if ((status == 0)) && [[ $output == "True" ]]; then
+            # Verify kubelet version
+            run kubectl get node -o jsonpath="{.items[0].status.nodeInfo.kubeletVersion}"
+            if ((status == 0)); then
+                # Turn "v1.23.4+k3s1" into "1.23.4"
+                local version=${output#v}
+                version=${version%+*}
+                if [ "$version" == "$desired_version" ]; then
+                    return 0
+                fi
             fi
         fi
         sleep 1
