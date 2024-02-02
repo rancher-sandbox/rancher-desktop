bcd802ec6fc9459ce1db8376a2f9bb28441951c3
Mark Yen <mark.yen@suse.com>
2024-02-02 14:18:18 -0800

    scripts/install-latest-ci: use TMPDIR
    This restores support for using TMPDIR when downloading artifacts.  IT also
    allows the user to set the directory to download the BATS tests into
    (defaulting to $TMPDIR/bats).
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- scripts/install-latest-ci.sh
+++ scripts/install-latest-ci.sh
@@ -12,6 +12,8 @@ set -o xtrace
 : "${BRANCH:=main}"           # Branch to fetch from
 : "${PR:=}"                   # PR number to fetch from (overrides BRANCH)
 : "${WORKFLOW:=package.yaml}" # Name of workflow that must have succeeded
+: "${BATS_DIR:=${TMPDIR:-/tmp}/bats}" # Directory to extract BATS tests to.
+: "${SKIP_INSTALL:=}"         # If set, don't install the application.
 
 : "${RD_LOCATION:=user}"
 
@@ -21,21 +23,23 @@ if ! [[ $RD_LOCATION =~ ^(system|user)$ ]]; then
 fi
 
 download_artifact() {
-    FILENAME=$1
+    local filename="$1"
+    local leaf
+    leaf="$(basename "$1")"
 
     # Get the artifact id for the package
     API="repos/$OWNER/$REPO/actions/runs/$ID/artifacts"
-    FILTER=".artifacts[] | select(.name == \"$FILENAME\").id"
+    FILTER=".artifacts[] | select(.name == \"$leaf\").id"
 
     ARTIFACT_ID=$(gh api "$API" --jq "$FILTER")
     if [ -z "$ARTIFACT_ID" ]; then
-        echo "No download url for '$FILENAME' from $WORKFLOW run $ID"
+        echo "No download url for '$leaf' from $WORKFLOW run $ID"
         exit 1
     fi
 
     # Download the package. It requires authentication, so use gh instead of curl.
     API="repos/$OWNER/$REPO/actions/artifacts/$ARTIFACT_ID/zip"
-    gh api "$API" > "$FILENAME"
+    gh api "$API" > "$filename"
 }
 
 wslpath_from_win32_env() {
@@ -46,29 +50,34 @@ wslpath_from_win32_env() {
 }
 
 install_application() {
+    local archive
     if [[ "$(uname -s)" == "Darwin" ]]; then
         ARCH=x86_64
         if [ "$(uname -m)" = "arm64" ]; then
             ARCH=aarch64
         fi
-        download_artifact "Rancher Desktop-mac.$ARCH.zip"
+        archive="${TMPDIR:-/tmp}/Rancher Desktop-mac.$ARCH.zip"
     elif [[ "$(uname -r)" =~ "WSL2" ]]; then
-        download_artifact "Rancher Desktop-win.zip"
+        archive="${TMPDIR:-/tmp}/Rancher Desktop-win.zip"
     else
-        download_artifact "Rancher Desktop-linux.zip"
+        archive="${TMPDIR:-/tmp}/Rancher Desktop-linux.zip"
     fi
+    download_artifact "$archive"
 
     # Artifacts are zipped, so extract inner ZIP file from outer wrapper.
     # The outer ZIP has a predictable name like "Rancher Desktop-mac.x86_64.zip"
     # but the inner one has a version string: "Rancher Desktop-1.7.0-1061-g91ab3831-mac.zip"
     # Run unzip in "zipinfo" mode, which can print just the file name.
-    ZIP="$(unzip -Z -1 "$FILENAME" | head -n1)"
+    local ZIP
+    ZIP="$(unzip -Z -1 "$archive" | head -n1)"
     if [ -z "$ZIP" ]; then
-        echo "Cannot find inner archive in $FILENAME"
+        echo "Cannot find inner archive in $(basename "$archive")"
         exit 1
     fi
+    local zip_full="${TMPDIR:-/tmp}/$ZIP"
 
-    unzip -o "$FILENAME" "$ZIP"
+    unzip -o "$archive" "$ZIP" -d "${TMPDIR:-/tmp}"
+    local DEST
 
     if [[ "$(uname -s)" == "Darwin" ]]; then
         # Extract from inner archive into /Applications
@@ -79,7 +88,7 @@ install_application() {
 
         APP="Rancher Desktop.app"
         rm -rf "${DEST:?}/$APP"
-        unzip -o "$ZIP" "$APP/*" -d "$DEST" >/dev/null
+        unzip -o "$zip_full" "$APP/*" -d "$DEST" >/dev/null
     elif [[ "$(uname -r)" =~ "WSL2" ]]; then
         LOCALAPPDATA="$(wslpath_from_win32_env LOCALAPPDATA)"
         PROGRAMFILES="$(wslpath_from_win32_env ProgramFiles)"
@@ -89,16 +98,17 @@ install_application() {
         fi
         APP="Rancher Desktop"
         rm -rf "${DEST:?}/$APP"
-        unzip -o "$ZIP" "$APP/*" -d "$DEST" >/dev/null
+        unzip -o "$zip_full" "$APP/*" -d "$DEST" >/dev/null
     else
         # Linux doesn't support per-user installs
         DEST="/opt/rancher-desktop"
         sudo rm -rf "${DEST:?}"
-        sudo unzip -o "$ZIP" -d "$DEST" >/dev/null
+        sudo unzip -o "$zip_full" -d "$DEST" >/dev/null
     fi
 }
 
 download_bats() {
+    pushd "${TMPDIR:-/tmp}"
     download_artifact "bats.tar.gz"
 
     # GitHub always wraps the artifact in a zip file, so the downloaded file
@@ -106,11 +116,11 @@ download_bats() {
     mv bats.tar.gz bats.tar.gz.zip
     unzip -o bats.tar.gz.zip bats.tar.gz
 
-    # Unpack bats into $PWD/bats
-    DEST="bats"
-    rm -rf "$DEST"
-    mkdir "$DEST"
-    tar xfz "bats.tar.gz" -C "$DEST"
+    # Unpack bats into $BATS_DIR
+    rm -rf "$BATS_DIR"
+    mkdir "$BATS_DIR"
+    tar xfz "bats.tar.gz" -C "$BATS_DIR"
+    popd
 }
 
 API_ARGS="exclude_pull_requests=true"
@@ -132,7 +142,7 @@ if [ -z "$ID" ]; then
     exit 1
 fi
 
-if [[ -z "${SKIP_INSTALL:-}" ]]; then
+if [[ -z "$SKIP_INSTALL" ]]; then
     install_application
 fi
 download_bats
