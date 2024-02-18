12a93d1545103f5b127c3e1fb9a60f78eef3c3df
Jan Dubois <jan.dubois@suse.com>
2024-02-18 12:58:53 -0800

    Wait for default namespace to exist in wait_for_kubelete
    To deal with spurious errors like this:
       Error from server (Forbidden): pods "nginx" is forbidden: error looking up service account default/default: serviceaccount "default" not found
    
    See https://github.com/kubernetes/kubernetes/issues/66689
    
    Also refactors wait_for_kubelet to remove deeply nested logic
    
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- bats/tests/helpers/kubernetes.bash
+++ bats/tests/helpers/kubernetes.bash
@@ -3,27 +3,35 @@ wait_for_kubelet() {
     local timeout=$(($(date +%s) + RD_KUBELET_TIMEOUT * 60))
     trace "waiting for Kubernetes ${desired_version} to be available"
     while true; do
-        until kubectl get --raw /readyz &>/dev/null; do
-            assert [ "$(date +%s)" -lt "$timeout" ]
-            sleep 1
-        done
+        sleep 1
         assert [ "$(date +%s)" -lt "$timeout" ]
+        if ! kubectl get --raw /readyz &>/dev/null; then
+            continue
+        fi
 
         # Check that kubelet is Ready
         run kubectl get node -o jsonpath="{.items[0].status.conditions[?(@.type=='Ready')].status}"
-        if ((status == 0)) && [[ $output == "True" ]]; then
-            # Verify kubelet version
-            run kubectl get node -o jsonpath="{.items[0].status.nodeInfo.kubeletVersion}"
-            if ((status == 0)); then
-                # Turn "v1.23.4+k3s1" into "1.23.4"
-                local version=${output#v}
-                version=${version%+*}
-                if [ "$version" == "$desired_version" ]; then
-                    return 0
-                fi
-            fi
+        if ((status != 0)) || [[ $output != "True" ]]; then
+            continue
         fi
-        sleep 1
+
+        # Make sure the "default" serviceaccount exists
+        if ! kubectl get --namespace default serviceaccount default; then
+            continue
+        fi
+
+        # Get kubelet version
+        run kubectl get node -o jsonpath="{.items[0].status.nodeInfo.kubeletVersion}"
+        if ((status != 0)); then
+            continue
+        fi
+
+        # Turn "v1.23.4+k3s1" into "1.23.4"
+        local version=${output#v}
+        version=${version%+*}
+        if [ "$version" == "$desired_version" ]; then
+            return 0
+        fi
     done
 }
 
