e955fbba0fdc2e58d995812b2eb031d54086ae9b
Jan Dubois <jan.dubois@suse.com>
2024-02-20 15:15:23 -0800

    Some tests have the current settings version hardcoded in them
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- src/go/rdctl/pkg/reg/reg_test.go
+++ src/go/rdctl/pkg/reg/reg_test.go
@@ -55,7 +55,7 @@ func TestJsonToRegFormat(t *testing.T) {
 		assert.Equal(t, "[HKEY_CURRENT_USER\\SOFTWARE\\Policies]", lines[1])
 		assert.Equal(t, "[HKEY_CURRENT_USER\\SOFTWARE\\Policies\\Rancher Desktop]", lines[2])
 		assert.Equal(t, "[HKEY_CURRENT_USER\\SOFTWARE\\Policies\\Rancher Desktop\\defaults]", lines[3])
-		assert.Equal(t, `"version"=dword:a`, lines[4])
+		assert.Equal(t, `"version"=dword:b`, lines[4])
 	})
 	t.Run("converts the registry-type arguments into reg headers", func(t *testing.T) {
 		type testCaseType struct {
@@ -109,7 +109,7 @@ func TestJsonToRegFormat(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, 12, len(lines))
 		assert.Equal(t, fmt.Sprintf("[%s]", header), lines[3])
-		assert.Equal(t, `"version"=dword:a`, lines[4])
+		assert.Equal(t, `"version"=dword:b`, lines[4])
 		assert.Equal(t, fmt.Sprintf("[%s\\application]", header), lines[5])
 		assert.Equal(t, fmt.Sprintf("[%s\\application\\extensions]", header), lines[6])
 		assert.Equal(t, fmt.Sprintf("[%s\\application\\extensions\\allowed]", header), lines[7])
