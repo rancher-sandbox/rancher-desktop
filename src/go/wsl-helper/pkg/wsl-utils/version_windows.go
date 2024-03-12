rancher-sandbox/rancher-desktop

commit b7aa9dd964767d1d3d154037f188a9f8d44e063c

WSL-Helper: Address review feedback
- Rename `isInboxWSLInstalled` to `getInboxWSLInfo` because we now return
  the in-box kernel version too.
- Assume when `MsiGetProductInfoW` returns `ERROR_MORE_DATA` it has set the
  `pcchValueBuf` parameter to the length of the required buffer (minus the
  null terminator).
- Rename `PropWslInstalled` and `PropWslKernelOutdated` to use upper case
  `WSL`

Signed-off-by: Mark Yen <mark.yen@suse.com>

src/go/wsl-helper/pkg/wsl-utils/version_windows.go
@@ -355,9 +355,9 @@ func getAppxVersion(ctx context.Context, log *logrus.Entry) (*PackageVersion, *P
 	return &wslVersion, &kernelVersion, nil
 }
 
-// isInboxWSLInstalled checks if the "in-box" version of WSL is installed,
-// returning whether it's installed, the version of the kernel installed (if any)
-func isInboxWSLInstalled(ctx context.Context, log *logrus.Entry) (bool, *PackageVersion, error) {
+// getInboxWSLInfo checks if the "in-box" version of WSL is installed, returning
+// whether it's installed, and the version of the kernel installed (if any)
+func getInboxWSLInfo(ctx context.Context, log *logrus.Entry) (bool, *PackageVersion, error) {
 	var allErrors []error
 
 	// Check if the core is installed
@@ -425,31 +425,48 @@ func getMSIVersion(productCode []uint16) (*PackageVersion, error) {
 	if err != nil {
 		return nil, err
 	}
-	bufSize := uint32(64)
-	wideBuf := make([]uint16, bufSize)
-	for {
-		rv, _, _ := msiGetProductInfo.Call(
-			uintptr(unsafe.Pointer(unsafe.SliceData(productCode))),
-			uintptr(unsafe.Pointer(versionStringWide)),
-			uintptr(unsafe.Pointer(unsafe.SliceData(wideBuf))),
-			uintptr(unsafe.Pointer(&bufSize)),
-		)
-		switch rv {
-		case uintptr(windows.ERROR_SUCCESS):
-			versionString := windows.UTF16ToString(wideBuf[:bufSize])
-			if err = version.UnmarshalText([]byte(versionString)); err != nil {
-				return nil, err
-			}
-			return &version, nil
-		case uintptr(windows.ERROR_MORE_DATA):
-			bufSize = uint32(len(wideBuf) * 2)
-			wideBuf = make([]uint16, bufSize)
-		case uintptr(windows.ERROR_BAD_CONFIGURATION):
-			err = errorFromWin32("Windows Installer configuration data is corrupt", rv, nil)
+
+	bufSize := 0
+	var wideBuf []uint16
+	rv, _, _ := msiGetProductInfo.Call(
+		uintptr(unsafe.Pointer(unsafe.SliceData(productCode))),
+		uintptr(unsafe.Pointer(versionStringWide)),
+		uintptr(unsafe.Pointer(nil)),
+		uintptr(unsafe.Pointer(&bufSize)),
+	)
+	switch rv {
+	case uintptr(windows.ERROR_SUCCESS):
+		return nil, fmt.Errorf("succeeded getting product version with no buffer")
+	case uintptr(windows.ERROR_MORE_DATA):
+		wideBuf = make([]uint16, bufSize+1) // Add space for null terminator
+		bufSize = len(wideBuf)
+	case uintptr(windows.ERROR_BAD_CONFIGURATION):
+		err = errorFromWin32("Windows Installer configuration data is corrupt", rv, nil)
+		return nil, err
+	default:
+		return nil, errorFromWin32("failed to get WSL kernel MSI version", rv, nil)
+	}
+
+	rv, _, _ = msiGetProductInfo.Call(
+		uintptr(unsafe.Pointer(unsafe.SliceData(productCode))),
+		uintptr(unsafe.Pointer(versionStringWide)),
+		uintptr(unsafe.Pointer(unsafe.SliceData(wideBuf))),
+		uintptr(unsafe.Pointer(&bufSize)),
+	)
+	switch rv {
+	case uintptr(windows.ERROR_SUCCESS):
+		versionString := windows.UTF16ToString(wideBuf[:bufSize])
+		if err = version.UnmarshalText([]byte(versionString)); err != nil {
 			return nil, err
-		default:
-			return nil, errorFromWin32("failed to get WSL kernel MSI version", rv, nil)
 		}
+		return &version, nil
+	case uintptr(windows.ERROR_MORE_DATA):
+		return nil, errorFromWin32("allocated buffer was too small", rv, nil)
+	case uintptr(windows.ERROR_BAD_CONFIGURATION):
+		err = errorFromWin32("Windows Installer configuration data is corrupt", rv, nil)
+		return nil, err
+	default:
+		return nil, errorFromWin32("failed to get WSL kernel MSI version", rv, nil)
 	}
 }
 
@@ -482,7 +499,7 @@ func GetWSLInfo(ctx context.Context, log *logrus.Entry) (*WSLInfo, error) {
 	}
 
 	log.Trace("Failed to get WSL appx package, trying inbox versions...")
-	hasWSL, kernelVersion, err := isInboxWSLInstalled(ctx, log)
+	hasWSL, kernelVersion, err := getInboxWSLInfo(ctx, log)
 	if err != nil {
 		return nil, err
 	}

src/go/wsl-helper/pkg/wsl-utils/version_windows_test.go
@@ -270,7 +270,7 @@ func TestGetAppxVersion(t *testing.T) {
 	}
 }
 
-func TestIsInboxWSLInstalled(t *testing.T) {
+func TestGetInboxWSLInfo(t *testing.T) {
 	logger := logrus.New()
 	logger.SetOutput(io.Discard)
 
@@ -287,7 +287,7 @@ func TestIsInboxWSLInstalled(t *testing.T) {
 		})
 		// Use a random GUID here
 		ctx = context.WithValue(ctx, &kUpgradeCodeOverride, "{60486CC7-CD7A-4514-9E88-7F21E8A81679}")
-		hasWSL, kernelVersion, err := isInboxWSLInstalled(ctx, logrus.NewEntry(logger))
+		hasWSL, kernelVersion, err := getInboxWSLInfo(ctx, logrus.NewEntry(logger))
 		assert.NoError(t, err)
 		assert.False(t, hasWSL, "WSL should not be installed")
 		if !assert.Nil(t, kernelVersion, "kernel should not be installed") {
@@ -315,7 +315,7 @@ func TestIsInboxWSLInstalled(t *testing.T) {
 		})
 		// Use a random GUID here
 		ctx = context.WithValue(ctx, &kUpgradeCodeOverride, "{0C32EDDD-2674-4F32-B415-B715AF90BE74}")
-		hasWSL, kernelVersion, err := isInboxWSLInstalled(ctx, logrus.NewEntry(logger))
+		hasWSL, kernelVersion, err := getInboxWSLInfo(ctx, logrus.NewEntry(logger))
 		assert.NoError(t, err)
 		assert.True(t, hasWSL, "WSL should be installed")
 		if !assert.Nil(t, kernelVersion, "kernel should not be installed") {
@@ -333,7 +333,7 @@ func TestIsInboxWSLInstalled(t *testing.T) {
 		// Use the upgrade code for "Microsoft Update Health Tools", which is
 		// automatically installed from Windows Update.
 		ctx = context.WithValue(ctx, &kUpgradeCodeOverride, "{2E5106FD-42A1-4BBE-9C29-7E1D34CB79A1}")
-		hasWSL, kernelVersion, err := isInboxWSLInstalled(ctx, logrus.NewEntry(logger))
+		hasWSL, kernelVersion, err := getInboxWSLInfo(ctx, logrus.NewEntry(logger))
 		assert.NoError(t, err)
 		assert.True(t, hasWSL, "WSL should be installed")
 		if assert.NotNil(t, kernelVersion, "kernel should be installed") {

src/go/wsl-helper/wix/check_windows.go
@@ -25,9 +25,9 @@ import (
 
 const (
 	// Windows Installer property that is set if WSL was detected to be installed.
-	PropWslInstalled = "WSLINSTALLED"
+	PropWSLInstalled = "WSLINSTALLED"
 	// Windows Installer property that is set if the WSL kernel needs to be upgraded.
-	PropWslKernelOutdated = "WSLKERNELOUTDATED"
+	PropWSLKernelOutdated = "WSLKERNELOUTDATED"
 )
 
 // DetectWSLImpl checks if WSL is installed; it outputs results by setting
@@ -52,12 +52,12 @@ func DetectWSLImpl(hInstall MSIHANDLE) uint32 {
 	log.Infof("WSL install state: %+v", info)
 
 	if info.Installed {
-		if err = setProperty(hInstall, PropWslInstalled, "1"); err != nil {
-			log.WithError(err).Errorf("failed to set property %s", PropWslInstalled)
+		if err = setProperty(hInstall, PropWSLInstalled, "1"); err != nil {
+			log.WithError(err).Errorf("failed to set property %s", PropWSLInstalled)
 		}
 		if info.OutdatedKernel {
-			if err = setProperty(hInstall, PropWslKernelOutdated, "1"); err != nil {
-				log.WithError(err).Errorf("failed to set property %s", PropWslKernelOutdated)
+			if err = setProperty(hInstall, PropWSLKernelOutdated, "1"); err != nil {
+				log.WithError(err).Errorf("failed to set property %s", PropWSLKernelOutdated)
 			}
 		}
 	}

src/go/wsl-helper/wix/main_windows.go
@@ -23,8 +23,9 @@ func main() {
 }
 
 // DetectWSL is a wrapper around DetectWSLImpl; this is the stub to be exported
-// in the DLL.  This only exists because code analysis doesn't work as well in
-// cgo files.
+// in the DLL.  This only exists to limit cgo to this file so that editing on a
+// machine that requires cross compilation can avoid needing a cross cgo
+// toolchain.
 //
 //export DetectWSL
 func DetectWSL(hInstall C.ulong) C.ulong {
@@ -32,8 +33,9 @@ func DetectWSL(hInstall C.ulong) C.ulong {
 }
 
 // InstallWindowsFeature is a wrapper around InstallWindowsFeature; this is the
-// stub to be exported in the DLL.  This only exists because code analysis
-// doesn't work as well in cgo files.
+// stub to be exported in the DLL.  This only exists to limit cgo to this file
+// so that editing on a machine that requires cross compilation can avoid
+// needing a cross cgo toolchain.
 //
 //export InstallWindowsFeature
 func InstallWindowsFeature(hInstall C.ulong) C.ulong {
@@ -41,8 +43,9 @@ func InstallWindowsFeature(hInstall C.ulong) C.ulong {
 }
 
 // InstallWSL is a wrapper around InstallWSLImpl; this is the stub to be
-// exported in the DLL.  This only exists because code analysis doesn't work as
-// well in cgo files.
+// exported in the DLL.  This only exists to limit cgo to this file so that
+// editing on a machine that requires cross compilation can avoid needing a
+// cross cgo toolchain.
 //
 //export InstallWSL
 func InstallWSL(hInstall C.ulong) C.ulong {
@@ -50,8 +53,9 @@ func InstallWSL(hInstall C.ulong) C.ulong {
 }
 
 // UpdateWSL is a wrapper around UpdateWSLImpl; this is the stub to be exported
-// in the DLL.  This only exists because code analysis doesn't work as well in
-// cgo files.
+// in the DLL.  This only exists to limit cgo to this file so that editing on a
+// machine that requires cross compilation can avoid needing a cross cgo
+// toolchain.
 //
 //export UpdateWSL
 func UpdateWSL(hInstall C.ulong) C.ulong {

commit 1c334e8e2becd50610657f7fd3dbe2e57b27480e

WSL-Helper: Fix getting MSI version
When getting the MSI version, we initially supply no buffer to get the
total buffer size required.  This can return success instead of
ERROR_MORE_DATA as previously expected; the actual result is the same, and
we need to allocate a buffer large enough.

Signed-off-by: Mark Yen <mark.yen@suse.com>
Fixes: b7aa9dd964767d1d3d154037f188a9f8d44e063c

src/go/wsl-helper/pkg/wsl-utils/version_windows.go
@@ -59,7 +59,7 @@ func (i WSLInfo) String() string {
 	if len(parts) == 0 {
 		parts = append(parts, "not-installed")
 	}
-	return fmt.Sprintf("Version=%s (%s)", i.Version, strings.Join(parts, ", "))
+	return fmt.Sprintf("Version=%s kernel=%s (%s)", i.Version, i.KernelVersion, strings.Join(parts, ", "))
 }
 
 const (
@@ -403,7 +403,7 @@ func getInboxWSLInfo(ctx context.Context, log *logrus.Entry) (bool, *PackageVers
 		)
 		switch rv {
 		case uintptr(windows.ERROR_SUCCESS):
-			kernelVersion, err = getMSIVersion(productCode)
+			kernelVersion, err = getMSIVersion(productCode, log)
 			if err != nil {
 				allErrors = append(allErrors, fmt.Errorf("error getting kernel version: %w", err))
 			}
@@ -419,7 +419,7 @@ func getInboxWSLInfo(ctx context.Context, log *logrus.Entry) (bool, *PackageVers
 }
 
 // Get the version of an installed MSI package, given its product code.
-func getMSIVersion(productCode []uint16) (*PackageVersion, error) {
+func getMSIVersion(productCode []uint16, log *logrus.Entry) (*PackageVersion, error) {
 	version := PackageVersion{}
 	versionStringWide, err := windows.UTF16PtrFromString(INSTALLPROPERTY_VERSIONSTRING)
 	if err != nil {
@@ -436,7 +436,8 @@ func getMSIVersion(productCode []uint16) (*PackageVersion, error) {
 	)
 	switch rv {
 	case uintptr(windows.ERROR_SUCCESS):
-		return nil, fmt.Errorf("succeeded getting product version with no buffer")
+		log.WithFields(logrus.Fields{"bufSize": bufSize}).Trace("unexpected success, assuming needs more data")
+		fallthrough
 	case uintptr(windows.ERROR_MORE_DATA):
 		wideBuf = make([]uint16, bufSize+1) // Add space for null terminator
 		bufSize = len(wideBuf)