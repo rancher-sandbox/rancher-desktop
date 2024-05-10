5496547e69f36b1f19e76f0ab80d63bb12fef412
Mark Yen <mark.yen@suse.com>
2024-05-10 13:05:53 -0700

    Lint: Handle go modules
    We also add the go modules handling to a lint step, so that
    1) We can catch issues with cross-repository PRs.
    2) We can fix it trivially by running `yarn lint` and committing.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- scripts/lint-go.ts
+++ scripts/lint-go.ts
@@ -1,14 +1,81 @@
 /**
- * This script runs gofmt for CI.
+ * This script handles linting for go-related files.
  *
- * The wrapper is needed because `gofmt -d` never exits with an error.
- * https://github.com/golang/go/issues/46289
+ * If any argument is `--fix`, then changes are automatically applied.
  */
-import { execFileSync } from 'child_process';
+import path from 'path';
 
-const stdout = execFileSync('gofmt', ['-d', 'src/go']).toString();
+import { spawnFile } from '@pkg/utils/childProcess';
 
-if (stdout) {
-  console.log(stdout);
-  process.exit(1);
+const fix = process.argv.includes('--fix');
+
+async function format(fix: boolean): Promise<boolean> {
+  if (fix) {
+    await spawnFile('gofmt', ['-w', 'src/go']);
+  } else {
+    // `gofmt -d` never exits with an error; we need to check if the output is
+    // empty instead.
+    const { stdout } = await spawnFile('gofmt', ['-d', 'src/go'], { stdio: 'pipe' });
+
+    if (stdout.trim()) {
+      console.log(stdout.trim());
+
+      return false;
+    }
+  }
+
+  return true;
 }
+
+async function syncModules(fix: boolean): Promise<boolean> {
+  const listFiles = async(...globs: string[]) => {
+    const { stdout } = await spawnFile('git', ['ls-files', ...globs], { stdio: 'pipe' });
+
+    return stdout.split(/\r?\n/).filter(x => x);
+  };
+  const modFiles = await listFiles('**/go.mod');
+  const files = ['go.work', ...modFiles, ...await listFiles('**/go.sum')];
+  const getChanges = async() => {
+    const { stdout } = await spawnFile('git', ['status', '--porcelain=1', '--', ...files], { stdio: 'pipe' });
+
+    return stdout.replace(/^\s+/, '').replace(/\s+$/, '');
+  };
+
+  if (!fix) {
+    const changes = await getChanges();
+
+    if (changes) {
+      console.log('Cannot run lint without fix with local changes');
+      console.log(changes);
+
+      return false;
+    }
+  }
+
+  await spawnFile('go', ['work', 'sync']);
+  await Promise.all(modFiles.map(modFile => spawnFile('go', ['mod', 'tidy'], { stdio: 'inherit', cwd: path.dirname(modFile) })));
+  if (!fix) {
+    const changes = await getChanges();
+
+    if (changes) {
+      const { stdout } = await spawnFile('git', ['diff', '--', ...files], { stdio: 'pipe' });
+
+      console.log('Had to make modifications');
+      console.log(changes);
+      console.log(stdout);
+
+      return false;
+    }
+  }
+
+  return true;
+}
+
+Promise.all([format(fix), syncModules(fix)]).then((successes) => {
+  if (!successes.every(x => x)) {
+    process.exit(1);
+  }
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
