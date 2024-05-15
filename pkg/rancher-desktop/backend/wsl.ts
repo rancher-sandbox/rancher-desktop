086bbbd34f353825e720f2a722bb9db33685a064
Mark Yen <mark.yen@suse.com>
2024-05-15 09:59:00 -0700

    WSL: Terminate network-setup on stop
    This is needed to ensure that network-setup has the change to gracefully
    shut down.  Note that we suppress errors if the process was not found, so
    it is safe to run even when network tunnel is not in use.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- pkg/rancher-desktop/backend/wsl.ts
+++ pkg/rancher-desktop/backend/wsl.ts
@@ -1742,7 +1742,10 @@ export default class WSLBackend extends events.EventEmitter implements VMBackend
         const initProcess = this.process;
 
         this.process = null;
-        initProcess?.kill('SIGTERM');
+        if (initProcess) {
+          initProcess.kill('SIGTERM');
+          await this.execCommand('/usr/bin/killall', '-q', '/usr/local/bin/network-setup');
+        }
         await this.hostSwitchProcess.stop();
         if (await this.isDistroRegistered({ runningOnly: true })) {
           await this.execWSL('--terminate', INSTANCE_NAME);
