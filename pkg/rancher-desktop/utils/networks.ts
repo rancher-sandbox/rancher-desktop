a63b5aec0e2a39ff50423667beb847f88b443602
Jan Dubois <jan.dubois@suse.com>
2024-05-23 10:20:57 -0700

    The vEthernet address has changed in recent WSL for Win11 releases
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- pkg/rancher-desktop/utils/networks.ts
+++ pkg/rancher-desktop/utils/networks.ts
@@ -7,7 +7,9 @@ export enum networkStatus {
 }
 
 export function wslHostIPv4Address(): string | undefined {
-  const iface = os.networkInterfaces()['vEthernet (WSL)'] ?? [];
+  const interfaces = os.networkInterfaces();
+  // The veth interface name changed at some time on Windows 11, so try the new name if the old one doesn't exist
+  const iface = interfaces['vEthernet (WSL)'] ?? interfaces['vEthernet (WSL (Hyper-V firewall))'] ?? [];
 
   return iface.find(addr => addr.family === 'IPv4')?.address;
 }
