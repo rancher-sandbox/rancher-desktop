5573bab56e6939034af13fa0b4b489ba51661210
Mark Yen <mark.yen@suse.com>
2024-07-10 13:17:11 -0700

    golangci-lint: ineffassign
    We need to be careful of creating new local variables.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- src/go/rdctl/cmd/createProfile.go
+++ src/go/rdctl/cmd/createProfile.go
@@ -101,9 +101,9 @@ func createProfile() (string, error) {
 			// This should have been caught in validateProfileFormatFlags
 			return "", fmt.Errorf(`no input format specified: must specify exactly one input format of "--input FILE|-", "--body|-b STRING", or "--from-settings"`)
 		}
-		connectionInfo, err := config.GetConnectionInfo(false)
-		if err != nil {
-			return "", fmt.Errorf("failed to get connection info: %w", err)
+		connectionInfo, err2 := config.GetConnectionInfo(false)
+		if err2 != nil {
+			return "", fmt.Errorf("failed to get connection info: %w", err2)
 		}
 		rdClient := client.NewRDClient(connectionInfo)
 		command := client.VersionCommand("", "settings")
