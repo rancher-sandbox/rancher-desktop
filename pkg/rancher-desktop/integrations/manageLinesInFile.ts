7799584b61a7891eb3995d591638425e076e1d06
Mark Yen <mark.yen@suse.com>
2024-07-17 17:38:33 -0700

    Integrations: Reimplement manageLinesInFile
    This implementation passes the tests from the previous commit.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- pkg/rancher-desktop/integrations/manageLinesInFile.ts
+++ pkg/rancher-desktop/integrations/manageLinesInFile.ts
@@ -3,78 +3,133 @@ import os from 'os';
 
 import isEqual from 'lodash/isEqual.js';
 
+import { spawnFile } from '@pkg/utils/childProcess';
+
 export const START_LINE = '### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)';
 export const END_LINE = '### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)';
 const DEFAULT_FILE_MODE = 0o644;
 
-// Inserts/removes fenced lines into/from a file. Idempotent.
-// @param path The path to the file to work on.
-// @param desiredManagedLines The lines to insert into the file.
-// @param desiredPresent Whether the lines should be present.
+/**
+ * Inserts/removes fenced lines into/from a file. Idempotent.
+ * @param path The path to the file to work on.
+ * @param desiredManagedLines The lines to insert into the file.
+ * @param desiredPresent Whether the lines should be present.
+ */
 export default async function manageLinesInFile(path: string, desiredManagedLines: string[], desiredPresent: boolean): Promise<void> {
-  // read file, creating it if it doesn't exist
-  let currentContent: string;
+  const copyFlags = fs.constants.COPYFILE_EXCL | fs.constants.COPYFILE_FICLONE;
+  const desired = getDesiredLines(desiredManagedLines, desiredPresent);
+  let fileStats: fs.Stats;
 
   try {
-    currentContent = await fs.promises.readFile(path, 'utf8');
-  } catch (error: any) {
-    if (error.code === 'ENOENT' && desiredPresent) {
-      const lines = buildFileLines([], desiredManagedLines, ['']);
-      const content = lines.join(os.EOL);
+    fileStats = await fs.promises.lstat(path);
+  } catch (ex: any) {
+    if (ex && 'code' in ex && ex.code === 'ENOENT') {
+      // File does not exist.
+      const content = computeTargetContents('', desired);
 
-      await fs.promises.writeFile(path, content, { mode: DEFAULT_FILE_MODE });
+      if (content) {
+        await fs.promises.writeFile(path, content, { mode: DEFAULT_FILE_MODE });
+      }
 
       return;
-    } else if (error.code === 'ENOENT' && !desiredPresent) {
+    } else {
+      throw ex;
+    }
+  }
+
+  if (fileStats.isFile()) {
+    if (await fileHasExtendedAttributes(path)) {
+      throw new Error(`Refusing to manage ${ path } which has extended attributes`);
+    }
+
+    const tempName = `${ path }.rd-temp`;
+
+    await fs.promises.copyFile(path, tempName, copyFlags);
+
+    try {
+      const currentContents = await fs.promises.readFile(path, 'utf-8');
+      const targetContents = computeTargetContents(currentContents, desired);
+
+      if (targetContents === undefined) {
+        // No changes are needed
+        return;
+      }
+
+      if (targetContents === '') {
+        // The resulting file is empty; unlink it.
+        await fs.promises.unlink(path);
+
+        return;
+      }
+
+      await fs.promises.writeFile(tempName, targetContents, 'utf-8');
+      await fs.promises.rename(tempName, path);
+    } finally {
+      try {
+        await fs.promises.unlink(tempName);
+      } catch {
+        // Ignore errors unlinking the temporary file; if everything went well,
+        // it no longer exists anyway.
+      }
+    }
+  } else if (fileStats.isSymbolicLink()) {
+    const backupPath = `${ path }.rd-backup~`;
+
+    await fs.promises.copyFile(path, backupPath, copyFlags);
+
+    const currentContents = await fs.promises.readFile(path, 'utf-8');
+    const targetContents = computeTargetContents(currentContents, desired);
+
+    if (targetContents === undefined) {
+      // No changes are needed; just remove the backup file again.
+      await fs.promises.unlink(backupPath);
+
       return;
-    } else {
-      throw error;
     }
-  }
+    // Always write the file, even if the result will be empty.
+    await fs.promises.writeFile(path, targetContents, 'utf-8');
 
-  // split file into three parts
-  let before: string[];
-  let currentManagedLines: string[];
-  let after: string[];
+    const actualContents = await fs.promises.readFile(path, 'utf-8');
 
-  try {
-    const currentLines = currentContent.split('\n');
-
-    [before, currentManagedLines, after] = splitLinesByDelimiters(currentLines);
-  } catch (error) {
-    throw new Error(`could not split ${ path }: ${ error }`);
-  }
-
-  // make the changes
-  if (desiredPresent && !isEqual(currentManagedLines, desiredManagedLines)) {
-    // This is needed to ensure the file ends with an EOL
-    if (after.length === 0) {
-      after = [''];
+    if (!isEqual(targetContents, actualContents)) {
+      throw new Error(`Error writing to ${ path }: written contents are unexpected; see backup in ${ backupPath }`);
     }
-    const newLines = buildFileLines(before, desiredManagedLines, after);
-    const newContent = newLines.join(os.EOL);
-
-    await fs.promises.writeFile(path, newContent);
-  }
-  if (!desiredPresent) {
-    // Ignore the extra empty line that came from the managed block.
-    if (after.length === 1 && after[0] === '') {
-      after = [];
-    }
-    if (before.length === 0 && after.length === 0) {
-      await fs.promises.rm(path);
-    } else {
-      const newLines = buildFileLines(before, [], after);
-      const newContent = newLines.join(os.EOL);
-
-      await fs.promises.writeFile(path, newContent);
-    }
+    await fs.promises.unlink(backupPath);
+  } else {
+    // Target exists, and is neither a normal file nor a symbolic link.
+    // Return with an error.
+    throw new Error(`Refusing to manage ${ path } which is not a regular file`);
   }
 }
 
-// Splits a file into three arrays containing the lines before the managed portion,
-// the lines in the managed portion and the lines after the managed portion.
-// @param lines An array where each element represents a line in a file.
+/**
+ * Check if the given file has any extended attributes.
+ *
+ * We do this check because we are not confident of being able to write the file
+ * atomically (that is, either the old content or new content is visible) while
+ * also preserving extended attributes.
+ */
+async function fileHasExtendedAttributes(filePath: string): Promise<boolean> {
+  const commandMap: Record<string, string[]> = {
+    darwin: ['/usr/bin/xattr', '-l', filePath],
+    linux:  ['/usr/bin/getfattr', '--dump', filePath],
+  };
+  const command = commandMap[process.platform];
+
+  if (!command) {
+    throw new Error(`Cannot check for extended attributes on ${ process.platform }`);
+  }
+
+  const { stdout } = await spawnFile(command[0], command.slice(1), { stdio: 'pipe' });
+
+  return stdout.trim() !== '';
+}
+
+/**
+ * Splits a file into three arrays containing the lines before the managed portion,
+ * the lines in the managed portion and the lines after the managed portion.
+ * @param lines An array where each element represents a line in a file.
+ */
 function splitLinesByDelimiters(lines: string[]): [string[], string[], string[]] {
   const startIndex = lines.indexOf(START_LINE);
   const endIndex = lines.indexOf(END_LINE);
@@ -94,12 +149,47 @@ function splitLinesByDelimiters(lines: string[]): [string[], string[], string[]]
   return [before, currentManagedLines, after];
 }
 
-// Builds an array where each element represents a line in a file.
-// @param before The portion of the file before the managed lines.
-// @param toInsert The managed lines, not including the fences.
-// @param after The portion of the file after the managed lines.
-function buildFileLines(before: string[], toInsert: string[], after: string[]): string[] {
-  const rancherDesktopLines = toInsert.length > 0 ? [START_LINE, ...toInsert, END_LINE] : [];
+/**
+ * Calculate the desired content of the managed lines.
+ * @param desiredManagedLines The lines to insert into the file.
+ * @param desiredPresent Whether the lines should be present.
+ * @returns The lines that should end up in the managed section of the final file.
+ */
+function getDesiredLines(desiredManagedLines: string[], desiredPresent: boolean): string[] {
+  const desired = desiredPresent && desiredManagedLines.length > 0;
 
-  return [...before, ...rancherDesktopLines, ...after];
+  return desired ? [START_LINE, ...desiredManagedLines, END_LINE] : [];
+}
+
+/**
+ * Given the current contents of the file, determine what the final file
+ * contents should be.
+ * @param currentContents The current contents of the file.
+ * @param desired The desired content of the managed lines.
+ * @returns The final content; if no changes are needed, `undefined` is returned.
+ *          There will never be any leading empty lines,
+ *          and there will always be exactly one trailing empty line.
+ */
+function computeTargetContents(currentContents: string, desired: string[]): string | undefined {
+  const [before, current, after] = splitLinesByDelimiters(currentContents.split('\n'));
+
+  if (isEqual(current, desired)) {
+    // No changes are needed
+    return undefined;
+  }
+
+  const lines = [...before, ...desired, ...after];
+
+  // Remove all leading empty lines.
+  while (lines.length > 0 && lines[0] === '') {
+    lines.shift();
+  }
+  // Remove all trailing empty lines.
+  while (lines.length > 0 && lines[lines.length - 1] === '') {
+    lines.pop();
+  }
+  // Add one trailing empty line to the end.
+  lines.push('');
+
+  return lines.join(os.EOL);
 }
