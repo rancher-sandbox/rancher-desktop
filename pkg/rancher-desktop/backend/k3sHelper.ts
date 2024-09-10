f1b333523d8281841251c7752b713d942e24b80a
Mark Yen <mark.yen@suse.com>
2024-09-10 15:14:29 -0700

    K3sHelper: Don't send GitHub token to k3s.io
    We were accidentally sending the GitHub token to the non-GitHub host.
    This should be typically unset, but that's still kind of bad.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- pkg/rancher-desktop/backend/k3sHelper.ts
+++ pkg/rancher-desktop/backend/k3sHelper.ts
@@ -360,12 +360,8 @@ export default class K3sHelper extends events.EventEmitter {
       let channelResponse: Response;
 
       try {
-        const headers: HeadersInit = { Accept: this.channelApiAccept };
-
-        if (process.env.GITHUB_TOKEN) {
-          headers.Authorization = `Bearer ${ process.env.GITHUB_TOKEN }`;
-        }
-        channelResponse = await fetch(this.channelApiUrl, { headers });
+        channelResponse = await fetch(this.channelApiUrl,
+          { headers: { Accept: this.channelApiAccept } });
       } catch (ex: any) {
         console.log(`updateCache: error: ${ ex }`);
         if (!(await checkConnectivity('k3s.io'))) {
