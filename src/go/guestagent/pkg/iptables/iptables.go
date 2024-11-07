8b1bab008f0d6297f93c38580ae17949f1d1c83a
Nino Kodabande <nkodabande@suse.com>
2024-11-07 10:45:07 -0800

    Replace Time.Sleep for a ticker in ForwardPorts
    Signed-off-by: Nino Kodabande <nkodabande@suse.com>

--- src/go/guestagent/pkg/iptables/iptables.go
+++ src/go/guestagent/pkg/iptables/iptables.go
@@ -37,69 +37,74 @@ import (
 // The argument is a time, in seconds, to wait between updating.
 func ForwardPorts(ctx context.Context, tracker tracker.Tracker, updateInterval time.Duration) error {
 	var ports []iptables.Entry
+
+	ticker := time.NewTicker(updateInterval)
+	defer ticker.Stop()
+
 	for {
-		// Detect ports for forward
-		newPorts, err := iptables.GetPorts()
-		if err != nil {
-			// iptables exiting with an exit status of 4 means there
-			// is a resource problem. For example, something else is
-			// running iptables. In that case, we can skip trying it for
-			// this loop. You can find the exit code in the iptables
-			// source at https://git.netfilter.org/iptables/tree/include/xtables.h
-			if strings.Contains(err.Error(), "exit status 4") {
-				log.Debug("iptables exited with status 4 (resource error). Retrying...")
-				time.Sleep(updateInterval)
-				continue
-			}
-			return err
-		}
-		log.Debugf("iptable found ports %+v", newPorts)
-		// Diff from existing forwarded ports
-		added, removed := comparePorts(ports, newPorts)
-		ports = newPorts
-		// Remove old forwards
-		for _, p := range removed {
-			name := entryToString(p)
-			if err := tracker.Remove(generateID(name)); err != nil {
-				log.Warnf("failed to close listener %q: %w", name, err)
-			}
-		}
-		portMap := make(nat.PortMap)
-		// Add new forwards
-		for _, p := range added {
-			if p.TCP {
-				port := strconv.Itoa(p.Port)
-				portMapKey, err := nat.NewPort("tcp", port)
-				if err != nil {
-					log.Errorf("failed to create a corresponding key for the portMap: %s", err)
-					continue
-				}
-				portBinding := nat.PortBinding{
-					// We can set the hostIP to INADDR_ANY the API Tracker will determine
-					// the admin installation and can adjust this to localhost accordingly
-					HostIP:   "0.0.0.0",
-					HostPort: port,
-				}
-				if pb, ok := portMap[portMapKey]; ok {
-					portMap[portMapKey] = append(pb, portBinding)
-				} else {
-					portMap[portMapKey] = []nat.PortBinding{portBinding}
-				}
-				name := entryToString(p)
-				if err := tracker.Add(generateID(name), portMap); err != nil {
-					log.Errorf("failed to listen %q: %w", name, err)
-				} else {
-					log.Infof("opened listener for %q", name)
-				}
-			}
-		}
 		select {
+		case <-ticker.C:
+			// Detect ports for forward
+			newPorts, err := iptables.GetPorts()
+			if err != nil {
+				// iptables exiting with an exit status of 4 means there
+				// is a resource problem. For example, something else is
+				// running iptables. In that case, we can skip trying it for
+				// this loop. You can find the exit code in the iptables
+				// source at https://git.netfilter.org/iptables/tree/include/xtables.h
+				if strings.Contains(err.Error(), "exit status 4") {
+					log.Debug("iptables exited with status 4 (resource error). Retrying...")
+					continue // Retry in the next iteration
+				}
+				return err
+			}
+			log.Debugf("iptable found ports %+v", newPorts)
+
+			// Diff from existing forwarded ports
+			added, removed := comparePorts(ports, newPorts)
+			ports = newPorts
+
+			// Remove old forwards
+			for _, p := range removed {
+				name := entryToString(p)
+				if err := tracker.Remove(generateID(name)); err != nil {
+					log.Warnf("failed to close listener %q: %w", name, err)
+				}
+			}
+
+			portMap := make(nat.PortMap)
+
+			// Add new forwards
+			for _, p := range added {
+				if p.TCP {
+					port := strconv.Itoa(p.Port)
+					portMapKey, err := nat.NewPort("tcp", port)
+					if err != nil {
+						log.Errorf("failed to create a corresponding key for the portMap: %s", err)
+						continue
+					}
+					portBinding := nat.PortBinding{
+						// We can set the hostIP to INADDR_ANY the API Tracker will determine
+						// the admin installation and can adjust this to localhost accordingly
+						HostIP:   "0.0.0.0",
+						HostPort: port,
+					}
+					if pb, ok := portMap[portMapKey]; ok {
+						portMap[portMapKey] = append(pb, portBinding)
+					} else {
+						portMap[portMapKey] = []nat.PortBinding{portBinding}
+					}
+					name := entryToString(p)
+					if err := tracker.Add(generateID(name), portMap); err != nil {
+						log.Errorf("failed to listen %q: %w", name, err)
+					} else {
+						log.Infof("opened listener for %q", name)
+					}
+				}
+			}
 		case <-ctx.Done():
 			return nil
-		default: // continue the loop
 		}
-		// Wait for next loop
-		time.Sleep(updateInterval)
 	}
 }
 
