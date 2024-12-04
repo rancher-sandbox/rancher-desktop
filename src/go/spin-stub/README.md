a2ea40d48e389663f7e972218fb4825c5771259d
Jan Dubois <jan.dubois@suse.com>
2024-12-04 12:22:32 -0800

    Code review feedback
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- src/go/spin-stub/README.md
+++ src/go/spin-stub/README.md
@@ -4,4 +4,4 @@ This is a stub executable used to launch spin on Windows.
 
 ## Usage
 
-Use like normal spin. It just sets up `SPIN_DATA_DIR` to point to the `spin` subdirectory in the Rancher Desktop application data directory and then invokes `../internal/spin.exe` (relative to the location of the `spin-stub` binary).
+Use it as you would with a normal `spin` command. It simply configures the `SPIN_DATA_DIR` environment variable to point to the spin subdirectory within the Rancher Desktop application data directory, and then runs `../internal/spin.exe` (relative to the location of the `spin-stub` binary).
--- src/go/spin-stub/main.go
+++ src/go/spin-stub/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -12,7 +13,7 @@ const appName = "rancher-desktop"
 func main() {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
-		panic(fmt.Sprintf("failed to get user home directory: %s", err))
+		panic(fmt.Errorf("failed to retrieve the user's home directory: %w", err))
 	}
 	localAppData := os.Getenv("LOCALAPPDATA")
 	if localAppData == "" {
@@ -20,11 +21,11 @@ func main() {
 	}
 	err = os.Setenv("SPIN_DATA_DIR", filepath.Join(localAppData, appName, "spin"))
 	if err != nil {
-		panic(fmt.Sprintf("failed to set SPIN_DATA_DIR: %s", err))
+		panic(fmt.Errorf("failed to set SPIN_DATA_DIR: %w", err))
 	}
 	exe, err := os.Executable()
 	if err != nil {
-		panic(fmt.Sprintf("failed to get executable path: %s", err))
+		panic(fmt.Errorf("failed to get executable path: %w", err))
 	}
 	spin := filepath.Join(filepath.Dir(filepath.Dir(exe)), "internal", "spin.exe")
 	command := exec.Command(spin, os.Args[1:]...)
@@ -32,7 +33,11 @@ func main() {
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 	err = command.Run()
+	var exitError *exec.ExitError
+	if errors.As(err, &exitError) {
+		os.Exit(exitError.ExitCode())
+	}
 	if err != nil {
-		panic(fmt.Sprintf("failed to execute command: %s", err))
+		panic(fmt.Errorf("failed to execute command: %w", err))
 	}
 }
