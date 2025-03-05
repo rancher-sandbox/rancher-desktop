rancher-sandbox/rancher-desktop

commit 2ad5bce65a779090be3ebab8c7cb5f77a4cf6132

RDX: Update preload script loading
Electron has a new API for dealing with preload scripts now; use that
instead of the previous API, which they are likely to remove.

Signed-off-by: Mark Yen <mark.yen@suse.com>

pkg/rancher-desktop/window/index.ts
@@ -211,14 +211,20 @@ function createView() {
   const webPreferences: Electron.WebPreferences = {
     nodeIntegration:     false,
     contextIsolation:    true,
-    preload:             path.join(paths.resources, 'preload.js'),
     sandbox:             true,
     additionalArguments: [JSON.stringify(hostInfo)],
   };
 
   if (currentExtension?.id) {
     webPreferences.partition = `persist:rdx-${ currentExtension.id }`;
-    const webRequest = Electron.session.fromPartition(webPreferences.partition).webRequest;
+    const session = Electron.session.fromPartition(webPreferences.partition);
+    const { webRequest } = session;
+
+    session.registerPreloadScript({
+      id:       `rdx-preload-${ currentExtension.id }`,
+      filePath: path.join(paths.resources, 'preload.js'),
+      type:     'frame',
+    });
 
     webRequest.onBeforeSendHeaders((details, callback) => {
       const source = details.webContents?.getURL() ?? '';

commit 7924c3f2a38f4132a12b20f8f6db8603ee20d7c1

RDX: Don't apply preload script if already applied
If the preload script has already been applied to the session, do not apply
it again as that causes an error.

Signed-off-by: Mark Yen <mark.yen@suse.com>
Fixes: 2ad5bce65a779090be3ebab8c7cb5f77a4cf6132

pkg/rancher-desktop/window/index.ts
@@ -219,12 +219,15 @@ function createView() {
     webPreferences.partition = `persist:rdx-${ currentExtension.id }`;
     const session = Electron.session.fromPartition(webPreferences.partition);
     const { webRequest } = session;
+    const id = `rdx-preload-${ currentExtension.id }`;
 
-    session.registerPreloadScript({
-      id:       `rdx-preload-${ currentExtension.id }`,
-      filePath: path.join(paths.resources, 'preload.js'),
-      type:     'frame',
-    });
+    if (!session.getPreloadScripts().some(script => script.id === id)) {
+      session.registerPreloadScript({
+        id,
+        filePath: path.join(paths.resources, 'preload.js'),
+        type:     'frame',
+      });
+    }
 
     webRequest.onBeforeSendHeaders((details, callback) => {
       const source = details.webContents?.getURL() ?? '';