rancher-sandbox/rancher-desktop

commit 7074a4516885ae66f8d85c713409dfc237aa47e9

Scripts: Use tsx instead of ts-node
tsx seems to better support using ES modules (and mixing with CommonJS
modules); we still have issues with that with ts-node.

This lets us upgrade `@electron/notarize`.

The downside is that tsx uses esbuild, which means we do not get proper
type checking for our scripts.

Signed-off-by: Mark Yen <mark.yen@suse.com>

scripts/ts-wrapper.js
@@ -1,18 +1,22 @@
 /**
- * This script is a wrapper to run TypeScript scripts via node.  This wrapper is
- * necessary as ts-node by default doesn't override the tsconfig.json at all,
- * which means that the `module` compiler option will be ESNext, which works
- * fine elsewhere, but not on the command line.
+ * This script is a wrapper to run TypeScript scripts via tsx.  This is mostly
+ * to set defaults for our tools, such as disabling BrowsersList updates and
+ * showing deprecations.
  */
 
-// Load tsconfig-paths so that ts-node can resolve files based on the 'paths'
-// compiler options key in tsconfig.json.
-require('tsconfig-paths/register');
-
-const { main: tsNodeMain } = require('ts-node/dist/bin');
+const { spawnSync } = require('child_process');
 
 function main(args) {
-  return tsNodeMain(args, { '--compiler-options': { module: 'commonjs' } });
+  const childArgs = [
+    '--trace-warnings',
+    '--trace-deprecation',
+    'node_modules/tsx/dist/cli.mjs',
+  ];
+
+  const finalArgs = [...childArgs, ...args];
+
+  console.log(process.argv0, ...finalArgs);
+  spawnSync(process.argv0, finalArgs, { stdio: 'inherit' });
 }
 
 if (require.main === module) {

commit 6d68037878c4cda2de4c6dddc440cddd5d5d8250

scripts/ts-wrapper: Exit on failure
Previously when we switched to `tsx` we missed checking the return value of
the subprocess (as it doesn't throw an exception on error).  Check that
properly so CI fails correctly when needed.

Fixes: 7074a4516885ae66f8d85c713409dfc237aa47e9
Signed-off-by: Mark Yen <mark.yen@suse.com>

scripts/ts-wrapper.js
@@ -17,7 +17,20 @@ function main(args) {
   const finalArgs = [...childArgs, ...args];
 
   console.log(process.argv0, ...finalArgs);
-  spawnSync(process.argv0, finalArgs, { stdio: 'inherit' });
+  const result = spawnSync(process.argv0, finalArgs, { stdio: 'inherit' });
+
+  if (result.error) {
+    throw result.error;
+  }
+
+  if (typeof result.status === 'number') {
+    process.exit(result.status);
+  }
+
+  if (result.signal) {
+    console.log(`Process exited with signal ${ result.signal }`);
+    process.exit(-1);
+  }
 }
 
 if (require.main === module) {