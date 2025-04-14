rancher-sandbox/rancher-desktop

commit 31564297ef3d093dbb3bfe36f405b1e14651c24d

Fix slowly growing dialogs on Windows
On Windows, if the DPI changed during RD's run, or (possibly) if the
primary display has different DPI settings than the display the dialog is
shown on, we end up with a window that keeps growing taller to infinity.
Fix this by checking (only on Windows) the body height compared to the
document element height, and only resize if the body is shorter.

Signed-off-by: Mark Yen <mark.yen@suse.com>

pkg/rancher-desktop/window/index.ts
@@ -540,11 +540,29 @@ export function openDialog(id: string, opts?: Electron.BrowserWindowConstructorO
     }
   });
 
-  window.webContents.on('preferred-size-changed', (_event, { width, height }) => {
-    if (os.platform() === 'linux') {
+  window.webContents.on('preferred-size-changed', async(_event, { width, height }) => {
+    switch (process.platform) {
+    case 'linux':
       resizeWindow(window, width, height);
-    } else {
-      window.setContentSize(width, height, true);
+      break;
+    case 'win32': {
+      // On Windows, if the primary display DPI is not the same as the current
+      // display DPI, or if the DPI has been change since Rancher Desktop
+      // started, we end up getting successively larger heights.  To work around
+      // this, check for the actual height of the element and jump to the
+      // desired height directly, but only if the body is shorter than the
+      // document.  Note that all the units here are already affected by DPI
+      // scaling, so we don't need to do that manually.
+      const scripts = [{ code: `[document.documentElement.offsetHeight, document.body.offsetHeight]` }];
+      const [docHeight, bodyHeight] = await window.webContents.executeJavaScriptInIsolatedWorld(0, scripts);
+
+      if (docHeight < bodyHeight) {
+        window.setContentSize(width, Math.max(height, bodyHeight));
+      }
+      break;
+    }
+    default:
+      window.setContentSize(width, height);
     }
   });
 

commit db8667eb5c3f126814377b67935390970b41ec97

Fix too-small first-run dialog on Windows
It appears that for the first-run dialog, the document height ends up
being high enough but the actual window (content) height is too small.
Adjust the preferred-size-changed check to check if the content height
is at least the body height (instead of checking the document height
against the body height), to ensure that we can always display the body
fully.

Fixes: 31564297ef3d093dbb3bfe36f405b1e14651c24d
Signed-off-by: Mark Yen <mark.yen@suse.com>

pkg/rancher-desktop/window/index.ts
@@ -549,15 +549,16 @@ export function openDialog(id: string, opts?: Electron.BrowserWindowConstructorO
       // On Windows, if the primary display DPI is not the same as the current
       // display DPI, or if the DPI has been change since Rancher Desktop
       // started, we end up getting successively larger heights.  To work around
-      // this, check for the actual height of the element and jump to the
-      // desired height directly, but only if the body is shorter than the
-      // document.  Note that all the units here are already affected by DPI
+      // this, check for the actual height of the body element and jump to the
+      // desired height directly, but only if the current content height is
+      // smaller.  Note that all the units here are already affected by DPI
       // scaling, so we don't need to do that manually.
-      const scripts = [{ code: `[document.documentElement.offsetHeight, document.body.offsetHeight]` }];
-      const [docHeight, bodyHeight] = await window.webContents.executeJavaScriptInIsolatedWorld(0, scripts);
+      const scripts = [{ code: `document.body.offsetHeight` }];
+      const bodyHeight = await window.webContents.executeJavaScriptInIsolatedWorld(0, scripts);
+      const [, currentHeight] = window.getContentSize();
 
-      if (docHeight < bodyHeight) {
-        window.setContentSize(width, Math.max(height, bodyHeight));
+      if (currentHeight < bodyHeight) {
+        window.setContentSize(width, bodyHeight);
       }
       break;
     }