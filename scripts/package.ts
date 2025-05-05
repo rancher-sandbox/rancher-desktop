rancher-sandbox/rancher-desktop

commit 6075f3a52cba98507bb81cf9eed5e49a436c7786

package: fix macOS usage dropping
Electron-builder v26.0.10 changed their plist implementation, and it is no
longer possible to remove Info.plist keys by setting the value to null.
Manually replicate the functionality using an afterPack hook to edit the
plist file instead.

Signed-off-by: Mark Yen <mark.yen@suse.com>

scripts/package.ts
@@ -15,6 +15,7 @@ import {
   AfterPackContext, Arch, build, CliOptions, Configuration, LinuxTargetSpecificOptions,
 } from 'electron-builder';
 import _ from 'lodash';
+import plist from 'plist';
 import yaml from 'yaml';
 
 import buildUtils from './lib/build-utils';
@@ -98,9 +99,41 @@ class Builder {
     await helper.writeDesktopEntry(options, context.packager.executableName, destination);
   }
 
+  /**
+   * Edit the application's `Info.plist` file to remove the UsageDescription
+   * keys; there is no reason for the application to get any of those permissions.
+   */
+  protected async removeMacUsageDescriptions(context: AfterPackContext) {
+    const { MacPackager } = await import('app-builder-lib/out/macPackager');
+    const { packager } = context;
+    const config = packager.config.mac;
+
+    if (!(packager instanceof MacPackager) || !config) {
+      return;
+    }
+
+    const { productFilename } = packager.appInfo;
+    const plistPath = path.join(context.appOutDir, `${ productFilename }.app`, 'Contents', 'Info.plist');
+    const plistContents = await fs.promises.readFile(plistPath, 'utf-8');
+    const plistData = plist.parse(plistContents);
+
+    if (typeof plistData !== 'object' || !('CFBundleName' in plistData)) {
+      return;
+    }
+    const plistCopy: Record<string, plist.PlistValue> = structuredClone(plistData);
+
+    for (const key in plistData) {
+      if (/^NS.*UsageDescription$/.test(key)) {
+        delete plistCopy[key];
+      }
+    }
+    await fs.promises.writeFile(plistPath, plist.build(plistCopy), 'utf-8');
+  }
+
   protected async afterPack(context: AfterPackContext) {
     await this.flipFuses(context);
     await this.writeLinuxDesktopFile(context);
+    await this.removeMacUsageDescriptions(context);
   }
 
   async package(): Promise<CliOptions> {

packaging/electron-builder.yml
@@ -24,11 +24,6 @@ mac:
   darkModeSupport: true
   hardenedRuntime: true
   gatekeeperAssess: false
-  extendInfo:
-    NSBluetoothAlwaysUsageDescription: ~
-    NSBluetoothPeripheralUsageDescription: ~
-    NSCameraUsageDescription: ~
-    NSMicrophoneUsageDescription: ~
   icon: ./resources/icons/mac-icon.png
   target: [ dmg, zip ]
   identity: ~ # We sign in a separate step

scripts/lib/sign-macos.ts
@@ -259,13 +259,17 @@ async function isBundleExecutable(fullPath: string): Promise<boolean> {
       const infoPlist = path.sep + path.join(...parts.slice(2).reverse(), 'Info.plist');
 
       try {
-        const { stdout } = await spawnFile('/usr/bin/plutil',
-          ['-extract', 'CFBundleExecutable', 'raw', '-expect', 'string', infoPlist],
-          { stdio: 'pipe' });
+        const executableKey = 'CFBundleExecutable';
+        const plistContents = await fs.promises.readFile(infoPlist, 'utf-8');
+        const value = plist.parse(plistContents);
 
-        return stdout.trimEnd() === parts[0];
-      } catch {
-        log.info({ infoPlist }, 'Failed to read Info.plist, assuming not the bundle executable.');
+        if (typeof value !== 'object' || !(executableKey in value)) {
+          return false;
+        }
+
+        return value[executableKey] === parts[0];
+      } catch (ex) {
+        log.info({ ex, infoPlist }, 'Failed to read Info.plist, assuming not the bundle executable.');
 
         return false;
       }

commit 4d6e0fb0b721e29a4c94b6046c9d468551d3ed79

package: re-sign app after modifying Info.plist
Because we manually modified the Info.plist after packing, we need to re-do
the (ad-hoc) code signing step to avoid the system complaining about a
corrupt Info.plist.

Signed-off-by: Mark Yen <mark.yen@suse.com>
Fixes: 6075f3a52cba98507bb81cf9eed5e49a436c7786

scripts/package.ts
@@ -21,6 +21,7 @@ import yaml from 'yaml';
 import buildUtils from './lib/build-utils';
 import buildInstaller, { buildCustomAction } from './lib/installer-win32';
 
+import { spawnFile } from '@pkg/utils/childProcess';
 import { ReadWrite } from '@pkg/utils/typeUtils';
 
 class Builder {
@@ -113,7 +114,8 @@ class Builder {
     }
 
     const { productFilename } = packager.appInfo;
-    const plistPath = path.join(context.appOutDir, `${ productFilename }.app`, 'Contents', 'Info.plist');
+    const appPath = path.join(context.appOutDir, `${ productFilename }.app`);
+    const plistPath = path.join(appPath, 'Contents', 'Info.plist');
     const plistContents = await fs.promises.readFile(plistPath, 'utf-8');
     const plistData = plist.parse(plistContents);
 
@@ -128,6 +130,13 @@ class Builder {
       }
     }
     await fs.promises.writeFile(plistPath, plist.build(plistCopy), 'utf-8');
+
+    // Because we modified the Info.plist, we need to re-sign the app.  This is
+    // just using ad-hoc signing.  Note that this will fail on x86_64, so ignore
+    // it there.
+    if (context.arch !== Arch.x64) {
+      await spawnFile('codesign', ['--sign', '-', '--force', '--verbose', appPath], { stdio: 'inherit' });
+    }
   }
 
   protected async afterPack(context: AfterPackContext) {