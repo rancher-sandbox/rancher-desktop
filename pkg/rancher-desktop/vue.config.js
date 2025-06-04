afa72b7a1681f1e22080fa3f80830e7414f9d470
Mark Yen <mark.yen@suse.com>
2025-06-04 16:37:12 -0700

    Vue 3: Define Vue feature flags
    These are only necessary for production builds, but defining them silences
    a warning.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- pkg/rancher-desktop/vue.config.js
+++ pkg/rancher-desktop/vue.config.js
@@ -50,6 +50,10 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
 
       'process.env.FEATURE_DIAGNOSTICS_FIXES': process.env.RD_ENV_DIAGNOSTICS_FIXES === '1',
+
+      __VUE_OPTIONS_API__:                     true,
+      __VUE_PROD_DEVTOOLS__:                   false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
     }]);
 
     config.module.rule('vue').use('vue-loader').tap((options) => {
