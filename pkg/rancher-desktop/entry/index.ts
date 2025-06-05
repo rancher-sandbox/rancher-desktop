288db108d0bb41033fbf899a8db87ed471a93017
Mark Yen <mark.yen@suse.com>
2025-06-05 14:00:55 -0700

    Vue 3: Stop using global mount, and fix layout detection
    The latter is needed because we switched to defineComponent.
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- pkg/rancher-desktop/entry/index.ts
+++ pkg/rancher-desktop/entry/index.ts
@@ -3,7 +3,7 @@
  */
 
 import Cookies from 'cookie-universal';
-import Vue, { h } from 'vue';
+import { createApp } from 'vue';
 
 import router from './router';
 import store from './store';
@@ -17,8 +17,6 @@ import '../plugins/tooltip';
 import '../plugins/trim-whitespace';
 import '../plugins/v-select';
 
-import type { CompatVue } from '@vue/runtime-dom';
-
 // This does just the Vuex part of cookie-universal-nuxt, which is all we need.
 (store as any).$cookies = Cookies();
 
@@ -27,10 +25,10 @@ import type { CompatVue } from '@vue/runtime-dom';
 // Because we're always using the hash mode for the router, get the correct
 // route based on the hash.
 const matched = router.match(location.hash.substring(1)).matched.find(r => r);
-const component = matched?.components.default as any;
-const layoutName: string = component?.extendOptions?.layout ?? 'default';
+const component = matched?.components.default;
+const layoutName: string = component?.layout ?? 'default';
 const { default: layout } = await import(`../layouts/${ layoutName }.vue`);
 
-new (Vue as unknown as CompatVue)({
-  router, store, render: () => h(layout),
-}).$mount('#app');
+const app = createApp({...layout, store, router});
+
+app.mount('#app');
