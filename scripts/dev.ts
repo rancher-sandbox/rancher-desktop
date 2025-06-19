46ca8b64813595c50130095e453b163305b7105a
Mark Yen <mark.yen@suse.com>
2025-06-18 18:09:23 -0700

    scripts/dev: Fix startup
    If we throw on the second iteration of checkDevServer, we would drop the
    error on the floor and never do anything about it.  This fixes that by
    avoiding the timeout based callback, using a simpler async function instead
    (though that means more complicated handling around the process).
    
    Signed-off-by: Mark Yen <mark.yen@suse.com>

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -7,6 +7,7 @@
 import childProcess from 'child_process';
 import events from 'events';
 import https from 'https';
+import util from 'util';
 
 import fetch from 'node-fetch';
 import psTree from 'ps-tree';
@@ -34,9 +35,7 @@ class DevRunner extends events.EventEmitter {
     this.emit('error', newError);
   }
 
-  get rendererPort() {
-    return 8888;
-  }
+  readonly rendererPort = 8888;
 
   /**
    * Spawn a child process, set up to emit errors on unexpected exit.
@@ -100,75 +99,74 @@ class DevRunner extends events.EventEmitter {
 
   #rendererProcess: null | childProcess.ChildProcess = null;
   /**
-   * Start the renderer process.
+   * Start the renderer process.  The function should return once the renderer
+   * is ready for connections, and the renderer process should continue after
+   * the function returns.
    */
   async startRendererProcess(): Promise<void> {
     await buildUtils.buildPreload();
+    let started = false;
+    let startError: Error | undefined;
 
-    return new Promise((resolve, reject) => {
-      console.info('Renderer process: starting...');
-      process.env.VUE_CLI_SERVICE_CONFIG_PATH = 'pkg/rancher-desktop/vue.config.js';
+    console.info('Renderer process: starting...');
+    process.env.VUE_CLI_SERVICE_CONFIG_PATH = 'pkg/rancher-desktop/vue.config.js';
 
-      this.#rendererProcess = this.spawn(
-        'Renderer process',
-        process.execPath,
-        '--stack-size=16384',
-        'node_modules/@vue/cli-service/bin/vue-cli-service.js',
-        'serve',
-        '--host',
-        'localhost',
-        '--port',
-        this.rendererPort.toString(),
-        '--skip-plugins',
-        'eslint',
-      );
+    this.#rendererProcess = this.spawn(
+      'Renderer process',
+      process.execPath,
+      '--stack-size=16384',
+      'node_modules/@vue/cli-service/bin/vue-cli-service.js',
+      'serve',
+      '--host',
+      'localhost',
+      '--port',
+      this.rendererPort.toString(),
+      '--skip-plugins',
+      'eslint',
+    );
 
-      // Listen for the 'exit' event of the child process and resolve or reject the Promise accordingly.
-      this.#rendererProcess.on('exit', (code, _signal) => {
-        if (code === 0) {
-          resolve();
-        } else {
-          reject(new Error(`Renderer build failed with code ${ code }`));
-        }
-      });
-
-      // Wait for the renderer to finish, so that vue-cli output doesn't
-      // clobber debugging output.
-      const rendererEnv = this.rendererEnv();
-
-      const maxRetries = 30;
-      let retryCount = 0;
-      const retryInterval = 1000;
-
-      const checkDevServer = async() => {
-        try {
-          const response = await fetch(rendererEnv.home, { agent: rendererEnv.agent });
-
-          if (response.ok) {
-            console.info('Renderer process: dev server started');
-            resolve();
-          } else {
-            // Retry if response is not okay
-            retryCount++;
-            if (retryCount < maxRetries) {
-              setTimeout(checkDevServer, retryInterval);
-            } else {
-              reject(new Error(`Renderer process: failed to connect`));
-            }
-          }
-        } catch (error) {
-          // Retry if fetch throws an error
-          retryCount++;
-          if (retryCount < maxRetries) {
-            setTimeout(checkDevServer, retryInterval);
-          } else {
-            reject(new Error(`Renderer process: failed to connect`));
-          }
-        }
-      };
-
-      checkDevServer().catch(e => console.error(e));
+    // Listen for the 'exit' event of the child process and resolve or reject the Promise accordingly.
+    this.#rendererProcess.on('exit', (code, _signal) => {
+      if (!started) {
+        startError = new Error(`Renderer process exited prematurely with code ${ code }`);
+      } else if (code !== 0) {
+        this.emit('error', new Error(`Renderer process exited with code ${ code }`));
+      }
     });
+
+    // Wait for the renderer to finish, so that vue-cli output doesn't
+    // clobber debugging output.
+    const rendererEnv = this.rendererEnv();
+
+    const maxRetries = 30;
+    const retryInterval = 1000;
+
+    for (let retryCount = 0; retryCount < maxRetries;) {
+      if (startError) {
+        throw startError;
+      }
+      try {
+        const response = await fetch(rendererEnv.home, { agent: rendererEnv.agent });
+
+        if (response.ok && !startError) {
+          console.info('Renderer process: dev server started');
+          break;
+        }
+      } catch (ex) {
+        /* nothing */
+      }
+      if (startError) {
+        throw startError;
+      }
+      // Retry if response is not okay or fetch throws an error.
+      if (++retryCount < maxRetries) {
+        await util.promisify(setTimeout)(retryInterval);
+      } else {
+        throw new Error(`Renderer process: failed to connect`);
+      }
+    }
+
+    started = true;
   }
 
   /**
