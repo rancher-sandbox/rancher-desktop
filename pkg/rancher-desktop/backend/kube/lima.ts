rancher-sandbox/rancher-desktop

commit 74c41937783befe6dd97e88669feb07ddb62e381

Lima: install more things in parallel
- K3s can be installed in parallel with writing the service config and the
  runtime classes
- That can be done in parallel with installing buildkit, trivy, and
  credential  helpers.

Signed-off-by: Mark Yen <mark.yen@suse.com>

pkg/rancher-desktop/backend/kube/lima.ts
@@ -136,11 +136,10 @@ export default class LimaKubernetesBackend extends events.EventEmitter implement
    */
   async install(config: BackendSettings, desiredVersion: semver.SemVer, allowSudo: boolean) {
     await this.progressTracker.action('Installing k3s', 50, async() => {
-      // installK3s removes old config and makes sure the directories are recreated
-      await this.installK3s(desiredVersion);
-
       const promises: Promise<void>[] = [];
 
+      // installK3s removes old manifests and static workloads.
+      promises.push(this.installK3s(desiredVersion));
       promises.push(this.writeServiceScript(config, desiredVersion, allowSudo));
       if (config.experimental?.containerEngine?.webAssembly?.enabled) {
         promises.push(BackendHelper.configureRuntimeClasses(this.vm));

pkg/rancher-desktop/backend/lima.ts
@@ -1921,15 +1921,17 @@ export default class LimaBackend extends events.EventEmitter implements VMBacken
         case ContainerEngine.NONE:
           throw new Error('No container engine is set');
         }
-        if (kubernetesVersion) {
-          await this.kubeBackend.install(config, kubernetesVersion, this.#adminAccess);
-        }
 
-        await this.progressTracker.action('Installing Buildkit', 50, this.writeBuildkitScripts());
-        await Promise.all([
+        const tasks = [
+          this.progressTracker.action('Installing Buildkit', 50, this.writeBuildkitScripts()),
           this.progressTracker.action('Installing image scanner', 50, this.installTrivy()),
           this.progressTracker.action('Installing credential helper', 50, this.installCredentialHelper()),
-        ]);
+        ];
+        if (kubernetesVersion) {
+          tasks.push(this.kubeBackend.install(config, kubernetesVersion, this.#adminAccess));
+        }
+
+        await Promise.all(tasks);
 
         if (this.currentAction !== Action.STARTING) {
           // User aborted

commit 210d3e48d9de045a101d476b5adb1a5ee948d085

Backend: kube/lima: wait for k3s before adding manifests
Running the k3s installer will delete any existing manifests / static files
so we need to wait for that to finish before adding anything we need.

Fixes: 74c41937783befe6dd97e88669feb07ddb62e381
Signed-off-by: Mark Yen <mark.yen@suse.com>

pkg/rancher-desktop/backend/kube/lima.ts
@@ -137,16 +137,23 @@ export default class LimaKubernetesBackend extends events.EventEmitter implement
   async install(config: BackendSettings, desiredVersion: semver.SemVer, allowSudo: boolean) {
     await this.progressTracker.action('Installing k3s', 50, async() => {
       const promises: Promise<void>[] = [];
-
-      // installK3s removes old manifests and static workloads.
-      promises.push(this.installK3s(desiredVersion));
       promises.push(this.writeServiceScript(config, desiredVersion, allowSudo));
-      if (config.experimental?.containerEngine?.webAssembly?.enabled) {
-        promises.push(BackendHelper.configureRuntimeClasses(this.vm));
-        if (config.experimental?.kubernetes?.options?.spinkube) {
-          promises.push(BackendHelper.configureSpinOperator(this.vm));
+
+      promises.push((async() => {
+        // installK3s removes old config and makes sure the directories are recreated;
+        // this means it must be done before adding custom manifests.
+        await this.installK3s(desiredVersion);
+
+        const localPromises: Promise<void>[] = [];
+
+        if (config.experimental?.containerEngine?.webAssembly?.enabled) {
+          localPromises.push(BackendHelper.configureRuntimeClasses(this.vm));
+          if (config.experimental?.kubernetes?.options?.spinkube) {
+            localPromises.push(BackendHelper.configureSpinOperator(this.vm));
+          }
         }
-      }
+        await Promise.all(localPromises);
+      })());
       await Promise.all(promises);
     });
 